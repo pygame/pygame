e7a550e55571666b92c730c4adf3e5e538a35ba4
Peter Shinners <PeterShinners@users.noreply.github.com>
2001-09-22 06:56:02 +0000

    Surface.convert() respects SRCALPHA

--- lib/version.py
+++ lib/version.py
@@ -27,7 +27,7 @@ The python version information should always compare greater than any previous
 releases. (hmm, until we get to versions > 10)
 """
 
-ver = '1.2'
+ver = '1.2.1'
 
 def __hiddendummyfunc():
     """pygame.version.ver 
