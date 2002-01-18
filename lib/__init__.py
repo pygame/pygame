c6d9744127d26324e652c12bb55b6990abd3a66e
Peter Shinners <PeterShinners@users.noreply.github.com>
2002-01-18 00:50:19 +0000

    import fixups and icons

--- lib/__init__.py
+++ lib/__init__.py
@@ -17,57 +17,107 @@
 ##
 ##    Pete Shinners
 ##    pete@shinners.org
+"""Pygame is a set of Python modules designed for writing games.
+It is written on top of the excellent SDL library. This allows you
+to create fully featured games and multimedia programs in the python
+language. The package is highly portable, with games running on
+Windows, MacOS, OSX, BeOS, FreeBSD, IRIX, and Linux.
+"""
 
-# main pygame source
-# lets get things coordinated
-
-def import_all_pygame():
-    import sys
-    if sys.platform=='darwin':
-        import os
-        # this may change someday, but we want to chdir to where our file is if we're in / for no
-        # good reason..
-        if (os.getcwd() == '/') and len(sys.argv):
-            os.chdir(os.path.split(sys.argv[0])[0])
-        else:
-            argv0=''
-            if len(sys.argv): argv0=sys.argv[0]
-            print "WARNING!  Running pygame apps from any method other than through python.app (aka through the finder or launchservices) is UNSUPPORTED!"
-            print "          If you insist on using the terminal, type \"open %s\", and hold down the option key if you need to" % (argv0)
-            print "          specify additional command line arguments.  A dialog box will pop up and make you happy, I promise."
-            print ""
-            print "          I sure hope you ran as \"%s %s\" exactly, otherwise you will really have problems."%(sys.executable,' '.join(sys.argv))
-            print "          WindowServer doesn't like what you're doing as is, and it gets really funky if you run things from the path for whatever reason."
-            print ""
-            # not ready for prime time yet, it just rewrites the commandline so windowserver can pick it up
-            #import pygame.macosx
-
-    def makemodules(**mods): return mods
-    modules = makemodules(
-        base=2, cdrom=0, constants=2, cursors=0, display=0,
-        draw=0, event=0, font=0, image=0, joystick=0,
-        key=0, mixer=0, mouse=0, movie=0, rect=1, sprite=0,
-        surface=0, time=0, transform=0, surfarray=0, version=2
-    )
-    for mod, required in modules.items():
-        try:
-            module = __import__('pygame.'+mod, None, None, ['pygame.'+mod])
-            globals()[mod] = module
-            if required == 2:
-                for item in dir(module):
-                    if item[0] != '_':
-                        globals()[item] = getattr(module, item)
-        except ImportError:
-            if required:
-                CannotImportPygame = ImportError
-                MissingModule =  "Cannot Import 'pygame.%s'"%mod
-                raise CannotImportPygame, MissingModule
-            globals()[mod] = None
-
-import_all_pygame()
-del import_all_pygame
-
-Surface = getattr(surface, 'Surface', lambda:Missing_Pygame_Function)
-Rect = getattr(rect, 'Rect', lambda:Missing_Pygame_Function)
+import sys, os
+if sys.platform=='darwin':
+    # this may change someday, but we want to chdir to where our file is if we're in / for no
+    # good reason..
+    if (os.getcwd() == '/') and len(sys.argv):
+        os.chdir(os.path.split(sys.argv[0])[0])
+    else:
+        argv0=''
+        if len(sys.argv): argv0=sys.argv[0]
+        print "WARNING!  Running pygame apps from any method other than through python.app (aka through the finder or launchservices) is UNSUPPORTED!"
+        print "          If you insist on using the terminal, type \"open %s\", and hold down the option key if you need to" % (argv0)
+        print "          specify additional command line arguments.  A dialog box will pop up and make you happy, I promise."
+        print ""
+        print "          I sure hope you ran as \"%s %s\" exactly, otherwise you will really have problems."%(sys.executable,' '.join(sys.argv))
+        print "          WindowServer doesn't like what you're doing as is, and it gets really funky if you run things from the path for whatever reason."
+        print ""
+        # not ready for prime time yet, it just rewrites the commandline so windowserver can pick it up
+        #import pygame.macosx
 
 
+#we need to import like this, each at a time. the cleanest way to import
+#our modules is with the import command (not the __import__ function)
+
+#first, the "required" modules
+from pygame.base import *
+from pygame.constants import *
+from pygame.version import *
+from pygame.rect import Rect
+import pygame.rwobject
+import pygame.surflock
+
+#next, the "standard" modules
+#we still allow them to be missing for stripped down pygame distributions
+try: import pygame.cdrom
+except (ImportError,IOError):cdrom=None
+
+try: import pygame.cursors
+except (ImportError,IOError):cursors=None
+
+try: import pygame.display
+except (ImportError,IOError):display=None
+
+try: import pygame.draw
+except (ImportError,IOError):draw=None
+
+try: import pygame.event
+except (ImportError,IOError):event=None
+
+try: import pygame.image
+except (ImportError,IOError):image=None
+
+try: import pygame.joystick
+except (ImportError,IOError):joystick=None
+
+try: import pygame.key
+except (ImportError,IOError):key=None
+
+try: import pygame.mouse
+except (ImportError,IOError):mouse=None
+
+try: import pygame.sprite
+except (ImportError,IOError):sprite=None
+
+try: from pygame.surface import *
+except (ImportError,IOError):Surface = lambda:Missing_Function
+
+try: import pygame.time
+except (ImportError,IOError):time=None
+
+try: import pygame.transform
+except (ImportError,IOError):transform=None
+
+#lastly, the "optional" pygame modules
+try: import pygame.font
+except (ImportError,IOError):font=None
+
+try: import pygame.mixer
+except (ImportError,IOError):mixer=None
+
+try: import pygame.movie
+except (ImportError,IOError):movie=None
+
+try: import pygame.surfarray
+except (ImportError,IOError):surfarray=None
+
+#there's also a couple "internal" modules not needed
+#by users, but putting them here helps "dependency finder"
+#programs get everything they need (like py2exe)
+try: import pygame.imageext; del imageext
+except (ImportError,IOError):pass
+
+try: import pygame.mixer_music; del mixer_music
+except (ImportError,IOError):pass
+
+
+#cleanup namespace
+del pygame, os, sys, rwobject, surflock
