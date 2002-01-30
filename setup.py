d7633c427ee04816f10a27cd84f2e5c04a0e0ffa
Peter Shinners <PeterShinners@users.noreply.github.com>
2002-01-30 05:18:17 +0000

    bump to version 1.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ audio and video output, and keyboard, mouse and joystick input."""
 
 METADATA = {
     "name":             "pygame",
-    "version":          "1.3.5",
+    "version":          "1.4",
     "license":          "LGPL",
     "url":              "http://www.pygame.org",
     "author":           "Pete Shinners",
