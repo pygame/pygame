4862773faa84b0cc356c123f3ab0217f34e9bfc7
pygame <pygame@localhost>
2003-10-23 05:12:06 +0000

    prep for 1.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ audio and video output, and keyboard, mouse and joystick input."""
 
 METADATA = {
     "name":             "pygame",
-    "version":          "1.5.6",
+    "version":          "1.6",
     "license":          "LGPL",
     "url":              "http://www.pygame.org",
     "author":           "Pete Shinners",
