a31912871cda08e0623b89f73614e33f8a06a0a4
bob <bob@localhost>
2004-05-07 23:29:19 +0000

    Mac cleanup

--- lib/__init__.py
+++ lib/__init__.py
@@ -153,21 +153,26 @@ def __rect_reduce(r):
 copy_reg.pickle(Rect, __rect_reduce, __rect_constructor)
 
 if sys.platform == 'darwin':
-    import MacOS
-    if not MacOS.WMAvailable():
-        raise ImportError, "Can not access the window manager, use bundlebuilder or execute with the pythonw script"
-    del MacOS
-    if (os.getcwd() == '/') and len(sys.argv):
-        os.chdir(os.path.split(sys.argv[0])[0])
+    # replace pygame.init() with a MacOS X specific hook if applicable
     def init(_init=init):
+        import MacOS
+        if not MacOS.WMAvailable():
+            raise ImportError, "Can not access the window manager, use bundlebuilder or execute with the pythonw script"
         try:
             import AppKit
         except ImportError:
             raise ImportError, "PyObjC is required for pygame on OS X"
         if not AppKit.NSApp():
-            # Set up application
+            # install MacOS X specific handlers for when running outside
+            # of an app bundle
             import macosx
-        _init()
+            macosx.install()
+        # we need to do these imports again here because the global versions
+        # went away
+        import os, sys
+        if (os.getcwd() == '/') and len(sys.argv):
+            os.chdir(os.path.basedir(sys.argv[0]))
+        return _init()
 
 #cleanup namespace
 del pygame, os, sys, rwobject, surflock, MissingModule, copy_reg
