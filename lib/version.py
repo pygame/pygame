97a91398373c7e2d8ab6cbbe3c0fd02890ef6abe
René Dudfield <renesd@gmail.com>
2005-03-06 23:33:20 +0000

    changing to 1.7.0

--- lib/version.py
+++ lib/version.py
@@ -27,5 +27,5 @@ The python version information should always compare greater than any previous
 releases. (hmm, until we get to versions > 10)
 """
 
-ver =   '1.6.2'
-vernum = 1,6,2
+ver =   '1.7.0'
+vernum = 1,7,0
