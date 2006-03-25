617ce51eb96a5cdf7f6966ab59e5f20a72bf0689
René Dudfield <renesd@gmail.com>
2006-03-25 04:10:28 +0000

    The start of a clipboard example for the pygame.scrap module.
    The scrap module is unfinished at this stage, so is this example.

---
+++ examples/scrap_clipboard.py
@@ -0,0 +1,40 @@
+import pygame
+from pygame.locals import *
+pygame.init()
+
+pygame.display.set_mode((200, 200))
+
+import pygame.scrap
+pygame.scrap.init()
+
+c = pygame.time.Clock()
+
+going = True
+
+while going:
+    for e in pygame.event.get():
+        if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
+            going = False
+        if e.type == KEYDOWN and e.key == K_g:
+
+            # this number means to look for text data.
+            r = pygame.scrap.get(1413830740)
+            print type(r)
+            print ":%s:" % r
+
+            if type(r) == type(""):
+                print repr(r)
+                print len(r)
+                print "replaced :%s:" % r.replace("\r", "\n")
+
+        if e.type == KEYDOWN and e.key == K_b:
+            r = pygame.scrap.get(1)
+            print type(r)
+            print r
+
+    pygame.display.flip()
+    c.tick(40)
+
+
+
+
