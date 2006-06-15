483b770b22b2fc5a0ed302c9fc9366951cb62f94
Rene Dudfield <renesd@gmail.com>
2006-06-15 10:38:45 +0000

    Added a test for fonts.  Where it should make a one pixel big font  for the empty string.
    Also added a pygame.quit() at the end of a display test to avoid a segfault.

--- test/display_test.py
+++ test/display_test.py
@@ -21,6 +21,36 @@ class DisplayTest( unittest.TestCase ):
         r3 = pygame.Rect(-10,0,-100,-100)
         pygame.display.update(r3)
 
+        # NOTE: if I don't call pygame.quit there is a segfault.  hrmm.
+        pygame.quit()
+        #  I think it's because unittest runs stuff in threads
+        # here's a stack trace...
+        """
+#0  0x08103b7c in PyFrame_New ()
+#1  0x080bd666 in PyEval_EvalCodeEx ()
+#2  0x08105202 in PyFunction_SetClosure ()
+#3  0x080595ae in PyObject_Call ()
+#4  0x080b649f in PyEval_CallObjectWithKeywords ()
+#5  0x08059585 in PyObject_CallObject ()
+#6  0xb7f7aa2d in initbase () from /usr/lib/python2.4/site-packages/pygame/base.so
+#7  0x080e09bd in Py_Finalize ()
+#8  0x08055597 in Py_Main ()
+#9  0xb7e04eb0 in __libc_start_main () from /lib/tls/libc.so.6
+#10 0x08054e31 in _start ()
+
+        """
+
+
+
+    def test_init_quit( self ):
+        """ see if initing, and quiting works.
+        """
+
+        pygame.init()
+        screen = pygame.display.set_mode((100,100))
+        #pygame.quit()
+
+
 
 
 
