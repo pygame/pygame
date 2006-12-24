782b1c17478a0bcd1ad89158fc0f04cbdf1b08ab
Rene Dudfield <renesd@gmail.com>
2006-12-24 03:52:50 +0000

    Added a test for loading jpg, and png files.
    Commented out the display tests, as they crash on windows.
    It's to do with threading, and unittest calling the tests in a sub thread.
    Whereas SDL requires you to call some of those functions in the main thread.

--- test/image_test.py
+++ test/image_test.py
@@ -3,6 +3,7 @@
 
 import unittest
 import pygame, pygame.image, pygame.pkgdata
+import os
 
 class ImageTest( unittest.TestCase ):
     
@@ -17,6 +18,22 @@ class ImageTest( unittest.TestCase ):
         self.assertEqual(surf.get_at((0,0)),(5, 4, 5, 255))
         self.assertEqual(surf.get_height(),32)
         self.assertEqual(surf.get_width(),32)
+
+
+    def testLoadPNG( self ):
+        """ see if we can load a png.
+        """
+        f = os.path.join("examples", "data", "alien1.png")
+        surf = pygame.image.load(f)
+
+
+    def testLoadJPG( self ):
+        """ see if we can load a jpg.
+        """
+        f = os.path.join("examples", "data", "alien1.png")
+        surf = pygame.image.load(f)
+
+
 
 
 if __name__ == '__main__':
