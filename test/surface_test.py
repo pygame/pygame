637e536d0180266d281b50f0bf6bb908400d8d3e
Rene Dudfield <renesd@gmail.com>
2007-05-01 22:36:25 +0000

    Surface.set_clip(None) was misbehaving. Thanks for the bug report Diego Essaya!

---
+++ test/surface_test.py
@@ -0,0 +1,25 @@
+
+import unittest
+import pygame, pygame.transform
+
+
+class SurfaceTest( unittest.TestCase ):
+    
+    def test_set_clip( self ):
+        """ see if surface.set_clip(None) works correctly.
+        """
+        s = pygame.Surface((800, 600))
+        r = pygame.Rect(10, 10, 10, 10)
+        s.set_clip(r)
+        r.move_ip(10, 0)
+        s.set_clip(None)
+        res = s.get_clip()
+        # this was garbled before.
+        self.assertEqual(res[0], 0)
+        self.assertEqual(res[2], 800)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
