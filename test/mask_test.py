906a1e38d8a61304ff6923946cb51398274c4fca
René Dudfield <renesd@gmail.com>
2007-05-08 23:31:40 +0000

    Started on a unittest for Mask.

---
+++ test/mask_test.py
@@ -0,0 +1,34 @@
+
+import unittest
+import pygame
+
+
+class MaskTest( unittest.TestCase ):
+    
+    def test_mask_access( self ):
+        """ 
+        """
+        m = pygame.Mask((10,10))
+        m.set_at((0,0), 1)
+        self.assertEqual(m.get_at((0,0)), 1)
+        m.set_at((9,0), 1)
+        self.assertEqual(m.get_at((9,0)), 1)
+
+        #s = pygame.Surface((10,10))
+        #s.set_at((1,0), (0, 0, 1, 255))
+        #self.assertEqual(s.get_at((1,0)), (0, 0, 1, 255))
+        #s.set_at((-1,0), (0, 0, 1, 255))
+
+
+
+        # out of bounds, should get IndexError
+        m.get_at((-1,0))
+        m.set_at((-1,0), 1)
+        m.set_at((10,0), 1)
+        m.set_at((0,10), 1)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
