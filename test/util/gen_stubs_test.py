d102ab62e9d2133e12742a91edbefee9175eb058
akalias <sublimator@users.noreply.github.com>
2008-05-28 02:25:55 +0000

    Created trunk/test/util directory. Initial import of gen_stubs.py; a unittest test stub generator.

---
+++ test/util/gen_stubs_test.py
@@ -0,0 +1,33 @@
+######################### PYGAME UNITTEST STUBBER TESTS ########################
+"""
+
+TODO
+====
+
+more, more, more
+
+"""
+
+import pygame, unittest
+pygame.init()
+
+from gen_stubs import get_package_modules
+
+class TestStubGenerator(unittest.TestCase):
+    def test_get_package_modules(self):
+        " get_package_modules(pygame): each m must have 'pygame' in its repr()"
+        # eg pygame.mixer, pygame.color
+        
+        # There should be none where 'pygame' is not in the name of the module        
+        self.assert_(not filter( lambda t: 'pygame' not in repr(t),
+                                 get_package_modules(pygame) ) 
+        )
+
+if __name__ == "__main__":
+    if 1:
+        unittest.main()
+    else:
+        # scribble / sketch
+        pass
+        
+################################################################################
