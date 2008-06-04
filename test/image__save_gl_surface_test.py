d14054d48c2ed77886bdd006dc3ff670d6ee65ea
Rene Dudfield <renesd@gmail.com>
2008-06-04 06:00:32 +0000

    removed import of pyopengl

--- test/image__save_gl_surface_test.py
+++ test/image__save_gl_surface_test.py
@@ -1,32 +1,10 @@
 import pygame, unittest, os, test_utils
 from pygame.locals import *
 
-CUBE_POINTS = (
-    (0.5, -0.5, -0.5),  (0.5, 0.5, -0.5),
-    (-0.5, 0.5, -0.5),  (-0.5, -0.5, -0.5),
-    (0.5, -0.5, 0.5),   (0.5, 0.5, 0.5),
-    (-0.5, -0.5, 0.5),  (-0.5, 0.5, 0.5)
-)
-
-CUBE_QUAD_VERTS = (
-    (0, 1, 2, 3), (3, 2, 7, 6), (6, 7, 5, 4),
-    (4, 5, 1, 0), (1, 5, 7, 2), (4, 0, 3, 6)
-)
-
 
 class GL_ImageSave(unittest.TestCase):
     def test_image_save_works_with_opengl_surfaces(self):
-        #pygame.init()
-        from OpenGL.GL import *
-        from OpenGL.GLU import *
         screen = pygame.display.set_mode((640,480), OPENGL|DOUBLEBUF)
-
-        glBegin(GL_QUADS)
-        for face in CUBE_QUAD_VERTS:
-            for vert in face:
-                glVertex3fv(CUBE_POINTS[vert])
-        glEnd()
-
 
         pygame.display.flip()
 
