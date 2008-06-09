58547f914f8ce3734729bba0dd0309dceec073b2
akalias <sublimator@users.noreply.github.com>
2008-06-09 04:12:18 +0000

    Restructured image__save_gl_surface_test.py so the Segmentation Fault doesn't crash run_tests.py. Now raises Exception("Segmentation Fault")

---
+++ test/image__save_gl_surface_test_.py
@@ -0,0 +1,21 @@
+import pygame, unittest, os, test_utils
+from pygame.locals import *
+
+
+class GL_ImageSave(unittest.TestCase):
+    def test_image_save_works_with_opengl_surfaces(self):
+        screen = pygame.display.set_mode((640,480), OPENGL|DOUBLEBUF)
+
+        pygame.display.flip()
+        
+        tmp_dir = test_utils.get_tmp_dir()
+        tmp_file = os.path.join(tmp_dir, "opengl_save_surface_test.png")
+        
+        pygame.image.save(screen, tmp_file)
+        
+        self.assert_(os.path.exists(tmp_file))
+        
+        os.remove(tmp_file)
+
+if __name__ == '__main__': 
+    unittest.main()
