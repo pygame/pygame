9d01655e3329e772b1b4e11ad1d3f42829379430
Lenard Lindstrom <llindstrom@users.noreply.github.com>
2008-06-12 15:50:29 +0000

    Bug fix: pygame.image.save segfault on Windows for opengl surface

--- test/image__save_gl_surface_test_.py
+++ test/image__save_gl_surface_test_.py
@@ -9,7 +9,17 @@ class GL_ImageSave(unittest.TestCase):
         pygame.display.flip()
         
         tmp_dir = test_utils.get_tmp_dir()
+        # Try the imageext module.
         tmp_file = os.path.join(tmp_dir, "opengl_save_surface_test.png")
+        
+        pygame.image.save(screen, tmp_file)
+        
+        self.assert_(os.path.exists(tmp_file))
+        
+        os.remove(tmp_file)
+
+        # Only test the image module.
+        tmp_file = os.path.join(tmp_dir, "opengl_save_surface_test.bmp")
         
         pygame.image.save(screen, tmp_file)
         
