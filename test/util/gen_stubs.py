5d4338ced1237dace4117a8d083e6339f715e158
akalias <akalias@localhost>
2008-06-16 12:57:45 +0000

    Added in mixer.Sound tests

--- test/util/gen_stubs.py
+++ test/util/gen_stubs.py
@@ -1,45 +1,49 @@
-############################ PYGAME UNITTEST STUBBER ###########################
-
-"""
-
-==========
-= README =
-==========
-
-Tests at gen_stubs_test.py
-
-TODO
-====
-
-* more tests
-* optparse command line settings                                        = DONE =
-
-* only create needed stubs; untested units
-    * naming convention for tests
-        * name parser -- breakdown $Class, $unit, $description
-
-* create class XXX(unittest.TestCase) stubs
-    * relative indentation templates                                    = DONE =
-        * find existing or implement                                    = DONE =
-
-* output stubs as file per module
-
-* get some class meta info from src/*.doc using makeref
-
-BUGS
-====
-
-* needs tests
-
-"""
-
 #################################### IMPORTS ###################################
 
 from __future__ import with_statement
-from collections import defaultdict
+
 from optparse import OptionParser
 
-import relative_indentation, pygame, os, sys
+from inspect import isclass, ismodule, getdoc
+
+from unittest import TestCase
+
+import pygame, sys, relative_indentation, re
+
+################################ TESTS DIRECTORY ###############################
+
+from os.path import normpath, join, dirname, abspath
+
+sys.path.append(
+    abspath(normpath(
+        join(dirname(__file__), '../')
+    ))
+)
+
+#################################### IGNORES ###################################
+
+# pygame.sprite.Sprite.__module__ = 'pygame.sprite' 
+# pygame.sprite.Rect.__module__   = 'pygame'
+
+# Can not then filter out nonsensical classes automatically
+
+IGNORE = (
+    'pygame.sprite.Rect',
+)
+
+##################################### TODO #####################################
+
+"""
+
+1)
+    IGNORE. Find a better solution.
+
+
+2)
+
+    Test
+
+"""
 
 ################################ STUB TEMPLATES ################################
 
@@ -52,15 +56,13 @@ STUB_TEMPLATE = relative_indentation.Template ( '''
           ${comments}
 
         self.assert_(test_not_implemented()) ''', 
-        
+
         strip_common = 1, strip_excess = 1
 )
 
-# for list_all_stubs()
+############################## REGULAR EXPRESSIONS #############################
 
-DIVIDER_LINE = '# ' + (78 * '*')
-
-MODULE_HEADING_LINE = '# module: %s\n#\n#\n'
+module_re = re.compile(r"pygame\.([^.]*)\.?")
 
 #################################### OPTIONS ###################################
 
@@ -73,133 +75,175 @@ opt_parser.add_option(
      action = 'store_true',
      help   = "list only test names not stubs" )
 
+opt_parser.set_usage(
+"""
+$ %prog ROOT
+
+eg. 
+
+$ %prog sprite.Sprite
+
+def test_add(self):
+
+    # Doc string for pygame.sprite.Sprite:
+
+    ...
+"""
+)
+
 ################################### FUNCTIONS ##################################
-        
-def is_module(m):
-    return m.__class__.__name__ == 'module'
 
 def module_in_package(module, pkg):
     return ("%s." % pkg.__name__) in module.__name__
 
 def get_package_modules(pkg):
-    modules = (getattr(pkg, x) for x in dir(pkg))
-    return [m for m in modules if is_module(m) and module_in_package(m, pkg)]
+    modules = (getattr(pkg, x) for x in dir(pkg) if is_public(x))
+    return [m for m in modules if ismodule(m) and module_in_package(m, pkg)]
                                                  # Don't want to pick up 
                                                  # string module for example
-def get_doc_str(f, t = None, default = ''):
-    doc = f.__doc__
-    if doc:
-        if not t:         return doc
-        elif callable(t): return t(doc)
-        else:             return t % doc
-    else:
-        return default
-
 def py_comment(input_str):
     return '\n'.join (
-        [('# ' + l.strip(' ')) for l in input_str.split('\n')]
+        [('# ' + l) for l in input_str.split('\n')]
     )
 
 def is_public(obj_name):
-    #TODO: __init__ etc, will want to test constructors of Class X
-
     return not obj_name.startswith(('__','_'))
 
-def get_callables(obj):
+def is_test(f):
+    return f.__name__.startswith('test_')
+
+def get_callables(obj, if_of = None):
     publics = (getattr(obj, x) for x in dir(obj) if is_public(x))
     callables = [x for x in publics if callable(x)]
-    return callables
 
-def callables_from_module(m):
-    #TODO: look into what functions don't have __module__ attribute
-    return filter (
-        lambda f: not hasattr(f, '__module__') or f.__module__ == m.__name__,
-        get_callables(m)
-    )
+    if if_of:
+        callables = [x for x in callables if if_of(x)] # isclass, ismethod etc
+    
+    return set(callables)
 
-def get_methods_parent_class(f):
-    #TODO: test this
-    for attr in ('im_class', '__objclass__'):
-        if hasattr(f, attr):
-            return getattr(f, attr).__name__
+def get_class_from_test_case(TC, default = ''):
+    TC = TC.__name__
+    if 'Type' in TC:
+        return '.' + TC[:TC.index('Type')]
+    else:
+        return default
 
+################################################################################
 
-def test_stub(f, module):
-    parent_class = get_methods_parent_class(f)
-
-    test_name = 'test_'
+def test_stub(f, module, parent_class = None):
+    test_name = 'test_%s' % f.__name__
     unit_name = '%s.' % module.__name__
 
     if parent_class:
         unit_name += '%s.' % parent_class
-        test_name += "%s__" % parent_class
 
-    test_name += f.__name__
     unit_name += f.__name__
 
     stub = STUB_TEMPLATE.render (
 
         test_name = test_name,
-        comments = py_comment(get_doc_str(f)),
+        comments = py_comment(getdoc(f) or ''),
         unitname = unit_name,
     )
 
     return test_name, stub
 
-def module_test_stubs(m):
-    # get module.callable
-    for one_deep in callables_from_module(m):
-        yield test_stub(one_deep, m)
+def names_of(*args):
+    return tuple(map(lambda o: o.__name__, args))
 
-        # get module.Class.callable
-        # TODO: some classes need to be instantiated before can see members
-        
-        for two_deep in get_callables(one_deep):
-            yield test_stub(two_deep, m)
+def module_stubs(module):
+    stubs = {}
 
-def categorized_stubs(package):
-    stubs_categorized = defaultdict(dict)
+    classes = get_callables(module, isclass)
+    functions = get_callables(module) - classes
 
-    for module in [package] + get_package_modules(package):
-        for callable_testname, stub in module_test_stubs(module):
-            stubs_categorized[module.__name__][callable_testname] = stub
+    for class_ in classes:
+        if ("%s.%s" % names_of(module, class_)).startswith(IGNORE):
+            continue
 
-    return stubs_categorized
+        for method in get_callables(class_):
+            stub = test_stub(method, module, class_.__name__ )
+            stubs['%s.%s.%s' % names_of(module, class_, method) ] = stub
 
-def dict_items_ordered_by_key(dictionary):
-    for key in sorted(dictionary.iterkeys()):
-        yield key, dictionary[key]
+    for function in functions:
+        stub = test_stub(function, module)
+        stubs['%s.%s' % names_of(module, function) ] = stub
+    
+    return stubs
 
-def list_all_stubs(categorized, list_of = dict.keys):
-    output = []
-    for module, mapping in dict_items_ordered_by_key(categorized):
-        output += [DIVIDER_LINE]
-        output += [MODULE_HEADING_LINE % module]
+def package_stubs(package):
+    stubs = dict()
 
-        # list_of:
-        #     Use dict.keys for a listing of testname_suffixes one per line
-        #         dict.values for a listing of stubs
+    for module in get_package_modules(package):
+        stubs.update(module_stubs(module))
+    
+    return stubs
 
-        output += sorted(list_of(mapping))
+def already_tested_in_module(module):
+    already = []
 
-    return '\n'.join(output)
+    mod_name =  module.__name__
+    
+    test_name = "%s_test" % mod_name[7:]
+    
+    try: test_file = __import__(test_name)
+    except ImportError: 
+        # TODO: maybe notify?
+        return
+    
+    classes = get_callables(test_file, isclass)
+    test_cases = (t for t in classes if TestCase in t.__bases__)
+    
+    for class_ in test_cases:
+        class_tested = get_class_from_test_case(class_, default = '')
+    
+        for test in get_callables(class_, is_test):
+            fname = test.__name__[5:].split('__')[0]
+            already.append("%s%s.%s" % (mod_name, class_tested, fname))
+    
+    return already
+
+def already_tested_in_package(package):
+    already = []
+
+    for module in get_package_modules(package):
+        already.append(already_tested_in_module(module))
+
+    return already
+
+def get_stubs(root):
+    module_root = module_re.search(root)
+    if module_root:
+        module = getattr(pygame, module_root.group(1))
+        stubs = module_stubs(module)
+        tested = already_tested_in_module(module)
+    else:
+        stubs = package_stubs(pygame)
+        tested = already_tested_in_package(pygame)
+
+    return stubs, tested
 
 if __name__ == "__main__":
     options, args = opt_parser.parse_args()
 
-    stubs = categorized_stubs(pygame)
+    if args:
+        root = args[0]
+        if not root.startswith('pygame.'):
+            root = 'pygame.%s.' % root
 
-    if options.list: render_dict_part = dict.keys
-    else:            render_dict_part = dict.values
+        stubs, tested = get_stubs(root)
 
-    #TODO: make it work with python.version < 2.5
-    with open('test_stubs.py', 'w') as fh:
-        fh.write( list_all_stubs(stubs, list_of = render_dict_part) )
+        for fname in sorted(s for s in stubs.iterkeys() if s not in tested):
+            if not fname.startswith(root): continue  # eg. module.Class
 
-    total_stubs   = sum(len(m) for _, m in stubs.iteritems())
-    total_modules = len(stubs)
+            test_name, stub = stubs[fname]
 
-    print 'Done: %s public callable test stubs created from %s modules' %\
-            (total_stubs,                               total_modules)
+            if options.list:
+                print ('%13s: %s\n%13s: %s\n' %
+                      ('Callable Name', fname, 'Test Name', test_name))
+            else:
+                print stub
+    else:
+        opt_parser.print_help()
 
 ################################################################################
--- test/util/gen_stubs_test.py
+++ test/util/gen_stubs_test.py
@@ -4,30 +4,10 @@
 TODO
 ====
 
-more, more, more
+tests for new stubber: deleted tests for missing units
 
 """
 
 import pygame, unittest
-
-from gen_stubs import get_package_modules, module_test_stubs
-
-class TestStubGenerator(unittest.TestCase):
-    def test_get_package_modules(self):
-        " get_package_modules(pygame): each m must have 'pygame' in its repr()"
-        # eg pygame.mixer, pygame.color
-        
-        # There should be none where 'pygame' is not in the name of the module        
-        self.assert_(not filter( lambda t: 'pygame' not in repr(t),
-                                 get_package_modules(pygame) ) 
-        )
-
-if __name__ == "__main__":
-    if 1:
-        unittest.main()
-    else:
-        # scribble tests -->
-        for _, stub in module_test_stubs(pygame):
-            print stub
 
 ################################################################################
