ffc77341aaa17d507b3d5cc6da5821df13a06d34
René Dudfield <renesd@gmail.com>
2008-06-19 23:45:23 +0000

    commented prints out which were stuffing up the buildbot test parsing.

--- test/font_test.py
+++ test/font_test.py
@@ -5,9 +5,8 @@ class FontTest( unittest.TestCase ):
     def testFontRendering( self ):
         """ 
         """
-        return
-        print __file__
-        #pygame.font.init ()
+        #print __file__
+        pygame.font.init ()
         f = pygame.font.Font(None, 20)
         s = f.render("foo", True, [0, 0, 0], [255, 255, 255])
         s = f.render("xxx", True, [0, 0, 0], [255, 255, 255])
@@ -20,7 +19,7 @@ class FontTest( unittest.TestCase ):
         # null text should be 1 pixel wide.
         s = f.render("", False, [0, 0, 0], [255, 255, 255])
         self.assertEqual(s.get_size()[0], 1)
-        print "fonttest done"
+        #print "fonttest done"
         #pygame.font.quit ()
 
 if __name__ == '__main__':
