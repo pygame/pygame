76037823a139e7b1dbd08094cfb9ff08359ce9df
akalias <sublimator@users.noreply.github.com>
2008-06-24 00:39:18 +0000

    Experimenting with running tests in subprocesses with threads

--- run_tests_sub.py
+++ run_tests_sub.py
@@ -2,7 +2,7 @@
 
 #################################### IMPORTS ###################################
 
-import sys, os, re, unittest, subprocess, time
+import sys, os, re, unittest, subprocess, time, threadmap
 
 ################################### CONSTANTS ##################################
 
@@ -12,40 +12,29 @@ IGNORE = (
 
 TEST_MODULE_RE = re.compile('^(.+_test\.py)$')
 
-NUM_TESTS_RE = re.compile(r"Ran (\d+) tests?")
+NUM_TESTS_RE   = re.compile(r"Ran (\d+) tests?")
+NUM_FAILS_RE   = re.compile(r"failures=(\d+)")
+NUM_ERRORS_RE  = re.compile(r"errors=(\d+)")
 
-NUM_FAILS_RE = re.compile(r"failures=(\d+)")
-
-NUM_ERRORS_RE = re.compile(r"errors=(\d+)")
-
-DIV = "----------------------------------------------------------------------\nRan"
+DIV = (70 * "-") + "\nRan"
 
 ################################################################################
 
-def proc_return_and_output(cmd, wd=None, env=None, bufsize=-1):
-    if not isinstance(cmd, str) and sys.platform == "darwin":
-        cmd = " ".join(cmd)
-
-    proc = subprocess.Popen(
-        cmd, cwd = wd, env = env, shell = True, bufsize = bufsize, 
+def proc_return_and_output(cmd):
+    proc = subprocess.Popen (
+        cmd, shell = True, bufsize=-1,
         stdout = subprocess.PIPE, stderr = subprocess.STDOUT
     )
 
-    response = []
-    finished = 0
+    ret_code = proc.wait()
+    response = proc.stdout.read().replace("\r\n", "\n").replace("\r", "\n")
 
-    while not finished or proc.poll() is None:
-        line = proc.stdout.readline()
-        if not line: finished = 1
-        else:
-            response += [line.replace("\r\n", "\n").replace("\r", "\n")]
-
-    return proc.wait(), ''.join(response)
+    return ret_code, response
 
 ################################################################################
 
-def count_of(re, test_output):
-    count = re.search(test_output)
+def count_of(regex, test_output):
+    count = regex.search(test_output)
     return count and int(count.group(1)) or 0
 
 ################################################################################
@@ -54,7 +43,7 @@ main_dir = os.path.split(os.path.abspath(sys.argv[0]))[0]
 test_subdir = os.path.join(main_dir, 'test')
 
 sys.path.insert(0, test_subdir)
-os.chdir( main_dir )
+os.chdir(main_dir)
 
 import test_utils
 
@@ -64,23 +53,30 @@ t = time.time()
 
 total_tests = total_fails = total_errors = 0
 
-for f in os.listdir(test_subdir):
-    for module in TEST_MODULE_RE.findall(f):
-        if module in IGNORE : continue
+tests = [('python test/%s' % f) for f in os.listdir(test_subdir) 
+                                      if TEST_MODULE_RE.match(f)]
 
-        ret_code, ret = proc_return_and_output('python test/%s' % module)
+tests = threadmap.tmap(proc_return_and_output, tests, stop_on_error = False)
+
+print time.time() -t
+
+# for f in os.listdir(test_subdir):
+#     for module in TEST_MODULE_RE.findall(f):
+#         if module in IGNORE : continue
+
+#         ret_code, ret = proc_return_and_output('python test/%s' % module)
     
-        total_errors += count_of(NUM_ERRORS_RE, ret)
-        total_fails  += count_of(NUM_FAILS_RE,  ret)
-        total_tests  += count_of(NUM_TESTS_RE,  ret)
+#         total_errors += count_of(NUM_ERRORS_RE, ret)
+#         total_fails  += count_of(NUM_FAILS_RE,  ret)
+#         total_tests  += count_of(NUM_TESTS_RE,  ret)
 
-        print "%s %s" % (module, 'OK' in ret and 'OK' or ret.split(DIV)[0])
+#         print "%s %s" % (module, 'OK' in ret and 'OK' or ret.split(DIV)[0])
         
-print "\n%s %s tests in %.3fs\n" % (DIV, total_tests, (time.time() - t))
+# print "\n%s %s tests in %.3fs\n" % (DIV, total_tests, (time.time() - t))
 
-if not total_errors and not total_fails:
-    print 'OK'
-else:
-    print 'FAILED (failures=%s, errors=%s)' % (total_fails, total_errors)
+# if not total_errors and not total_fails:
+#     print 'OK'
+# else:
+#     print 'FAILED (failures=%s, errors=%s)' % (total_fails, total_errors)
 
 ################################################################################
