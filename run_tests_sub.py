91f0bd40fdd81657e0e0512b116e18721eaae239
akalias <sublimator@users.noreply.github.com>
2008-06-24 01:40:48 +0000

    Moved threadmap.py to lib dir and added option to use threads with run_tests_sub.py

--- run_tests_sub.py
+++ run_tests_sub.py
@@ -2,7 +2,18 @@
 
 #################################### IMPORTS ###################################
 
-import sys, os, re, unittest, subprocess, time, threadmap
+import sys, os, re, unittest, subprocess, time
+
+main_dir = os.path.split(os.path.abspath(sys.argv[0]))[0]
+test_subdir = os.path.join(main_dir, 'test')
+lib_subdir = os.path.join(main_dir, 'lib')
+
+sys.path.insert(0, test_subdir)
+sys.path.append(lib_subdir)
+
+import test_utils, threadmap
+
+os.chdir(main_dir)
 
 ################################### CONSTANTS ##################################
 
@@ -20,16 +31,19 @@ DIV = (70 * "-") + "\nRan"
 
 ################################################################################
 
-def proc_return_and_output(cmd):
+def test_return_and_output(cmd):
+    test = cmd.split('/')[1]
+    print 'Starting Test: %s' % test
+
     proc = subprocess.Popen (
-        cmd, shell = True, bufsize=-1,
+        cmd, shell = True, bufsize = -1,
         stdout = subprocess.PIPE, stderr = subprocess.STDOUT
     )
 
     ret_code = proc.wait()
     response = proc.stdout.read().replace("\r\n", "\n").replace("\r", "\n")
 
-    return ret_code, response
+    return test, ret_code, response
 
 ################################################################################
 
@@ -39,44 +53,49 @@ def count_of(regex, test_output):
 
 ################################################################################
 
-main_dir = os.path.split(os.path.abspath(sys.argv[0]))[0]
-test_subdir = os.path.join(main_dir, 'test')
+test_cmds = [('python test/%s' % f) for f in os.listdir(test_subdir) 
+                                          if TEST_MODULE_RE.match(f) 
+                                          and f not in IGNORE]
+t = time.time()
 
-sys.path.insert(0, test_subdir)
-os.chdir(main_dir)
-
-import test_utils
+if '-t' in sys.argv:
+    tests = threadmap.tmap (
+        test_return_and_output, test_cmds, 
+        stop_on_error = False,
+        num_workers = len(sys.argv) == 3 and int(sys.argv[2]) or 20
+    )
+else:
+    tests = map(test_return_and_output, test_cmds)
 
 ################################################################################
 
-t = time.time()
-
 total_tests = total_fails = total_errors = 0
 
-tests = [('python test/%s' % f) for f in os.listdir(test_subdir) 
-                                      if TEST_MODULE_RE.match(f)]
+complete_failure = {}
 
-tests = threadmap.tmap(proc_return_and_output, tests, stop_on_error = False)
+for module, ret_code, ret in tests:
+    if ret_code:
+        complete_failure[module] = ret_code, ret
+        continue
 
-print time.time() -t
-
-# for f in os.listdir(test_subdir):
-#     for module in TEST_MODULE_RE.findall(f):
-#         if module in IGNORE : continue
-
-#         ret_code, ret = proc_return_and_output('python test/%s' % module)
+    total_errors += count_of(NUM_ERRORS_RE, ret)
+    total_fails  += count_of(NUM_FAILS_RE,  ret)
+    total_tests  += count_of(NUM_TESTS_RE,  ret)
     
-#         total_errors += count_of(NUM_ERRORS_RE, ret)
-#         total_fails  += count_of(NUM_FAILS_RE,  ret)
-#         total_tests  += count_of(NUM_TESTS_RE,  ret)
+    print "%s %s" % (module, 'OK' in ret and 'OK' or ret.split(DIV)[0])
 
-#         print "%s %s" % (module, 'OK' in ret and 'OK' or ret.split(DIV)[0])
-        
-# print "\n%s %s tests in %.3fs\n" % (DIV, total_tests, (time.time() - t))
+print "\n%s %s tests in %.3fs\n" % (DIV, total_tests, (time.time() - t))
 
-# if not total_errors and not total_fails:
-#     print 'OK'
-# else:
-#     print 'FAILED (failures=%s, errors=%s)' % (total_fails, total_errors)
+if not total_errors and not total_fails:
+    print 'OK'
+else:
+    print 'FAILED (failures=%s, errors=%s)' % (total_fails, total_errors)
+
+################################################################################
+
+if complete_failure: print '\n' + (70 * '=') +'\nComplete Failures\n'
+for module, (ret_code, ret) in complete_failure.iteritems():
+    print "%s failed with return code of %s" % (module, ret_code)
+    print '\n' + ret
 
 ################################################################################
