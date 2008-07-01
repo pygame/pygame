f2c6f9880373cb6263c13c2ea894bc6e83c16814
akalias <akalias@localhost>
2008-07-01 08:15:08 +0000

    Added in a failure suite for run_tests__test.py

--- test/run_tests__tests/run_tests__test.py
+++ test/run_tests__tests/run_tests__test.py
@@ -1,7 +1,6 @@
 import subprocess, os, sys, re
 
-MERGE_PROGRAM = ""
-INTERACTIVE = 0
+DIFF_PROGRAM = r"H:\PortableApps\WinMergePortable\\WinMergePortable.exe"
 
 IGNORE =  (
     '.svn',
@@ -9,24 +8,17 @@ IGNORE =  (
 )
 
 NORMALIZERS = (
-    ("Ran (\d+) tests in (\d+\.\d+)s", "Ran XXX tests in X.XXXs" ),
+    (r"Ran (\d+) tests in (\d+\.\d+)s", "Ran XXX tests in X.XXXs" ),
+    (r'File ".*?py",', 'File "XXXX.py",')  #TODO: look into why os.path.sep differs
 )
 
-main_dir = os.path.split(os.path.abspath(sys.argv[0]))[0]
+main_dir  = os.path.split(os.path.abspath(sys.argv[0]))[0]
 trunk_dir = os.path.normpath(os.path.join(main_dir, '../../'))
-
-single_cmd = "%s run_tests_sub.py -f %s" 
-subprocess_cmd = single_cmd + ' -s'
 
 test_suite_dirs = [x for x in os.listdir(main_dir) if os.path.isdir(x)
                                                   and x not in IGNORE ]
-
 def norm_result(result):
-    """
-    
-    normalize differences, such as timing between output
-    
-    """
+    "normalize differences, such as timing between output"
 
     for normalizer, replacement in NORMALIZERS:
         result = re.sub(normalizer, replacement, result)
@@ -40,15 +32,41 @@ def call_proc(cmd):
     assert not proc.wait()
     return proc.stdout.read()
 
+def str_2_file(f, s):
+    f = open(f, 'w')
+    try:
+        f.write(s)
+    finally:
+        f.close()
+
+interactive = '-i' in sys.argv
+if interactive: 
+    print 'Running in interactive mode, will view diffs with %s' % DIFF_PROGRAM
+
+single_cmd = "%s run_tests_sub.py -f %s"
+subprocess_cmd = single_cmd + ' -s'
+
 os.chdir(trunk_dir)
 for suite in test_suite_dirs:
-    single = call_proc(single_cmd % (sys.executable, suite))[1]
-    subs = call_proc(subprocess_cmd % (sys.executable, suite))[1]
-    
-    if norm_result(single) != norm_result(subs):
-        if INTERACTIVE:
-            pass ## open in MERGE_PROGRAM
-        else:
-            raise Exception("not matching")
+    single = call_proc(single_cmd % (sys.executable, suite))
+    subs = call_proc(subprocess_cmd % (sys.executable, suite))
 
-print 'OK'
+    normed_single = norm_result(single)
+    normed_subs = norm_result(subs)
+
+    if normed_single != normed_subs:
+        if interactive:
+            os.chdir(main_dir)
+            
+            single_file = "%s_single.txt" % suite
+            subs_file = "%s_subs.txt" % suite
+            
+            str_2_file(single_file, normed_single)
+            str_2_file(subs_file, normed_subs)
+            
+            subprocess.Popen([DIFF_PROGRAM, single_file, subs_file])
+            sys.exit()
+        else:
+            raise Exception("%s suite output not matching" % suite)
+
+print 'OK, output same'
