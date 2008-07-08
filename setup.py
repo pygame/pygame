756db085a340106114ae3aece0e0aab9f147446b
René Dudfield <renesd@gmail.com>
2008-07-08 06:22:38 +0000

    updating version numbers for 1.8.1rc1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ EXTRAS = {}
 
 METADATA = {
     "name":             "pygame",
-    "version":          "1.8.1pre",
+    "version":          "1.8.1rc1",
     "license":          "LGPL",
     "url":              "http://www.pygame.org",
     "author":           "Pete Shinners, Rene Dudfield, Marcus von Appen, Bob Pendleton, others...",
