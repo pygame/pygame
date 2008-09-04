3146b63b458ce9db063888577e9b604816e67707
René Dudfield <renesd@gmail.com>
2008-09-04 06:12:32 +0000

    Test for average_surfaces in 24 bit mode.

--- test/transform_test.py
+++ test/transform_test.py
@@ -387,6 +387,39 @@ class TransformModuleTest( unittest.TestCase ):
         self.assertRaises(TypeError, pygame.transform.average_surfaces, [s1, s2, 1])
 
         self.assertRaises(TypeError, pygame.transform.average_surfaces, (s for s in [s1, s2,s3] ))
+
+
+
+    def test_average_surfaces__24(self):
+
+        SIZE = 32
+        depth = 24
+        s1 = pygame.Surface((SIZE, SIZE), 0, depth)
+        s2 = pygame.Surface((SIZE, SIZE), 0, depth)
+        s3 = pygame.Surface((SIZE, SIZE), 0, depth)
+        s1.fill((10,10,70, 255))
+        s2.fill((10,20,70, 255))
+        s3.fill((10,130,10, 255))
+
+        surfaces = [s1, s2, s3]
+        sr = pygame.transform.average_surfaces(surfaces)
+        self.assertEqual( sr.get_masks(), s1.get_masks() )
+        self.assertEqual( sr.get_flags(), s1.get_flags() )
+        self.assertEqual( sr.get_losses(), s1.get_losses() )
+
+        print ( sr.get_masks(), s1.get_masks() )
+        print ( sr.get_flags(), s1.get_flags() )
+        print ( sr.get_losses(), s1.get_losses() )
+
+        self.assertEqual(sr.get_at((0,0)), (10,53,50,255))
+
+
+
+
+
+
+
+
         
     def test_average_color(self):
         """
