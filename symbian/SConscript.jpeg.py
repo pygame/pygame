9deaf53ef65ac15acc3bb01a69c19d403d12ac8c
gamedude <gamedude@localhost>
2009-01-06 10:02:55 +0000

    Added missing jpeg build scripts

---
+++ symbian/SConscript.jpeg.py
@@ -0,0 +1,78 @@
+# This file is generated with mmp2sconscript
+from scons_symbian import *
+
+Import("TARGET_NAME UID3 PACKAGE_NAME")
+
+# Built as dll because needed by SDL and the application
+target     = TARGET_NAME
+targettype = "dll"
+libraries  = ['euser', 'estlib']
+
+# Static libs
+libraries += []
+
+uid3 = UID3
+
+sources = [
+ 'deps/jpeg/jcomapi.c',
+ 'deps/jpeg/jutils.c',
+ 'deps/jpeg/jerror.c',
+ 'deps/jpeg/jmemmgr.c',
+ 'deps/jpeg/jmemnobs.c',
+ 'deps/jpeg/jcapimin.c',
+ 'deps/jpeg/jcapistd.c',
+ 'deps/jpeg/jctrans.c',
+ 'deps/jpeg/jcparam.c',
+ 'deps/jpeg/jdatadst.c',
+ 'deps/jpeg/jcinit.c',
+ 'deps/jpeg/jcmaster.c',
+ 'deps/jpeg/jcmarker.c',
+ 'deps/jpeg/jcmainct.c',
+ 'deps/jpeg/jcprepct.c',
+ 'deps/jpeg/jccoefct.c',
+ 'deps/jpeg/jccolor.c',
+ 'deps/jpeg/jcsample.c',
+ 'deps/jpeg/jchuff.c',
+ 'deps/jpeg/jcphuff.c',
+ 'deps/jpeg/jcdctmgr.c',
+ 'deps/jpeg/jfdctfst.c',
+ 'deps/jpeg/jfdctflt.c',
+ 'deps/jpeg/jfdctint.c',
+ 'deps/jpeg/jdapimin.c',
+ 'deps/jpeg/jdapistd.c',
+ 'deps/jpeg/jdtrans.c',
+ 'deps/jpeg/jdatasrc.c',
+ 'deps/jpeg/jdmaster.c',
+ 'deps/jpeg/jdinput.c',
+ 'deps/jpeg/jdmarker.c',
+ 'deps/jpeg/jdhuff.c',
+ 'deps/jpeg/jdphuff.c',
+ 'deps/jpeg/jdmainct.c',
+ 'deps/jpeg/jdcoefct.c',
+ 'deps/jpeg/jdpostct.c',
+ 'deps/jpeg/jddctmgr.c',
+ 'deps/jpeg/jidctfst.c',
+ 'deps/jpeg/jidctflt.c',
+ 'deps/jpeg/jidctint.c',
+ 'deps/jpeg/jidctred.c',
+ 'deps/jpeg/jdsample.c',
+ 'deps/jpeg/jdcolor.c',
+ 'deps/jpeg/jquant1.c',
+ 'deps/jpeg/jquant2.c',
+ 'deps/jpeg/jdmerge.c']
+
+
+includes    = []
+sysincludes = ['/epoc32/include', '/epoc32/include/libc']
+defines     = ['JPEG_DLL']
+
+SymbianProgram( target, targettype,
+    sources = sources,
+    includes    = includes,
+    sysincludes = sysincludes,
+    libraries   = libraries,
+    defines     = defines,
+    uid3 = uid3, 
+    package = PACKAGE_NAME,
+)
+
