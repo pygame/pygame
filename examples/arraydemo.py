e4ccad48a9f369f275e4057106fad314b91f8eaf
Lenard Lindstrom <len-l@telus.net>
2009-01-21 21:07:02 +0000

    fix NumPy bug in arraydemo.py and add an array choice command line arguemnt

--- examples/arraydemo.py
+++ examples/arraydemo.py
@@ -1,22 +1,41 @@
 #!/usr/bin/env python
 
+import sys
 import os
 try:
     import pygame
     from pygame import surfarray
-    if 'numpy' in surfarray.get_arraytypes():
-        surfarray.use_arraytype('numpy')
-        import numpy as N
-    else:
-        import Numeric as N
-        surfarray.use_arraytype('numeric')
     from pygame.locals import *
 except ImportError:
-    raise ImportError, 'Error Importing Pygame/surfarray or Numeric'
+    raise ImportError, 'Error Importing Pygame/surfarray'
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 
-def main():
+def main(arraytype=None):
+    """show various surfarray effects
+
+    If arraytype is provided then use that array package. Valid
+    values are 'numeric' or 'numpy'. Otherwise default to NumPy,
+    or fall back on Numeric if NumPy is not installed.
+
+    """
+
+    if arraytype is None:
+        if 'numpy' in surfarray.get_arraytypes():
+            surfarray.use_arraytype('numpy')
+        elif 'numeric' in surfarray.get_arraytype():
+            surfarray.use_arraytype('numeric')
+        else:
+            raise ImportError, 'No array package is installed'
+    else:
+        surfarray.use_arraytype(arraytype)
+
+    if surfarray.get_arraytype() == 'numpy':
+        import numpy as N
+        from numpy import int32
+    else:
+        import Numeric as N
+        from Numeric import Int32 as int32
 
     pygame.init()
     print 'Using', surfarray.get_arraytype().capitalize()
@@ -53,7 +72,9 @@ def main():
 
 
     #striped
-    striped = N.zeros((128, 128, 3))
+    #the element type is required for N.zeros in  NumPy else
+    #an array of float is returned.
+    striped = N.zeros((128, 128, 3), int32)
     striped[:] = (255, 0, 0)
     striped[:,::3] = (0, 255, 255)
     surfdemo_show(striped, 'striped')
@@ -77,8 +98,10 @@ def main():
 
 
     #scaleup
+    #the element type is required for N.zeros in NumPy else
+    #an #array of floats is returned.
     size = N.array(imgarray.shape)*2
-    scaleup = N.zeros(size)
+    scaleup = N.zeros(size, int32)
     scaleup[::2,::2] = imgarray
     scaleup[1::2,::2] = imgarray
     scaleup[:,1::2] = scaleup[:,::2]
@@ -119,7 +142,23 @@ def main():
     #alldone
     pygame.quit()
 
+def usage():
+    print "Usage: command line option [--numpy|--numeric]"
+    print "  The default is to use NumPy if installed,"
+    print "  otherwise Numeric"
 
-if __name__ == '__main__': 
-    main()
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        if '--numpy' in sys.argv:
+            main('numpy')
+        elif '--numeric' in sys.argv:
+            main('numeric')
+        else:
+            usage()
+    elif len(sys.argv) == 1:
+        main()
+    else:
+        usage()
+
+        
 
