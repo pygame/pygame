d5c104d96cdf1e18fa5f2242d544ce20ddea9062
Rene Dudfield <renesd@gmail.com>
2009-02-14 21:28:23 +0000

    Made it use the pygame.examples.camera to test it.  Filled in missing methods.

--- lib/_camera_opencv_highgui.py
+++ lib/_camera_opencv_highgui.py
@@ -18,10 +18,11 @@ def list_cameras():
 
 class Camera:
 
-    def __init__(self, device =0):
+    def __init__(self, device =0, size = (640,480), mode = "RGB"):
         """
         """
         self.camera = highgui.cvCreateCameraCapture(device)
+
 
 
     def set_resolution(self, width, height):
@@ -30,15 +31,19 @@ class Camera:
         # nothing to do here.
         pass
 
+    def start(self):
+        # do nothing here... since the camera is already open.
+        pass
+
     def get_buffer(self):
         """Returns a string containing the raw pixel data.
         """
         return self.get_surface().get_buffer()
 
-    def get_image(self):
-        return self.get_surface()
+    def get_image(self, dest_surf = None):
+        return self.get_surface(dest_surf)
 
-    def get_surface(self):
+    def get_surface(self, dest_surf = None):
         camera = self.camera
 
         im = highgui.cvQueryFrame(camera)
@@ -56,76 +61,23 @@ class Camera:
                 raise ValueError("not sure what to do about this size")
 
             pg_img = pygame.image.frombuffer(xxx, (xx.shape[1],xx.shape[0]), "RGB")
+
+            # if there is a destination surface given, we blit onto that.
+            if dest_surf:
+                dest_surf.blit(pg_img, (0,0))
             return pg_img
             #return xxx
             #return opencv.adaptors.Ipl2PIL(im) 
 
 
-    def get_surfacexx(self):
-        """Returns a pygame Surface.
-        """
-        abuffer, width, height = self.get_buffer()
-        if abuffer:
-            if 1:
-                surf = pygame.image.frombuffer(abuffer, (width, height), "RGB")
-
-                # swap it from a BGR surface to an RGB surface.
-                r,g,b,a = surf.get_masks()
-                surf.set_masks((b,g,r,a))
-
-                r,g,b,a = surf.get_shifts()
-                surf.set_shifts((b,g,r,a))
-
-                surf = pygame.transform.flip(surf, 0,1)
-
-            else:
-
-                # Need to flip the image.
-                surf = pygame.image.fromstring(abuffer, (width, height), "RGB", 1)
-                # swap it from a BGR surface to an RGB surface.
-                r,g,b,a = surf.get_masks()
-                surf.set_masks((b,g,r,a))
-
-                r,g,b,a = surf.get_shifts()
-                surf.set_shifts((b,g,r,a))
-            return surf
-
-
 if __name__ == "__main__":
 
-    from pygame.locals import *
-    pygame.init()
+    # try and use this camera stuff with the pygame camera example.
+    import pygame.examples.camera
 
-    #print list_cameras()
-    #raise ""
+    pygame.camera.Camera = Camera
+    pygame.camera.list_cameras = list_cameras
+    pygame.examples.camera.main()
 
-
-    c = Camera(0)
-    c.set_resolution(640,480)
-    #c.display_capture_pin_properties()
-    #c.display_capture_filter_properties()
-
-
-
-    #import time
-    #time.sleep(1.0)
-    screen = pygame.display.set_mode((640,480))
-    clk = pygame.time.Clock()
-
-    going = True
-    while going:
-        events = pygame.event.get()
-        for e in events:
-            if e.type in [QUIT, KEYDOWN]:
-                going = False
-
-        asurf = c.get_surface()
-        screen.blit(asurf, (0,0))
-        pygame.display.flip()
-
-        clk.tick()
-        print clk.get_fps()
-
-
-
+    
 
