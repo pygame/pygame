a592847fbcd45c0f5df0bed7dbe258ddd5b5bdf5
Lenard Lindstrom <llindstrom@users.noreply.github.com>
2009-03-25 01:58:11 +0000

    Adds tentative SDL_gfx module

---
+++ examples/aacircle.py
@@ -0,0 +1,24 @@
+"""Proof of concept gfxdraw example"""
+
+import pygame
+import pygame.gfxdraw
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((500,500))
+    screen.fill((255, 0, 0))
+    s = pygame.Surface(screen.get_size(), pygame.SRCALPHA, 32)
+    pygame.gfxdraw.aacircle(s, 250, 250, 200, (0, 0, 0))
+    screen.blit(s, (0, 0))
+    pygame.display.flip()
+    try:
+        while 1:
+            event = pygame.event.wait()
+            if event.type == pygame.QUIT:
+                break
+            pygame.display.flip()
+    finally:
+        pygame.quit()
+
+if __name__ == '__main__':
+    main()
