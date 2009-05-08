b586b538cef5b828c0dd1ba5b3c3600f9846cc9c
René Dudfield <renesd@gmail.com>
2009-05-08 08:35:22 +0000

    symbian branch merging

--- src/music.c
+++ src/music.c
@@ -432,7 +432,8 @@ MODINIT_DEFINE (mixer_music)
 #if PY3
     module = PyModule_Create (&_module);
 #else
-    module = Py_InitModule3 ("mixer_music", _music_methods,
+    module = Py_InitModule3 (MODPREFIX "mixer_music", 
+                             _music_methods,
                              DOC_PYGAMEMIXERMUSIC);
 #endif
     if (module == NULL) {
