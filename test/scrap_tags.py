87e8eb52708a79e7a627384b3b7c094923b75ec8
Lenard Lindstrom <len-l@telus.net>
2009-05-11 05:22:45 +0000

    clean up exclude tags. allow scrap_test.py to run in special cases

--- test/scrap_tags.py
+++ test/scrap_tags.py
@@ -1,4 +1,23 @@
-__tags__ = ['ignore', 'subprocess_ignore']
+__tags__ = []
+
+import sys
+
+exclude = False
+
+if ((sys.platform == 'win32' or sys.platform == 'linux2') and
+    sys.version_info < (3, 0, 0)):
+    try:
+        import pygame
+        pygame.scrap._NOT_IMPLEMENTED_
+    except AttributeError:
+        pass
+    else:
+        exclude = True
+else:
+    exclude = True
+
+if exclude:
+    __tags__.extend(['ignore', 'subprocess_ignore'])
 
 
 
