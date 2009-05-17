6fea1ff2287edf1a6584a9058ad27bb95bc6116e
René Dudfield <renesd@gmail.com>
2009-05-17 10:13:31 +0000

    Added a few more doc strings

--- lib/midi.py
+++ lib/midi.py
@@ -71,6 +71,7 @@ def quit():
     """uninitialize the midi module
     pygame.midi.quit(): return None
 
+
     Called automatically atexit if you don't call it.
 
     It is safe to call this function more than once.
@@ -87,8 +88,11 @@ def quit():
 
 
 def get_count():
-    """ gets the number of devices.
-        Device ids range from 0 to get_count() -1
+    """gets the number of devices.
+    pygame.midi.get_count(): return num_devices
+
+
+    Device ids range from 0 to get_count() -1
     """
     return pypm.CountDevices()
 
@@ -97,6 +101,59 @@ def get_count():
 
 def get_default_input_device_id():
     """gets the device number of the default input device.
+    pygame.midi.get_default_input_device_id(): return default_id
+
+
+    Return the default device ID or -1 if there are no devices.
+    The result can be passed to the Input()/Ouput() class.
+
+    On the PC, the user can specify a default device by
+    setting an environment variable. For example, to use device #1.
+
+        set PM_RECOMMENDED_INPUT_DEVICE=1
+
+    The user should first determine the available device ID by using
+    the supplied application "testin" or "testout".
+
+    In general, the registry is a better place for this kind of info,
+    and with USB devices that can come and go, using integers is not
+    very reliable for device identification. Under Windows, if
+    PM_RECOMMENDED_OUTPUT_DEVICE (or PM_RECOMMENDED_INPUT_DEVICE) is
+    *NOT* found in the environment, then the default device is obtained
+    by looking for a string in the registry under:
+        HKEY_LOCAL_MACHINE/SOFTWARE/PortMidi/Recommended_Input_Device
+    and HKEY_LOCAL_MACHINE/SOFTWARE/PortMidi/Recommended_Output_Device
+    for a string. The number of the first device with a substring that
+    matches the string exactly is returned. For example, if the string
+    in the registry is "USB", and device 1 is named
+    "In USB MidiSport 1x1", then that will be the default
+    input because it contains the string "USB".
+
+    In addition to the name, get_device_info() returns "interf", which
+    is the interface name. (The "interface" is the underlying software
+	system or API used by PortMidi to access devices. Examples are
+	MMSystem, DirectX (not implemented), ALSA, OSS (not implemented), etc.)
+	At present, the only Win32 interface is "MMSystem", the only Linux
+	interface is "ALSA", and the only Max OS X interface is "CoreMIDI".
+    To specify both the interface and the device name in the registry,
+    separate the two with a comma and a space, e.g.:
+        MMSystem, In USB MidiSport 1x1
+    In this case, the string before the comma must be a substring of
+    the "interf" string, and the string after the space must be a
+    substring of the "name" name string in order to match the device.
+
+    Note: in the current release, the default is simply the first device
+	(the input or output device with the lowest PmDeviceID).
+    """
+    return pypm.GetDefaultInputDeviceID()
+
+
+
+
+def get_default_output_device_id():
+    """get the device number of the default output device.
+    pygame.midi.get_default_output_device_id(): return default_id
+
 
     Return the default device ID or -1 if there are no devices.
     The result can be passed to the Input()/Ouput() class.
@@ -139,22 +196,12 @@ def get_default_input_device_id():
     Note: in the current release, the default is simply the first device
 	(the input or output device with the lowest PmDeviceID).
     """
-    return pypm.GetDefaultInputDeviceID()
-
-
-
-
-def get_default_output_device_id():
-    """get the device number of the default output device.
-
-    TODO: rewrite the doc from get_default_input_device_id for here too.
-           TODO: once those docs have been rewritten.  
-    """
     return pypm.GetDefaultOutputDeviceID()
 
 
 def get_device_info(an_id):
     """ returns (interf, name, input, output, opened)
+    pygame.midi.get_device_info(an_id): return (interf, name, input, output, opened) 
 
     If the id is out of range, the function returns None.
     """
@@ -169,7 +216,13 @@ class MidiException(Exception):
 
 
 class Input(object):
+    """ An Input object is used to get midi input from midi devices.
+    Input(device_id)
+    Input(device_id, buffer_size)
 
+    buffer_size -the number of input events to be buffered waiting to 
+      be read using Input.read() 
+    """
 
     def __init__(self, device_id, buffer_size=4096):
         """
