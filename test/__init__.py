d82cf71d29c011eba75d82a80c5af90399d7d4a3
Lenard Lindstrom <len-l@telus.net>
2009-05-19 00:23:46 +0000

    make python -m pygame.docs bring up the docs in a browser. also fix Python 3 issues with executable packages

--- test/__init__.py
+++ test/__init__.py
@@ -5,7 +5,7 @@ Exports function run()
 A quick way to run the test suite package from the command line
 is by importing the go submodule:
 
-python -c "import pygame.tests.go" [&lt;test options&gt;]
+python -m "import pygame.tests" [<test options>]
 
 Command line option --help displays a usage message. Available options
 correspond to the pygame.tests.run arguments.
@@ -14,16 +14,18 @@ The xxxx_test submodules of the tests package are unit test suites for
 individual parts of Pygame. Each can also be run as a main program. This is
 useful if the test, such as cdrom_test, is interactive.
 
-For Pygame development the test suite can be run from a Pygame 
-distribution root directory. Program run_tests.py is provided for convenience,
-though test/go.py can be run directly.
+For Pygame development the test suite can be run from a Pygame distribution
+root directory using run_tests.py. Alternately, test/__main__.py can be run
+directly.
 
 """
 
 if __name__ == 'pygame.tests':
     from pygame.tests.test_utils.run_tests import run
 elif __name__ == '__main__':
-    import pygame.tests.go
+    import os
+    pkg_dir = os.path.dirname(os.path.abspath(__file__))
+    main = os.path.join(pkg_dir, '__main__.py')
+    execfile(main)
 else:
     from test.test_utils.run_tests import run
-
