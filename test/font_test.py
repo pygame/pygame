fc95e356140255bd426344326083a191f1f0a85f
Lenard Lindstrom <len-l@telus.net>
2009-05-21 01:49:23 +0000

    add some interactive font unit tests to fill in some holes

--- test/font_test.py
+++ test/font_test.py
@@ -257,7 +257,95 @@ class FontTypeTest( unittest.TestCase ):
         def fetch():
             font = pygame.font.Font('some-fictional-font.ttf', 20)
         self.failUnlessRaises(IOError, fetch)
-        
+
+
+class VisualTests( unittest.TestCase ):
+    __tags__ = ['interactive']
+    
+    screen = None
+    aborted = False
+    
+    def setUp(self):
+        if self.screen is None:
+            pygame.init()
+            self.screen = pygame.display.set_mode((600, 200))
+            self.screen.fill((255, 255, 255))
+            pygame.display.flip()
+            self.f = pygame.font.Font(None, 32)
+
+    def abort(self):
+        if self.screen is not None:
+            pygame.quit()
+        self.aborted = True
+
+    def query(self,
+              bold=False, italic=False, underline=False, antialiase=False):
+        if self.aborted:
+            return False
+        spacing = 10
+        offset = 20
+        y = spacing
+        f = self.f
+        screen = self.screen
+        screen.fill((255, 255, 255))
+        pygame.display.flip()
+        if not (bold or italic or underline or antialiase):
+            text = "normal"
+        else:
+            modes = []
+            if bold:
+                modes.append("bold")
+            if italic:
+                modes.append("italic")
+            if underline:
+                modes.append("underlined")
+            if antialiase:
+                modes.append("antialiased")
+            text = "%s (y/n):" % ('-'.join(modes),)
+        f.set_bold(bold)
+        f.set_italic(italic)
+        f.set_underline(underline)
+        s = f.render(text, antialiase, (0, 0, 0))
+        screen.blit(s, (offset, y))
+        y += s.get_size()[1] + spacing
+        f.set_bold(False)
+        f.set_italic(False)
+        f.set_underline(False)
+        s = f.render("(some comparison text)", False, (0, 0, 0))
+        screen.blit(s, (offset, y))
+        pygame.display.flip()
+        while 1:
+            for evt in pygame.event.get():
+                if evt.type == pygame.KEYDOWN:
+                    if evt.key == pygame.K_ESCAPE:
+                        self.abort()
+                        return False
+                    if evt.key == pygame.K_y:
+                        return True
+                    if evt.key == pygame.K_n:
+                        return False
+                if evt.type == pygame.QUIT:
+                    self.abort()
+                    return False
+
+    def test_bold(self):
+        self.failUnless(self.query(bold=True))
+
+    def test_italic(self):
+        self.failUnless(self.query(italic=True))
+
+    def test_underline(self):
+        self.failUnless(self.query(underline=True))
+
+    def test_antialiase(self):
+        self.failUnless(self.query(antialiase=True))
+
+    def test_bold_antialiase(self):
+        self.failUnless(self.query(bold=True, antialiase=True))
+
+    def test_italic_underline(self):
+        self.failUnless(self.query(italic=True, underline=True))
+
 
 if __name__ == '__main__':
     unittest.main()
