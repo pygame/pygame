594b51913ffd486a674fc757dc1187a608b55e7d
ab3 <ab3@localhost>
2009-08-15 21:43:48 +0000

    add test/camera_test.py

--- test/camera_test.py
+++ test/camera_test.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     pkg_dir = os.path.split(os.path.abspath(__file__))[0]
     parent_dir, pkg_name = os.path.split(pkg_dir)
     is_pygame_pkg = (pkg_name == 'tests' and
-                     os.path.split(parent_dir)[1] == 'pygame')
+                      os.path.split(parent_dir)[1] == 'pygame')
     if not is_pygame_pkg:
         sys.path.insert(0, parent_dir)
 else:
