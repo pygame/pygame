c078b5e2e2813f59319576a0956c7d18b324739c
Hobbes <TLHobbes@users.noreply.github.com>
2009-08-16 20:37:27 +0000

    Merged tylerthemovie branch changes r2168:2614 into the trunk

---
+++ test/font_tags.py
@@ -0,0 +1,6 @@
+__tags__ = []
+
+import sys
+# Font support not fully implemented for Python 3.x.
+if sys.version_info >= (3, 0, 0) and sys.platform.startswith['Win']:
+    __tags__.extend(['ignore', 'subprocess_ignore'])
---
+++ test/midi_tags.py
@@ -0,0 +1,1 @@
+__tags__ = ['interactive']
