bcac0bebde9f9f08bf53b25e9c49a693794f3670
Lenard Lindstrom <llindstrom@users.noreply.github.com>
2009-08-21 17:07:12 +0000

    remove _movie and freetype modules from Python 3 build

--- test/_movie_tags.py
+++ test/_movie_tags.py
@@ -7,6 +7,9 @@ if (sys.platform == 'linux2'):
 else:
     exclude = True
 
+# not portable to Python 3 yet.
+exclude = exclude or sys.version_info > (3,)
+
 if exclude:
     __tags__.extend(['ignore', 'subprocess_ignore'])
 
