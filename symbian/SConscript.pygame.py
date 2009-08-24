0dafe4f2472bcd5a4e484f71bbabb6b30093d311
Jussi Toivola <jtoivola@users.noreply.github.com>
2009-08-24 19:40:43 +0000

    Initial support for building pygame libraries as dynamically loaded .pyds instead of linking statically

--- symbian/SConscript.pygame.py
+++ symbian/SConscript.pygame.py
@@ -1,14 +1,11 @@
 Import("*")
 
+from glob import glob
 from scons_symbian import *
-import glob
+import os
 
 python_includes = [ PYTHON_INCLUDE ]
-
-#music.c
-#mixer.c
-#font.c
-#imageext.c
+ 
 IGNORED = r"""
 pypm.c
 camera.c
@@ -27,8 +24,8 @@ _numericsndarray.c
 joystick.c
 cdrom.c""".split()
 
-pygame_sources = glob.glob( "../src/*.c" )
-pygame_sources += glob.glob( "../src/SDL_gfx/*.c" )
+pygame_sources = glob( "../src/*.c" )
+pygame_sources += glob( "../src/SDL_gfx/*.c" )
 removed = []
 for x in pygame_sources:
     for y in IGNORED:        
@@ -38,17 +35,27 @@ for x in pygame_sources:
 
 for x in removed:    
     pygame_sources.remove(x)
-
-pygame_sources.append("common/builtins.c")
-
+    
 if USE_OPENC:
     C_LIB_INCLUDE = "OPENC"
 else:
     C_LIB_INCLUDE = ""
-    
-# Build pygame library
-SymbianProgram( "pygame", TARGETTYPE_LIB,
-                sources = pygame_sources,
+
+#: List of static modules for linking
+PYGAME_STATIC_MODULES = []
+
+def createPygameLibrary( modname, sources, ):
+    modname = "pygame_" + modname
+    uid     = 0
+    targettype = TARGETTYPE_LIB
+    if not HAVE_STATIC_MODULES:
+        targettype = TARGETTYPE_PYD
+        # Add the PyS60 prefix
+        modname    = "kf_" + modname
+        uid        = getUID()
+    # Build pygame library
+    SymbianProgram( modname, targettype,
+                sources = ["../src/" + x for x in sources],
                 defines = [ 
                    C_LIB_INCLUDE                   
                 ],
@@ -62,21 +69,64 @@ SymbianProgram( "pygame", TARGETTYPE_LIB,
                              join( "deps", "SDL", "include"),                              
                              join( "deps", "SDL", "symbian", "inc"),
                              C_INCLUDE,                             
-                             #join( "..", "..", "tools", "debug" )
                            ],
                 package = PACKAGE_NAME,
                 libraries = C_LIBRARY + [
                      PYTHON_LIB_NAME,                     
                      "euser", "avkon", "apparc", 
-                     "cone","eikcore", "libGLES_CM",                     
+                     "cone","eikcore", "libGLES_CM", "pygame_libjpeg",                    
                      SDL_DLL_NAME,
                      ],
                 winscw_options = "-w noempty",
+                uid3 = uid,
                 )
+    m = ".".join( [modname, targettype] )
+    
+    if HAVE_STATIC_MODULES:
+        PYGAME_STATIC_MODULES.append( m )
+     
+
+def createPygameMods():
+    """ Create pygame native modules """
+    
+    # Get the mods. Python wrappers can be conveniently used here.
+    mods = [ os.path.basename( x ).replace(".py", "") for x in glob("lib/*.py") ]
+    
+    # Most of the modules have only 1 source file : <modname>.c
+    # This dict can be used to map differing or multiple source files to module. 
+    module_src_map = { 
+        "surface" : (
+            "surface.c",
+            "scale2x.c",
+            "surface_fill.c",
+            "alphablit.c",            
+        ),
+        "gfxdraw" : ( 
+            "gfxdraw.c", 
+            "SDL_gfx/SDL_gfxPrimitives.c" 
+        ),   
+        "fastevent" : (
+            "fastevents.c",
+            "fastevent.c"
+        ),
+        "transform" : (
+            "transform.c",
+            "rotozoom.c",
+            "scale2x.c"
+        )
+    }
+
+    for x in mods:
+        # Get source mapping
+        src = module_src_map.get( x, [ x + ".c"])
+        createPygameLibrary( x, src )
+    
+    # This one is special    
+    createPygameLibrary( "mixer_music", ["music.c"])        
+    
+createPygameMods()
 
 # Install pygame python libraries
-from glob import glob
-
 pylibzip = "data/pygame/libs/pygame.zip"
 def to_package(**kwargs):
     kwargs["source"] = abspath( kwargs["source"] )
@@ -100,16 +150,16 @@ for x in glob( "../lib/threads/*.py"):
 for x in glob( "lib/*.py"): 
     to_package( source = x, target = pygame_lib )
     
-# Install default font
-#to_package( source = "../lib/freesansbold.ttf", target = pygame_lib )
-
-
-
 def packagePyS60Stdlib(**kwargs):
     kwargs["source"] = join( "deps/PythonForS60/module-repo/standard-modules", kwargs["source"] )
     return to_package( **kwargs )
 
-packagePyS60Stdlib( source = "glob.py",    target = "data/pygame/libs" )
+# Add files missing from standard PyS60 installation
+packagePyS60Stdlib( source = "glob.py", target = "data/pygame/libs" )
 zippath = packagePyS60Stdlib( source = "fnmatch.py", target = "data/pygame/libs" )
 
+# Install default font into zip as well
 File2Zip( zippath, "../lib/freesansbold.ttf", "pygame/freesansbold.ttf" )
+
+# Export static library names to be used for building pygame.exe
+Export( "PYGAME_STATIC_MODULES")
