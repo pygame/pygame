9dc428eb3a80e98ebf0c25668f9ed23aabf61bc1
Lenard Lindstrom <llindstrom@users.noreply.github.com>
2009-08-24 17:20:25 +0000

    fix Python 3 compiler error for freetype module

--- test/freetype_font_tags.py
+++ test/freetype_font_tags.py
@@ -1,7 +1,7 @@
 __tags__ = []
 import sys
 
-exclude = sys.version_info > (3,) # Remove when builds for Python 3.1
+exclude = False
 
 if exclude:
     __tags__.extend(['ignore', 'subprocess_ignore'])
