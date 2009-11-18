ee2a11b55ee5bf199faaa75dddfafb2125de9f05
Lorenz Quack <donlorenzo@users.noreply.github.com>
2009-11-18 08:45:04 +0000

    added missing documentation files from the math module

---
+++ src/doc/math_doc.h
@@ -0,0 +1,85 @@
+/* Auto generated file: with makeref.py .  Docs go in src/ *.doc . */
+#define DOC_PYGAMEMATH "pygame module for vector classes"
+
+#define DOC_PYGAMEMATHVECTOR2 "pygame.math.Vector2(): return Vector2\npygame.math.Vector2(Vector2): return Vector2\npygame.math.Vector2(x, y): return Vector2\npygame.math.Vector2((x, y)): return Vector2\na 2-Dimensional Vector"
+
+#define DOC_VECTOR2DOT "Vector2.dot(Vector2): return float\ncalculates the dot- or scalar-product with the other vector"
+
+#define DOC_VECTOR2CROSS "Vector2.cross(Vector2): return float\ncalculates the cross- or vector-product"
+
+#define DOC_ ""
+
+#define DOC_PYGAMEMATHVECTOR3 "pygame.math.Vector3(): return Vector3\npygame.math.Vector3(Vector3): return Vector3\npygame.math.Vector3(x, y, z): return Vector3\npygame.math.Vector3((x, y, z)): return Vector3\na 3-Dimensional Vector"
+
+#define DOC_VECTOR3DOT "Vector3.dot(Vector3): return float\ncalculates the dot- or scalar-product with the other vector"
+
+#define DOC_VECTOR3CROSS "Vector3.cross(Vector3): return float\ncalculates the cross- or vector-product"
+
+#define DOC_ ""
+
+
+
+/* Docs in a comments... slightly easier to read. */
+
+
+/*
+
+pygame.math
+ pygame module for vector classes
+
+
+
+pygame.math.Vector2
+ pygame.math.Vector2(): return Vector2
+pygame.math.Vector2(Vector2): return Vector2
+pygame.math.Vector2(x, y): return Vector2
+pygame.math.Vector2((x, y)): return Vector2
+a 2-Dimensional Vector
+
+
+
+Vector2.dot
+ Vector2.dot(Vector2): return float
+calculates the dot- or scalar-product with the other vector
+
+
+
+Vector2.cross
+ Vector2.cross(Vector2): return float
+calculates the cross- or vector-product
+
+
+
+
+ 
+
+
+
+pygame.math.Vector3
+ pygame.math.Vector3(): return Vector3
+pygame.math.Vector3(Vector3): return Vector3
+pygame.math.Vector3(x, y, z): return Vector3
+pygame.math.Vector3((x, y, z)): return Vector3
+a 3-Dimensional Vector
+
+
+
+Vector3.dot
+ Vector3.dot(Vector3): return float
+calculates the dot- or scalar-product with the other vector
+
+
+
+Vector3.cross
+ Vector3.cross(Vector3): return float
+calculates the cross- or vector-product
+
+
+
+
+ 
+
+
+
+*/
+
