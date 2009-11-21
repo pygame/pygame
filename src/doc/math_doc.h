7dd3647a43c3ec01ec9b4c19fcaa75a5e26c181e
Lorenz Quack <donlorenzo@users.noreply.github.com>
2009-11-21 13:20:37 +0000

    some more Py3k fixes. compiles but test don't pass yet. improved the pygame.math docs a bit

--- src/doc/math_doc.h
+++ src/doc/math_doc.h
@@ -1,19 +1,105 @@
 /* Auto generated file: with makeref.py .  Docs go in src/ *.doc . */
 #define DOC_PYGAMEMATH "pygame module for vector classes"
 
+#define DOC_PYGAMEMATHENABLESWIZZLING "math.enable_swizzling(): return None\nglobally enables swizzling for vectors."
+
+#define DOC_PYGAMEMATHDIABLESWIZZLING "math.disable_swizzling(): return None\nglobally disables swizzling for vectors."
+
 #define DOC_PYGAMEMATHVECTOR2 "pygame.math.Vector2(): return Vector2\npygame.math.Vector2(Vector2): return Vector2\npygame.math.Vector2(x, y): return Vector2\npygame.math.Vector2((x, y)): return Vector2\na 2-Dimensional Vector"
 
 #define DOC_VECTOR2DOT "Vector2.dot(Vector2): return float\ncalculates the dot- or scalar-product with the other vector"
 
 #define DOC_VECTOR2CROSS "Vector2.cross(Vector2): return float\ncalculates the cross- or vector-product"
 
-#define DOC_ ""
+#define DOC_VECTOR2LENGTH "Vector2.length(): return float\nreturns the euclidic length of the vector."
+
+#define DOC_VECTOR2LENGTHSQUARED "Vector2.length_squared(): return float\nreturns the squared euclidic length of the vector."
+
+#define DOC_VECTOR2NORMALIZE "Vector2.normalize(): return Vector2\nreturns a vector with the same direction but length 1."
+
+#define DOC_VECTOR2NORMALIZEIP "Vector2.normalize(): return None\nnormalizes the vector in place so that its length is 1."
+
+#define DOC_VECTOR2ISNORMALIZED "Vector2.is_normalized(): return Bool\ntests if the vector is normalized i.e. has length == 1."
+
+#define DOC_VECTOR2SCALETOLENGTH "Vector2.scale_to_length(float): return None\nscales the vector to a given length."
+
+#define DOC_VECTOR2REFLECT "Vector2.reflect(Vector2): return Vector2\nreturns a vector reflected of a given normal."
+
+#define DOC_VECTOR2REFLECTIP "Vector2.reflect(Vector2): return None\nreflect the vector of a given normal in place."
+
+#define DOC_VECTOR2DISTANCETO "Vector2.distance_to(Vector2): return float\ncalculates the euclidic distance to a given vector."
+
+#define DOC_VECTOR2DISTANCESQUAREDTO "Vector2.distance_squared_to(Vector2): return float\ncalculates the squared euclidic distance to a given vector."
+
+#define DOC_VECTOR2LERP "Vector2.lerp(Vector2): return Vector2\nreturns a linear interpolation to the given vector."
+
+#define DOC_VECTOR2SLERP "Vector2.slerp(Vector2): return Vector2\nreturns a spherical interpolation to the given vector."
+
+#define DOC_VECTOR2ELEMENTWISE "Vector2.elementwise(): return VectorElementwizeProxy\nThe next operation will be performed elementwize."
+
+#define DOC_VECTOR2ROTATE "Vector2.rotate(float): return Vector2\nrotates a vector by a given angle in degrees."
+
+#define DOC_VECTOR2ROTATEIP "Vector2.rotate_ip(float): return None\nrotates the vector by a given angle in degrees in place."
+
+#define DOC_VECTOR2ANGLETO "Vector2.angle_to(Vector2): return float\ncalculates the angle to a given vector in degrees."
+
+#define DOC_VECTOR2ASPOLAR "Vector2.as_polar(): return (r, phi)\nreturns a tuple with radial distance and azimuthal angle."
+
+#define DOC_VECTOR2FROMPOLAR "Vector2.from_polar((r, phi)): return None\nSets x and y from a polar coordinates tuple."
 
 #define DOC_PYGAMEMATHVECTOR3 "pygame.math.Vector3(): return Vector3\npygame.math.Vector3(Vector3): return Vector3\npygame.math.Vector3(x, y, z): return Vector3\npygame.math.Vector3((x, y, z)): return Vector3\na 3-Dimensional Vector"
 
 #define DOC_VECTOR3DOT "Vector3.dot(Vector3): return float\ncalculates the dot- or scalar-product with the other vector"
 
 #define DOC_VECTOR3CROSS "Vector3.cross(Vector3): return float\ncalculates the cross- or vector-product"
+
+#define DOC_VECTOR3LENGTH "Vector3.length(): return float\nreturns the euclidic length of the vector."
+
+#define DOC_VECTOR3LENGTHSQUARED "Vector3.length_squared(): return float\nreturns the squared euclidic length of the vector."
+
+#define DOC_VECTOR3NORMALIZE "Vector3.normalize(): return Vector3\nreturns a vector with the same direction but length 1."
+
+#define DOC_VECTOR3NORMALIZEIP "Vector3.normalize(): return None\nnormalizes the vector in place so that its length is 1."
+
+#define DOC_VECTOR3ISNORMALIZED "Vector3.is_normalized(): return Bool\ntests if the vector is normalized i.e. has length == 1."
+
+#define DOC_VECTOR3SCALETOLENGTH "Vector3.scale_to_length(float): return None\nscales the vector to a given length."
+
+#define DOC_VECTOR3REFLECT "Vector3.reflect(Vector3): return Vector3\nreturns a vector reflected of a given normal."
+
+#define DOC_VECTOR3REFLECTIP "Vector3.reflect(Vector3): return None\nreflect the vector of a given normal in place."
+
+#define DOC_VECTOR3DISTANCETO "Vector3.distance_to(Vector3): return float\ncalculates the euclidic distance to a given vector."
+
+#define DOC_VECTOR3DISTANCESQUAREDTO "Vector3.distance_squared_to(Vector3): return float\ncalculates the squared euclidic distance to a given vector."
+
+#define DOC_VECTOR3LERP "Vector3.lerp(Vector3): return Vector3\nreturns a linear interpolation to the given vector."
+
+#define DOC_VECTOR3SLERP "Vector3.slerp(Vector3): return Vector3\nreturns a spherical interpolation to the given vector."
+
+#define DOC_VECTOR3ELEMENTWISE "Vector3.elementwise(): return VectorElementwizeProxy\nThe next operation will be performed elementwize."
+
+#define DOC_VECTOR3ROTATE "Vector3.rotate(Vector3, float): return Vector3\nrotates a vector by a given angle in degrees."
+
+#define DOC_VECTOR3ROTATEIP "Vector3.rotate_ip(Vector3, float): return None\nrotates the vector by a given angle in degrees in place."
+
+#define DOC_VECTOR3ROTATEX "Vector3.rotate_x(float): return Vector3\nrotates a vector around the x-axis by the angle in degrees."
+
+#define DOC_VECTOR3ROTATEXIP "Vector3.rotate_x_ip(float): return None\nrotates the vector around the x-axis by the angle in degrees in place."
+
+#define DOC_VECTOR3ROTATEY "Vector3.rotate_y(float): return Vector3\nrotates a vector around the y-axis by the angle in degrees."
+
+#define DOC_VECTOR3ROTATEYIP "Vector3.rotate_y_ip(float): return None\nrotates the vector around the y-axis by the angle in degrees in place."
+
+#define DOC_VECTOR3ROTATEZ "Vector3.rotate_z(float): return Vector3\nrotates a vector around the z-axis by the angle in degrees."
+
+#define DOC_VECTOR3ROTATEZIP "Vector3.rotate_z_ip(float): return None\nrotates the vector around the z-axis by the angle in degrees in place."
+
+#define DOC_VECTOR3ANGLETO "Vector3.angle_to(Vector3): return float\ncalculates the angle to a given vector in degrees."
+
+#define DOC_VECTOR3ASSPHERICAL "Vector3.as_spherical(): return (r, theta, phi)\nreturns a tuple with radial distance, inclination and azimuthal angle."
+
+#define DOC_VECTOR3FROMSPHERICAL "Vector3.from_spherical((r, theta, phi)): return None\nSets x, y and z from a spherical coordinates 3-tuple."
 
 #define DOC_ ""
 
@@ -26,6 +112,18 @@
 
 pygame.math
  pygame module for vector classes
+
+
+
+pygame.math.enable_swizzling
+ math.enable_swizzling(): return None
+globally enables swizzling for vectors.
+
+
+
+pygame.math.diable_swizzling
+ math.disable_swizzling(): return None
+globally disables swizzling for vectors.
 
 
 
@@ -50,8 +148,111 @@ calculates the cross- or vector-product
 
 
 
+Vector2.length
+ Vector2.length(): return float
+returns the euclidic length of the vector.
 
- 
+
+
+Vector2.length_squared
+ Vector2.length_squared(): return float
+returns the squared euclidic length of the vector.
+
+
+
+Vector2.normalize
+ Vector2.normalize(): return Vector2
+returns a vector with the same direction but length 1.
+
+
+
+Vector2.normalize_ip
+ Vector2.normalize(): return None
+normalizes the vector in place so that its length is 1.
+
+
+
+Vector2.is_normalized
+ Vector2.is_normalized(): return Bool
+tests if the vector is normalized i.e. has length == 1.
+
+
+
+Vector2.scale_to_length
+ Vector2.scale_to_length(float): return None
+scales the vector to a given length.
+
+
+
+Vector2.reflect
+ Vector2.reflect(Vector2): return Vector2
+returns a vector reflected of a given normal.
+
+
+
+Vector2.reflect_ip
+ Vector2.reflect(Vector2): return None
+reflect the vector of a given normal in place.
+
+
+
+Vector2.distance_to
+ Vector2.distance_to(Vector2): return float
+calculates the euclidic distance to a given vector.
+
+
+
+Vector2.distance_squared_to
+ Vector2.distance_squared_to(Vector2): return float
+calculates the squared euclidic distance to a given vector.
+
+
+
+Vector2.lerp
+ Vector2.lerp(Vector2): return Vector2
+returns a linear interpolation to the given vector.
+
+
+
+Vector2.slerp
+ Vector2.slerp(Vector2): return Vector2
+returns a spherical interpolation to the given vector.
+
+
+
+Vector2.elementwise
+ Vector2.elementwise(): return VectorElementwizeProxy
+The next operation will be performed elementwize.
+
+
+
+Vector2.rotate
+ Vector2.rotate(float): return Vector2
+rotates a vector by a given angle in degrees.
+
+
+
+Vector2.rotate_ip
+ Vector2.rotate_ip(float): return None
+rotates the vector by a given angle in degrees in place.
+
+
+
+Vector2.angle_to
+ Vector2.angle_to(Vector2): return float
+calculates the angle to a given vector in degrees.
+
+
+
+Vector2.as_polar
+ Vector2.as_polar(): return (r, phi)
+returns a tuple with radial distance and azimuthal angle.
+
+
+
+Vector2.from_polar
+ Vector2.from_polar((r, phi)): return None
+Sets x and y from a polar coordinates tuple.
 
 
 
@@ -73,6 +274,150 @@ calculates the dot- or scalar-product with the other vector
 Vector3.cross
  Vector3.cross(Vector3): return float
 calculates the cross- or vector-product
+
+
+
+Vector3.length
+ Vector3.length(): return float
+returns the euclidic length of the vector.
+
+
+
+Vector3.length_squared
+ Vector3.length_squared(): return float
+returns the squared euclidic length of the vector.
+
+
+
+Vector3.normalize
+ Vector3.normalize(): return Vector3
+returns a vector with the same direction but length 1.
+
+
+
+Vector3.normalize_ip
+ Vector3.normalize(): return None
+normalizes the vector in place so that its length is 1.
+
+
+
+Vector3.is_normalized
+ Vector3.is_normalized(): return Bool
+tests if the vector is normalized i.e. has length == 1.
+
+
+
+Vector3.scale_to_length
+ Vector3.scale_to_length(float): return None
+scales the vector to a given length.
+
+
+
+Vector3.reflect
+ Vector3.reflect(Vector3): return Vector3
+returns a vector reflected of a given normal.
+
+
+
+Vector3.reflect_ip
+ Vector3.reflect(Vector3): return None
+reflect the vector of a given normal in place.
+
+
+
+Vector3.distance_to
+ Vector3.distance_to(Vector3): return float
+calculates the euclidic distance to a given vector.
+
+
+
+Vector3.distance_squared_to
+ Vector3.distance_squared_to(Vector3): return float
+calculates the squared euclidic distance to a given vector.
+
+
+
+Vector3.lerp
+ Vector3.lerp(Vector3): return Vector3
+returns a linear interpolation to the given vector.
+
+
+
+Vector3.slerp
+ Vector3.slerp(Vector3): return Vector3
+returns a spherical interpolation to the given vector.
+
+
+
+Vector3.elementwise
+ Vector3.elementwise(): return VectorElementwizeProxy
+The next operation will be performed elementwize.
+
+
+
+Vector3.rotate
+ Vector3.rotate(Vector3, float): return Vector3
+rotates a vector by a given angle in degrees.
+
+
+
+Vector3.rotate_ip
+ Vector3.rotate_ip(Vector3, float): return None
+rotates the vector by a given angle in degrees in place.
+
+
+
+Vector3.rotate_x
+ Vector3.rotate_x(float): return Vector3
+rotates a vector around the x-axis by the angle in degrees.
+
+
+
+Vector3.rotate_x_ip
+ Vector3.rotate_x_ip(float): return None
+rotates the vector around the x-axis by the angle in degrees in place.
+
+
+
+Vector3.rotate_y
+ Vector3.rotate_y(float): return Vector3
+rotates a vector around the y-axis by the angle in degrees.
+
+
+
+Vector3.rotate_y_ip
+ Vector3.rotate_y_ip(float): return None
+rotates the vector around the y-axis by the angle in degrees in place.
+
+
+
+Vector3.rotate_z
+ Vector3.rotate_z(float): return Vector3
+rotates a vector around the z-axis by the angle in degrees.
+
+
+
+Vector3.rotate_z_ip
+ Vector3.rotate_z_ip(float): return None
+rotates the vector around the z-axis by the angle in degrees in place.
+
+
+
+Vector3.angle_to
+ Vector3.angle_to(Vector3): return float
+calculates the angle to a given vector in degrees.
+
+
+
+Vector3.as_spherical
+ Vector3.as_spherical(): return (r, theta, phi)
+returns a tuple with radial distance, inclination and azimuthal angle.
+
+
+
+Vector3.from_spherical
+ Vector3.from_spherical((r, theta, phi)): return None
+Sets x, y and z from a spherical coordinates 3-tuple.
 
 
 
