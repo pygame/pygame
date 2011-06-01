654a863da37925740d3792e3aa1af36ecfb31372
Lenard Lindstrom <len-l@telus.net>
2011-06-01 17:24:18 +0000

    Change to Sphinx conf.py file to try and keep generated html consistent. Note: for now should use Sphinx 1.0.7

--- docs/reST/conf.py
+++ docs/reST/conf.py
@@ -108,7 +108,7 @@ html_theme_path = ['themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "%s v%s documentation" % (project, version)
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
