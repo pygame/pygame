b4e1821d8d3e0abf7256120033353ba577412911
Lenard Lindstrom <len-l@telus.net>
2011-07-21 17:12:40 +0000

    pixelcopy: completely disable unit tests until the problem with the automated build page is resolved.

---
+++ test/pixelcopy_tags.py
@@ -0,0 +1,5 @@
+# Problems with unit tests on 'The Spectacularly Adequate Automated Pygame
+# Build Page' for Pythons 2.6 and 3.1. Remove this file when the problem has
+# been resolved.
+__tags__ = ['development', 'ignore', 'subprocess_ignore']
+
