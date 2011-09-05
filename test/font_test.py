30074cb8e3f562780c2e6f8735a350de1df74437
René Dudfield <renesd@gmail.com>
2011-09-05 18:58:07 +0200

    Font.render can set backgound to None. Fixes #49. Thanks John and Marco Chieppa!

--- test/font_test.py
+++ test/font_test.py
@@ -157,6 +157,55 @@ class FontModuleTest( unittest.TestCase ):
         pygame_font.quit()
 
 
+
+
+
+class FontTest(unittest.TestCase):
+    def setUp(self):
+        pygame_font.init()
+
+    def tearDown(self):
+        pygame_font.quit()
+
+
+    def test_render_args(self):
+        screen = pygame.display.set_mode((600, 400))
+        rect = screen.get_rect()
+        f = pygame.font.Font(None, 20)
+        screen.fill((10, 10, 10))
+        font_surface = f.render("   bar", True, (0, 0, 0), (255, 255, 255))
+        font_rect = font_surface.get_rect()
+        font_rect.topleft = rect.topleft
+        self.assertTrue(font_surface)
+        screen.blit(font_surface, font_rect, font_rect)
+        pygame.display.update()
+        self.assertEqual(tuple(screen.get_at((0,0)))[:3], (255, 255, 255))        
+        self.assertEqual(tuple(screen.get_at(font_rect.topleft))[:3], (255, 255, 255))        
+        screen.fill((10, 10, 10))
+        font_surface = f.render("   bar", True, (0, 0, 0), None)
+        font_rect = font_surface.get_rect()
+        font_rect.topleft = rect.topleft
+        self.assertTrue(font_surface)
+        screen.blit(font_surface, font_rect, font_rect)
+        pygame.display.update()
+        self.assertEqual(tuple(screen.get_at((0,0)))[:3], (10, 10, 10))
+        self.assertEqual(tuple(screen.get_at(font_rect.topleft))[:3], (10, 10, 10))        
+        screen.fill((10, 10, 10))
+        font_surface = f.render("   bar", True, (0, 0, 0))
+        font_rect = font_surface.get_rect()
+        font_rect.topleft = rect.topleft
+        self.assertTrue(font_surface)
+        screen.blit(font_surface, font_rect, font_rect)
+        pygame.display.update(rect)
+        self.assertEqual(tuple(screen.get_at((0,0)))[:3], (10, 10, 10))
+        self.assertEqual(tuple(screen.get_at(font_rect.topleft))[:3], (10, 10, 10))
+
+
+
+
+
+
+
 class FontTypeTest( unittest.TestCase ):
     def setUp(self):
         pygame_font.init()
