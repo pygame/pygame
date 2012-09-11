e59d47c46c504aeebcf11b59205051f6fbfab1ec
jmm0 <jmm0@localhost>
2012-09-11 07:47:06 -0500

    fix issue 130

--- test/sprite_test.py
+++ test/sprite_test.py
@@ -910,6 +910,21 @@ class LayeredDirtyTypeTest__DirtySprite(LayeredGroupBase, unittest.TestCase):
     def setUp(self):
         self.LG = sprite.LayeredDirty()
 
+    def test_repaint_rect(self):
+        group = self.LG
+        surface = pygame.Surface((100, 100))
+
+        group.repaint_rect(pygame.Rect(0, 0, 100, 100))
+        group.draw(surface)
+
+    def test_repaint_rect_with_clip(self):
+        group = self.LG
+        surface = pygame.Surface((100, 100))
+
+        group.set_clip(pygame.Rect(0, 0, 100, 100))
+        group.repaint_rect(pygame.Rect(0, 0, 100, 100))
+        group.draw(surface)
+
 ############################### SPRITE BASE CLASS ##############################
 #
 # tests common between sprite classes
