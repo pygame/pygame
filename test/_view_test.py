8d8e0e85ae7c140f3dbe9585088cee64a1b91faa
Lenard Lindstrom <len-l@telus.net>
2012-11-25 20:35:18 -0800

    Port pygame.bufferproxy.BufferProxy properties to pygame._view.View. Enable View subclassing.
    For backward compatibility, add __dict__, raw, and length properties to
    pygame._view.View. Since the BufferProxy dictionary (__dict__) is for use by
    BufferProxy subclass instances, also allow View to be subclassed.

--- test/_view_test.py
+++ test/_view_test.py
@@ -2,6 +2,7 @@ import sys
 import re
 import weakref
 import gc
+import ctypes
 if __name__ == '__main__':
     import os
     pkg_dir = os.path.split(os.path.abspath(__file__))[0]
@@ -19,6 +20,7 @@ else:
     from test.test_utils import test_not_implemented, unittest
 import pygame
 from pygame._view import View
+from pygame.compat import as_bytes
 
 class ViewTest(unittest.TestCase):
     view_keywords = {'shape': (5, 4, 3),
@@ -193,6 +195,51 @@ class ViewTest(unittest.TestCase):
         r = repr(v)
         self.assertEqual(r[:2], '*<')
         self.assertEqual(r[-2:], '>*')
+
+class BufferProxyLegacyTest(unittest.TestCase):
+    content = as_bytes('\x01\x00\x00\x02') * 12
+    buffer = ctypes.create_string_buffer(content)
+    data = (ctypes.addressof(buffer), True)
+
+    def test_length(self):
+
+        # __doc__ (as of 2008-08-02) for pygame.bufferproxy.BufferProxy.length:
+
+          # The size of the buffer data in bytes.
+        bf = View(shape=(3, 4),
+                  typestr='|u4',
+                  data=self.data,
+                  strides=(12, 4))
+        self.assertEqual(bf.length, len(self.content))
+        bf = View(shape=(3, 3),
+                  typestr='|u4',
+                  data=self.data,
+                  strides=(12, 4))
+        self.assertEqual(bf.length, 3*3*4)
+
+    def todo_test_raw(self):
+
+        # __doc__ (as of 2008-08-02) for pygame.bufferproxy.BufferProxy.raw:
+
+          # The raw buffer data as string. The string may contain NUL bytes.
+
+        self.fail() 
+
+    def todo_test_write(self):
+
+        # __doc__ (as of 2008-08-02) for pygame.bufferproxy.BufferProxy.write:
+
+          # B.write (bufferproxy, buffer, offset) -> None
+          # 
+          # Writes raw data to the bufferproxy.
+          # 
+          # Writes the raw data from buffer to the BufferProxy object, starting
+          # at the specified offset within the BufferProxy.
+          # If the length of the passed buffer exceeds the length of the
+          # BufferProxy (reduced by the offset), an IndexError will be raised.
+
+        self.fail() 
+        
 
 if __name__ == '__main__':
     unittest.main()
