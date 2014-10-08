22f2c416150acf3334fd1e534d6e60356ff3c4f7
endolith <endolith@gmail.com>
2014-10-07 23:07:05 -0400

    use default parameter for note-off instead of None

--- lib/midi.py
+++ lib/midi.py
@@ -534,16 +534,17 @@ class Output(object):
 
         self.write_short(0x90+channel, note, velocity)
 
-    def note_off(self, note, velocity=None, channel = 0):
+    def note_off(self, note, velocity=0, channel=0):
         """turns a midi note off.  Note must be on.
-        Output.note_off(note, velocity=None, channel = 0)
+        Output.note_off(note, velocity=0, channel=0)
+
+        note is an integer from 0 to 127
+        velocity is an integer from 0 to 127 (release velocity)
+        channel is an integer from 0 to 15
 
         Turn a note off in the output stream.  The note must already
         be on for this to work correctly.
         """
-        if velocity is None:
-            velocity = 0
-
         if not (0 <= channel <= 15):
             raise ValueError("Channel not between 0 and 15.")
 
