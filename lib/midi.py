3df732d16884e71ce07701f92e6a2588f65eb2f9
endolith <endolith@gmail.com>
2014-10-07 23:18:29 -0400

    add pitch bend command

--- lib/midi.py
+++ lib/midi.py
@@ -566,6 +566,33 @@ class Output(object):
 
         self.write_short(0xc0 + channel, instrument_id)
 
+    def pitch_bend(self, value=0, channel=0):
+        """modify the pitch of a channel.
+        Output.pitch_bend(value=0, channel=0)
+
+        Adjust the pitch of a channel.  The value is a signed integer
+        from -8192 to +8191.  For example, 0 means "no change", +4096 is
+        typically a semitone higher, and -8192 is 1 whole tone lower (though
+        the musical range corresponding to the pitch bend range can also be
+        changed in some synthesizers).
+
+        If no value is given, the pitch bend is returned to "no change".
+        """
+        if not (0 <= channel <= 15):
+            raise ValueError("Channel not between 0 and 15.")
+
+        if not (-8192 <= value <= 8191):
+            raise ValueError("Pitch bend value must be between "
+                             "-8192 and +8191, not %d." % value)
+
+        # "The 14 bit value of the pitch bend is defined so that a value of
+        # 0x2000 is the center corresponding to the normal pitch of the note
+        # (no pitch change)." so value=0 should send 0x2000
+        value = value + 0x2000
+        LSB = value & 0x7f  # keep least 7 bits
+        MSB = value >> 7
+        self.write_short(0xe0 + channel, LSB, MSB)
+
 
 
 def time():
