b4ee763df37861dfef6c7ecb75d16b163bad555d
Paul V Craven <paul@cravenfamily.com>
2014-12-14 15:08:44 -0600

    Fix issue 226.

--- test/font_test.py
+++ test/font_test.py
@@ -181,24 +181,29 @@ class FontTest(unittest.TestCase):
         pygame.display.update()
         self.assertEqual(tuple(screen.get_at((0,0)))[:3], (255, 255, 255))        
         self.assertEqual(tuple(screen.get_at(font_rect.topleft))[:3], (255, 255, 255))        
-        screen.fill((10, 10, 10))
-        font_surface = f.render("   bar", True, (0, 0, 0), None)
-        font_rect = font_surface.get_rect()
-        font_rect.topleft = rect.topleft
-        self.assertTrue(font_surface)
-        screen.blit(font_surface, font_rect, font_rect)
-        pygame.display.update()
-        self.assertEqual(tuple(screen.get_at((0,0)))[:3], (10, 10, 10))
-        self.assertEqual(tuple(screen.get_at(font_rect.topleft))[:3], (10, 10, 10))        
-        screen.fill((10, 10, 10))
-        font_surface = f.render("   bar", True, (0, 0, 0))
-        font_rect = font_surface.get_rect()
-        font_rect.topleft = rect.topleft
-        self.assertTrue(font_surface)
-        screen.blit(font_surface, font_rect, font_rect)
-        pygame.display.update(rect)
-        self.assertEqual(tuple(screen.get_at((0,0)))[:3], (10, 10, 10))
-        self.assertEqual(tuple(screen.get_at(font_rect.topleft))[:3], (10, 10, 10))
+        
+        # If we don't have a real display, don't do this test.
+        # Transparent background doesn't seem to work without a read video card.
+        if not('SDL_VIDEODRIVER' in os.environ and os.environ['SDL_VIDEODRIVER'] == 'dummy'):        
+            screen.fill((10, 10, 10))
+            font_surface = f.render("   bar", True, (0, 0, 0), None)
+            font_rect = font_surface.get_rect()
+            font_rect.topleft = rect.topleft
+            self.assertTrue(font_surface)
+            screen.blit(font_surface, font_rect, font_rect)
+            pygame.display.update()
+            self.assertEqual(tuple(screen.get_at((0,0)))[:3], (10, 10, 10))
+            self.assertEqual(tuple(screen.get_at(font_rect.topleft))[:3], (10, 10, 10))        
+            
+            screen.fill((10, 10, 10))
+            font_surface = f.render("   bar", True, (0, 0, 0))
+            font_rect = font_surface.get_rect()
+            font_rect.topleft = rect.topleft
+            self.assertTrue(font_surface)
+            screen.blit(font_surface, font_rect, font_rect)
+            pygame.display.update(rect)
+            self.assertEqual(tuple(screen.get_at((0,0)))[:3], (10, 10, 10))
+            self.assertEqual(tuple(screen.get_at(font_rect.topleft))[:3], (10, 10, 10))
 
 
 
