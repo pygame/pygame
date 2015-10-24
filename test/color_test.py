a3364b9ce2224b246507da2e5d1a6fd13fd6b8f6
Lenard Lindstrom <len-l@telus.net>
2015-10-24 16:33:30 -0700

    Close issue #269
    PyColor OverflowError on HSVA with hue value of 360
    
    Since hue is an angle in degrees, 360? is equivalent to 0?.

--- test/color_test.py
+++ test/color_test.py
@@ -682,6 +682,27 @@ class ColorTypeTest (unittest.TestCase):
             self.assert_( -0.5 <= i2 <= 0.5)
             self.assert_( -0.5 <= i3 <= 0.5)
 
+    def test_issue_269 (self):
+        """PyColor OverflowError on HSVA with hue value of 360
+
+           >>> c = pygame.Color(0)
+           >>> c.hsva = (360,0,0,0)
+           Traceback (most recent call last):
+             File "<stdin>", line 1, in <module>
+           OverflowError: this is not allowed to happen ever
+           >>> pygame.ver
+           '1.9.1release'
+           >>>
+
+        """
+
+        c = pygame.Color(0)
+        c.hsva = 360, 0, 0, 0
+        self.assertEqual(c.hsva, (0, 0, 0, 0))
+        c.hsva = 360, 100, 100, 100
+        self.assertEqual(c.hsva, (0, 100, 100, 100))
+        self.assertEqual(c, (255, 0, 0, 255))
+
 ####################### COLORSPACE PROPERTY SANITY TESTS #######################
 
     def colorspaces_converted_should_not_raise (self, prop):
