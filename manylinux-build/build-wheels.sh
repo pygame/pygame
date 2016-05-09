b97a07b93abcf36d996574e2893be1019ebf1138
Thomas Kluyver <thomas@kluyver.me.uk>
2016-05-09 20:33:01 +0100

    Build and test wheels in docker

--- manylinux-build/build-wheels.sh
+++ manylinux-build/build-wheels.sh
@@ -10,8 +10,11 @@ yum install -y SDL-devel libpng-devel libjpeg-devel libX11-devel freetype-devel 
 
 SUPPORTED_PYTHONS="cp27-cp27mu cp34-cp34m cp35-cp35m"
 
+export PORTMIDI_INC_PORTTIME=1
+
 # Compile wheels
 for PYVER in $SUPPORTED_PYTHONS; do
+    rm -rf /io/Setup /io/build/
     PYBIN="/opt/python/${PYVER}/bin"
     ${PYBIN}/pip wheel /io/ -w wheelhouse/
 done
@@ -21,9 +24,13 @@ for whl in wheelhouse/*.whl; do
     auditwheel repair $whl -w /io/manylinux-build/wheelhouse/
 done
 
+# Dummy options for headless testing
+export SDL_AUDIODRIVER=disk
+export SDL_VIDEODRIVER=dummy
+
 # Install packages and test
 for PYVER in $SUPPORTED_PYTHONS; do
     PYBIN="/opt/python/${PYVER}/bin"
     ${PYBIN}/pip install pygame --no-index -f /io/manylinux-build/wheelhouse
-    (cd $HOME; ${PYBIN}/nosetests pymanylinuxdemo)
+    (cd $HOME; ${PYBIN}/python -m pygame.tests)
 done
