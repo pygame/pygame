7913a8512260d3b99f2bb9c6f413c5e919de9a2a
Thomas Kluyver <takowl@gmail.com>
2016-06-01 18:40:33 +0100

    Download and build SDL libraries manually, to have recent versions

---
+++ manylinux-build/install-sdl-libs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e -x
+
+yum install -y libpng-devel libjpeg-devel libX11-devel freetype-devel \
+                libogg-devel libvorbis-devel alsa-lib-devel mesa-libGLU-devel audiofile-devel
+
+SDL="SDL-1.2.15"
+IMG="SDL_image-1.2.12"
+TTF="SDL_ttf-2.0.11"
+MIX="SDL_mixer-1.2.12"
+
+# Download
+curl -sL https://www.libsdl.org/release/${SDL}.tar.gz > ${SDL}.tar.gz
+curl -sL https://www.libsdl.org/projects/SDL_image/release/${IMG}.tar.gz > ${IMG}.tar.gz
+curl -sL https://www.libsdl.org/projects/SDL_ttf/release/${TTF}.tar.gz > ${TTF}.tar.gz
+curl -sL https://www.libsdl.org/projects/SDL_mixer/release/${MIX}.tar.gz > ${MIX}.tar.gz
+sha512sum -c /io/manylinux-build/sdl.sha512
+
+# Build SDL
+tar xzf ${SDL}.tar.gz
+cd $SDL
+./configure
+make
+make install
+cd ..
+
+# Build SDL_image
+tar xzf ${IMG}.tar.gz
+cd $IMG
+./configure
+make
+make install
+cd ..
+
+# Build SDL_ttf
+tar xzf ${TTF}.tar.gz
+cd $TTF
+./configure
+make
+make install
+cd ..
+
+# Build SDL_mixer
+tar xzf ${MIX}.tar.gz
+cd $MIX
+./configure
+make
+make install
+cd ..
