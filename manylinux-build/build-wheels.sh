5b57274d13caefa7483adde9e0964eb640003875
Thomas Kluyver <thomas@kluyver.me.uk>
2016-06-01 18:40:33 +0100

    Download and build SDL libraries manually, to have recent versions

--- manylinux-build/build-wheels.sh
+++ manylinux-build/build-wheels.sh
@@ -2,11 +2,8 @@
 set -e -x
 
 bash /io/manylinux-build/enable-repoforge.sh
+bash /io/manylinux-build/install-sdl-libs.sh
 bash /io/manylinux-build/install-portmidi.sh
-
-# Install a system package required by our library
-yum install -y SDL-devel libpng-devel libjpeg-devel libX11-devel freetype-devel \
-                SDL_ttf-devel SDL_image-devel SDL_mixer-devel
 
 SUPPORTED_PYTHONS="cp27-cp27mu cp34-cp34m cp35-cp35m"
 
