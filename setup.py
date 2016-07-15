821e5e84482b93e7d29dea2235440b8a7273ef9e
Thomas Kluyver <takowl@gmail.com>
2016-07-15 21:23:09 +0100

    Set version number to 1.9.2b1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ EXTRAS = {}
 
 METADATA = {
     "name":             "pygame",
-    "version":          "1.9.2.dev1",
+    "version":          "1.9.2b1",
     "license":          "LGPL",
     "url":              "http://www.pygame.org",
     "author":           "Pete Shinners, Rene Dudfield, Marcus von Appen, Bob Pendleton, others...",
