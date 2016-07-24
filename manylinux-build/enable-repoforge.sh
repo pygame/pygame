67d8e1b3f4e366d70dad3a30db944e79b1d795e4
Thomas Kluyver <thomas@kluyver.me.uk>
2016-07-24 11:08:56 +0100

    Use mirror for repoforge

--- manylinux-build/enable-repoforge.sh
+++ manylinux-build/enable-repoforge.sh
@@ -5,9 +5,12 @@ rpm --import /io/manylinux-build/RPM-GPG-KEY.dag.txt
 
 if [ "$(uname -i)" = "x86_64" ]; then
     RPMFORGE_FILE="rpmforge-release-0.5.3-1.el5.rf.x86_64.rpm"
+    RPMFORGE_URL="https://repoforge.cu.be/redhat/el5/en/x86_64/dag/RPMS/${RPMFORGE_FILE}"
 else
     RPMFORGE_FILE="rpmforge-release-0.5.3-1.el5.rf.i386.rpm"
+    RPMFORGE_URL="https://repoforge.cu.be/redhat/el5/en/i386/dag/RPMS/${RPMFORGE_FILE}"
 fi
 
-wget http://pkgs.repoforge.org/rpmforge-release/${RPMFORGE_FILE}
+# wget http://pkgs.repoforge.org/rpmforge-release/${RPMFORGE_FILE}
+wget ${RPMFORGE_URL}
 rpm -i ${RPMFORGE_FILE}
