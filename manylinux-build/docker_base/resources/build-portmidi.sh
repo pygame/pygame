ac4d90a0eefa99120c62796e85b997c069c37dae
Thomas Kluyver <takowl@gmail.com>
2016-07-24 12:25:35 +0100

    Prepare our own base docker images for building manylinux wheels

--- manylinux-build/docker_base/resources/build-portmidi.sh
+++ manylinux-build/docker_base/resources/build-portmidi.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e -x
 
-yum install -y cmake alsa-lib-devel java-1.7.0-openjdk-devel jpackage-utils
+cd /pygame_base_build/
 
 SRC_ZIP="portmidi-src-217.zip"
 
 curl -sL http://sourceforge.net/projects/portmedia/files/portmidi/217/${SRC_ZIP}/download > ${SRC_ZIP}
-sha512sum -c /io/manylinux-build/portmidi.sha512
+sha512sum -c portmidi.sha512
 unzip $SRC_ZIP
 
 if [ "$(uname -i)" = "x86_64" ]; then
