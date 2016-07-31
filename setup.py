f6a488ae102a90de809e6c5df42d6ca8085a1e84
René Dudfield <renesd@gmail.com>
2016-07-31 11:25:43 +0200

    1.9.2b7

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ EXTRAS = {}
 
 METADATA = {
     "name":             "pygame",
-    "version":          "1.9.2b6",
+    "version":          "1.9.2b7",
     "license":          "LGPL",
     "url":              "http://www.pygame.org",
     "author":           "Pete Shinners, Rene Dudfield, Marcus von Appen, Bob Pendleton, others...",
