def5410613e801c37c0edce5d9a83090692ed476
René Dudfield <renesd@gmail.com>
2016-08-01 15:54:21 +0200

    Fix portmidi and fluidsynth sourceforge download links.

--- manylinux-build/docker_base/fluidsynth/build-fluidsynth.sh
+++ manylinux-build/docker_base/fluidsynth/build-fluidsynth.sh
@@ -5,7 +5,7 @@ cd $(dirname `readlink -f "$0"`)
 
 FSYNTH="fluidsynth-1.1.6"
 
-curl -sL https://sourceforge.net/projects/fluidsynth/files/${FSYNTH}/${FSYNTH}.tar.gz/download > ${FSYNTH}.tar.gz
+curl -sL https://downloads.sourceforge.net/project/fluidsynth/${FSYNTH}/${FSYNTH}.tar.gz > ${FSYNTH}.tar.gz
 sha512sum -c fluidsynth.sha512
 tar xzf ${FSYNTH}.tar.gz
 
