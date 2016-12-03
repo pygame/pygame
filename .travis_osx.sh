df4f610ad15d3fbe6b6407263c1f699b1e616fc7
Thomas Kluyver <thomas@kluyver.me.uk>
2016-12-03 16:33:06 +0000

    Changes from experimental OSX wheel building branch

--- .travis_osx.sh
+++ .travis_osx.sh
@@ -4,8 +4,7 @@ set -e
 git clone https://github.com/MacPython/terryfy.git
 cd terryfy
 # Work with a specific commit
-git checkout c9800884ae1171bfc72b2737974f5e8dec28479d
+git checkout 63fab201fab1f42ad213221be384fdd52ddf6561
 cd ..
 source terryfy/travis_tools.sh
 get_python_environment homebrew $PY_VERSION $(pwd)/_test_env
-
