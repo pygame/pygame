7921f53436abed467b4d9826c7c411a66096ab3a
Thomas Kluyver <takowl@gmail.com>
2016-12-04 19:55:28 +0000

    Integrate changes from Travis experimentation on Github

--- .travis_osx_after_success.sh
+++ .travis_osx_after_success.sh
@@ -1,4 +1,11 @@
-CXXFLAGS="-arch i386 -arch x86_64" CFLAGS="-arch i386 -arch x86_64" LDFLAGS="-arch i386 -arch x86_64" $PYTHON_EXE setup.py bdist_wheel
+if [[ ${BUILD_UNIVERSAL} == "1" ]]; then
+  export CXXFLAGS="-arch i386 -arch x86_64" CFLAGS="-arch i386 -arch x86_64" LDFLAGS="-arch i386 -arch x86_64"
+  echo "Set flags for universal build"
+  echo $CFLAGS
+fi
+
+echo "Building wheel..."
+$PYTHON_EXE setup.py bdist_wheel
 
 # This copies in the .dylib files that are linked to the .so files.
 # It also rewrites the .so file linking options. https://pypi.python.org/pypi/delocate
--- .travis_osx_before_install.sh
+++ .travis_osx_before_install.sh
@@ -1,7 +1,13 @@
 # Compiles pygame on homebrew for distribution.
 # This may not be what you want to do if not on travisci.
 
+set -e
+
+echo -en 'travis_fold:start:brew.update\\r'
 brew update
+echo -en 'travis_fold:end:brew.update\\r'
+export HOMEBREW_NO_AUTO_UPDATE=1
+
 brew unlink pkg-config
 brew install pkg-config
 brew link pkg-config
@@ -11,15 +17,17 @@ brew unlink libtiff
 # brew install ccache
 # export PATH=/usr/local/opt/ccache/libexec:$PATH
 
+if [[ ${BUILD_UNIVERSAL} == "1" ]]; then
+  UNIVERSAL_FLAG='--universal'
+else
+  UNIVERSAL_FLAG=''
+fi
+
 brew uninstall --force sdl
 brew uninstall --force sdl_image
 brew uninstall --force sdl_mixer
 brew uninstall --force sdl_ttf
 brew uninstall --force smpeg
-brew uninstall --force jpeg
-brew uninstall --force libpng
-brew uninstall --force libtiff
-brew uninstall --force webp
 brew uninstall --force flac
 brew uninstall --force fluid-synth
 brew uninstall --force smpeg
@@ -27,20 +35,20 @@ brew uninstall --force libmikmod
 brew uninstall --force libvorbis
 brew uninstall --force portmidi
 brew uninstall --force freetype
-brew install sdl --without-x --universal
-brew install jpeg --universal
-brew install libpng --universal
-brew install libtiff --universal --with-xz
-brew install webp --universal --with-libtiff --with-giflib
-brew install libvorbis --universal
-brew install libogg --universal
-brew install flac --universal --with-libogg
+brew install sdl
+brew install jpeg ${UNIVERSAL_FLAG}
+brew install libpng ${UNIVERSAL_FLAG}
+brew install libtiff ${UNIVERSAL_FLAG} --with-xz
+brew install webp ${UNIVERSAL_FLAG} --with-libtiff --with-giflib
+brew install libvorbis ${UNIVERSAL_FLAG}
+brew install libogg ${UNIVERSAL_FLAG}
+brew install flac ${UNIVERSAL_FLAG} --with-libogg
 brew install fluid-synth
-brew install libmikmod --universal
+brew install libmikmod ${UNIVERSAL_FLAG}
 brew install smpeg
-brew install portmidi --universal
-brew install freetype --universal
-brew install sdl_ttf --universal
+brew install portmidi
+brew install freetype ${UNIVERSAL_FLAG}
+brew install sdl_ttf ${UNIVERSAL_FLAG}
 
-brew install sdl_image --universal
+brew install sdl_image ${UNIVERSAL_FLAG}
 brew install sdl_mixer --with-flac --with-fluid-synth --with-libmikmod --with-libvorbis --with-smpeg
