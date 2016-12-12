325dd5fa7cbbef5d5e69978fb9279edb1f44b40c
Thomas Kluyver <thomas@kluyver.me.uk>
2016-12-12 20:27:02 +0000

    Update terryfy commit used

--- .travis_osx.sh
+++ .travis_osx.sh
@@ -4,7 +4,7 @@ set -e
 git clone https://github.com/MacPython/terryfy.git
 cd terryfy
 # Work with a specific commit
-git checkout 63fab201fab1f42ad213221be384fdd52ddf6561
+git checkout b48eff2ea5f194d404e88235cec7f270a8e5f24f
 cd ..
 source terryfy/travis_tools.sh
 get_python_environment homebrew $PY_VERSION $(pwd)/_test_env
