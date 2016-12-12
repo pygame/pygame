dd0d883979d17c2994b8b74fd6d12ea861d07c70
Thomas Kluyver <takowl@gmail.com>
2016-12-12 13:01:24 +0000

    Back to development

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ EXTRAS = {}
 
 METADATA = {
     "name":             "pygame",
-    "version":          "1.9.2",
+    "version":          "1.9.3.dev0",
     "license":          "LGPL",
     "url":              "http://www.pygame.org",
     "author":           "Pete Shinners, Rene Dudfield, Marcus von Appen, Bob Pendleton, others...",
