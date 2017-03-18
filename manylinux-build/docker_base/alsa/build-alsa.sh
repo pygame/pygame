8afe37b5858fcbdb24571513acb217bd6484fbcb
Thomas Kluyver <thomas@kluyver.me.uk>
2017-03-18 11:07:25 +0000

    Build alsa-lib ourselves in docker base images for Linux wheels.
    Closes issue #327

---
+++ manylinux-build/docker_base/alsa/build-alsa.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e -x
+
+cd $(dirname `readlink -f "$0"`)
+
+ALSA=alsa-lib-1.1.3
+
+curl -sL ftp://ftp.alsa-project.org/pub/lib/${ALSA}.tar.bz2 > ${ALSA}.tar.bz2
+sha512sum -c alsa.sha512
+
+tar xjf ${ALSA}.tar.bz2
+cd ${ALSA}
+./configure --with-configdir=/usr/share/alsa
+make
+make install
