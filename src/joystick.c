74fed299cf483fc0b198ebbcb206c435e8ed41ed
Lenard Lindstrom <len-l@telus.net>
2017-04-04 12:52:02 -0700

    joystick-module.patch

--- src/joystick.c
+++ src/joystick.c
@@ -164,8 +164,7 @@ joy_get_name (PyObject* self)
 {
     int joy_id = PyJoystick_AsID (self);
     JOYSTICK_INIT_CHECK ();
-    return Text_FromUTF8 (SDL_JoystickName (joy_id));
-#error integer to pointer warning above
+    return Text_FromUTF8 (SDL_JoystickNameForIndex (joy_id));
 }
 
 static PyObject*
