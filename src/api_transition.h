54ea8bf6fc30fd1301ba3746f45a73c5ebae1706
Lenard Lindstrom <len-l@telus.net>
2017-04-29 21:05:04 -0700

    Properly qualify #warning for gcc only

--- src/api_transition.h
+++ src/api_transition.h
@@ -3,10 +3,10 @@
  */
 #ifndef API_TRANSITION_H
 #define API_TRANSITION_H
-#warning Transitional include until Pygame C API is fully updated
-
 #ifdef __GNUC__
-#include "base_api_transition.h"
+#warning Transitional include until Pygame C API is fully updated
 #endif
+
+#include "base_api_transition.h"
 
 #endif
