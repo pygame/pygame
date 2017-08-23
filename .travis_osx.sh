b580cfd81ff9af8026aaa528da2ac221f023d2cb
Stuart Axon <stuaxo2@yahoo.com>
2017-08-24 00:08:58 +0100

    Update .travis_osx.sh
    Update terryfy to newer version, hopefull fixes bug where python not found on OSX.

--- .travis_osx.sh
+++ .travis_osx.sh
@@ -4,7 +4,7 @@ set -e
 git clone https://github.com/MacPython/terryfy.git
 cd terryfy
 # Work with a specific commit
-git checkout b48eff2ea5f194d404e88235cec7f270a8e5f24f
+git checkout 07480be3e0b3490495cb8a9629e55be54c3adac3
 cd ..
 source terryfy/travis_tools.sh
 get_python_environment homebrew $PY_VERSION $(pwd)/_test_env
