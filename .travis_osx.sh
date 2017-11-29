376b0e9d39399395b85add01a8cf2c120f47db25
Thomas Kluyver <thomas@kluyver.me.uk>
2017-11-29 14:51:30 +0000

    Ensure 'python' on PATH for Python 2

--- .travis_osx.sh
+++ .travis_osx.sh
@@ -13,4 +13,9 @@ git checkout 703737bd7be3a5d388146d5a95241ec2a17a4b2c
 cd ..
 source terryfy/travis_tools.sh
 
+# Ensure that 'python' is on $PATH
+if [[ "$PY_VERSION" == "2" ]]; then
+    export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+fi
+
 get_python_environment homebrew $PY_VERSION $(pwd)/_test_env
