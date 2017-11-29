4c2530bb4c088412ce0f0a865fe737b0c02ebad2
Thomas Kluyver <thomas@kluyver.me.uk>
2017-11-29 14:09:40 +0000

    Unset special RVM cd function

--- .travis_osx.sh
+++ .travis_osx.sh
@@ -2,17 +2,15 @@
 
 set -ex
 
-# Circumvent https://github.com/direnv/direnv/issues/210
-shell_session_update() { :; }
+# Work around https://github.com/travis-ci/travis-ci/issues/8703 :-@
+# Travis overrides cd to do something with Ruby. Revert to the default.
+unset -f cd
 
 git clone https://github.com/MacPython/terryfy.git
-# Work around https://github.com/travis-ci/travis-ci/issues/8703 :-@
-set +e
 cd terryfy
 # Work with a specific commit
 git checkout 703737bd7be3a5d388146d5a95241ec2a17a4b2c
 cd ..
 source terryfy/travis_tools.sh
-set -e
 
 get_python_environment homebrew $PY_VERSION $(pwd)/_test_env
