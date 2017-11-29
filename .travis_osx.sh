4a39091a96713f51e5d27a740e4781078cd9404b
Thomas Kluyver <thomas@kluyver.me.uk>
2017-11-29 14:00:39 +0000

    cd can fail on Travis, even when it doesn't actually fail

--- .travis_osx.sh
+++ .travis_osx.sh
@@ -6,9 +6,13 @@ set -ex
 shell_session_update() { :; }
 
 git clone https://github.com/MacPython/terryfy.git
+# Work around https://github.com/travis-ci/travis-ci/issues/8703 :-@
+set +e
 cd terryfy
 # Work with a specific commit
 git checkout 703737bd7be3a5d388146d5a95241ec2a17a4b2c
 cd ..
 source terryfy/travis_tools.sh
+set -e
+
 get_python_environment homebrew $PY_VERSION $(pwd)/_test_env
