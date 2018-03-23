88dfb601d3131b8f02c8d14977744b7f5a2685ed
René Dudfield <renesd@gmail.com>
2018-03-23 17:42:25 +0100

    Download pypy and pypy3 to test with.

---
+++ appveyor/download_pypy.ps1
@@ -0,0 +1,37 @@
+// TODO: a better powershell dev would:
+//         - make this function reusable.
+//         - add sha or even md5 checksum verification.
+function DownloadPyPy($which_pypy) {
+    $webclient = New-Object System.Net.WebClient
+
+    $which_pypy_zip = $which_pypy + ".zip"
+    $download_url = "https://bitbucket.org/pypy/pypy/downloads/" + $which_pypy + ".zip"
+
+    $filepath = "$env:appveyor_build_folder\" + $which_pypy + ".zip"
+
+    Write-Host "Downloading" $filepath "from" $download_url
+    $retry_attempts = 3
+    for($i=0; $i -lt $retry_attempts; $i++){
+        try {
+            $webclient.DownloadFile($download_url, $filepath)
+            break
+        }
+        Catch [Exception]{
+            Start-Sleep 1
+        }
+   }
+   Write-Host "File saved at" $filepath
+
+   & 7z x $which_pypy_zip
+   $env:path = "$env:appveyor_build_folder\$which_pypy;$env:path"
+}
+
+
+function main ($pypy_version) {
+    Write-Host "pypy_version" $pypy_version
+	if ($pypy_version) {
+	    DownloadPyPy $pypy_version
+	}
+}
+
+main $pypy_version
