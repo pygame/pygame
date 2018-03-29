f40731cf2acf3307bd0a175c2c2cb7ce67b999d2
René Dudfield <renesd@gmail.com>
2018-03-29 11:20:17 +0200

    Fix the pypy on travis mac PIP_CMD variable.

--- .travis_osx.sh
+++ .travis_osx.sh
@@ -5,18 +5,18 @@ set -e
 if [[ "$PY_VERSION" == "pypy2" ]] || [[ "$PY_VERSION" == "pypy3" ]]; then
 	brew install $PY_VERSION
 	export PYTHON_EXE=$PY_VERSION
-	export PIP_CMD=$PYTHON_EXE -m pip
+	export PIP_CMD="$PY_VERSION -m pip"
 else
 
 if [[ "$PY_VERSION" == "2" ]]; then
 	brew install python@2
 	export PYTHON_EXE=/usr/local/bin/python2
-	export PIP_CMD=$PYTHON_EXE -m pip
+	export PIP_CMD="$PYTHON_EXE -m pip"
 fi
 if [[ "$PY_VERSION" == "3" ]]; then
 	brew install python
 	export PYTHON_EXE=/usr/local/bin/python3
-	export PIP_CMD=$PYTHON_EXE -m pip
+	export PIP_CMD="$PYTHON_EXE -m pip"
 fi
 
 fi
