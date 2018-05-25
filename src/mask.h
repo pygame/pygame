c412a8facd3e97348f9d7a887fe0aa9aecd4ac2a
Lenard Lindstrom <len-l@telus.net>
2018-05-24 20:11:43 -0700

    Update src/mask.c
    Do a PEP 7 update of external pygame api identifiers. Use new IS_SDLvl/IS_SDLv2 macros.

--- src/mask.h
+++ src/mask.h
@@ -8,14 +8,14 @@
 typedef struct {
   PyObject_HEAD
   bitmask_t *mask;
-} PyMaskObject;
+} pgMaskObject;
 
-#define PyMask_AsBitmap(x) (((PyMaskObject*)x)->mask)
+#define pgMask_AsBitmap(x) (((pgMaskObject*)x)->mask)
 
 #ifndef PYGAMEAPI_MASK_INTERNAL
 
-#define PyMask_Type     (*(PyTypeObject*)PyMASK_C_API[0])
-#define PyMask_Check(x) ((x)->ob_type == &PyMask_Type)
+#define pgMask_Type     (*(PyTypeObject*)PyMASK_C_API[0])
+#define pgMask_Check(x) ((x)->ob_type == &pgMask_Type)
 
 #define import_pygame_mask() \
     _IMPORT_PYGAME_MODULE(mask, MASK, PyMASK_C_API)
