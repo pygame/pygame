8079924a62c648a9d609d6d47fd5810eee559053
e1000 <eea@terbium>
2018-08-27 14:59:11 +0200

    midi_test : rm commented docstrings "__doc__ as of 2009 ..."

--- test/midi_test.py
+++ test/midi_test.py
@@ -44,13 +44,6 @@ class MidiInputTest(unittest.TestCase):
 
     def test_poll(self):
 
-        # __doc__ (as of 2009-05-19) for pygame.midi.Input.poll:
-
-          # returns true if there's data, or false if not.
-          # Input.poll(): return Bool
-          #
-          # raises a MidiException on error.
-
         if not self.midi_input:
            self.skipTest('No midi Input device')
 
@@ -63,15 +56,6 @@ class MidiInputTest(unittest.TestCase):
         self.midi_input = None
 
     def test_read(self):
-
-        # __doc__ (as of 2009-05-19) for pygame.midi.Input.read:
-
-          # reads num_events midi events from the buffer.
-          # Input.read(num_events): return midi_event_list
-          #
-          # Reads from the Input buffer and gives back midi events.
-          # [[[status,data1,data2,data3],timestamp],
-          #  [[status,data1,data2,data3],timestamp],...]
 
         if not self.midi_input:
            self.skipTest('No midi Input device')
@@ -130,14 +114,6 @@ class MidiOutputTest(unittest.TestCase):
         """|tags: interactive|
         """
 
-        # __doc__ (as of 2009-05-19) for pygame.midi.Output.note_off:
-
-          # turns a midi note off.  Note must be on.
-          # Output.note_off(note, velocity=None, channel = 0)
-          #
-          # Turn a note off in the output stream.  The note must already
-          # be on for this to work correctly.
-
         if self.midi_output:
             out = self.midi_output
             out.note_on(5, 30, 0)
@@ -152,13 +128,6 @@ class MidiOutputTest(unittest.TestCase):
     def test_note_on(self):
         """|tags: interactive|
         """
-        # __doc__ (as of 2009-05-19) for pygame.midi.Output.note_on:
-
-          # turns a midi note on.  Note must be off.
-          # Output.note_on(note, velocity=None, channel = 0)
-          #
-          # Turn a note on in the output stream.  The note must already
-          # be off for this to work correctly.
 
         if self.midi_output:
             out = self.midi_output
@@ -172,11 +141,6 @@ class MidiOutputTest(unittest.TestCase):
             self.assertEqual(str(cm.exception), "Channel not between 0 and 15.")
 
     def test_set_instrument(self):
-
-        # __doc__ (as of 2009-05-19) for pygame.midi.Output.set_instrument:
-
-          # Select an instrument, with a value between 0 and 127.
-          # Output.set_instrument(instrument_id, channel = 0)
 
         if not self.midi_output:
            self.skipTest('No midi device')
@@ -231,22 +195,6 @@ class MidiOutputTest(unittest.TestCase):
     def test_write_short(self):
         """|tags: interactive|
         """
-        # __doc__ (as of 2009-05-19) for pygame.midi.Output.write_short:
-
-          # write_short(status <, data1><, data2>)
-          # Output.write_short(status)
-          # Output.write_short(status, data1 = 0, data2 = 0)
-          #
-          # output MIDI information of 3 bytes or less.
-          # data fields are optional
-          # status byte could be:
-          #      0xc0 = program change
-          #      0x90 = note on
-          #      etc.
-          #      data bytes are optional and assumed 0 if omitted
-          # example: note 65 on with velocity 100
-          #      write_short(0x90,65,100)
-
         if not self.midi_output:
            self.skipTest('No midi device')
 
@@ -323,53 +271,6 @@ class MidiModuleTest(unittest.TestCase):
 
     def test_get_default_input_id(self):
 
-        # __doc__ (as of 2009-05-19) for pygame.midi.get_default_input_device_id:
-
-          # gets the device number of the default input device.
-          # pygame.midi.get_default_input_device_id(): return default_id
-          #
-          #
-          # Return the default device ID or -1 if there are no devices.
-          # The result can be passed to the Input()/Ouput() class.
-          #
-          # On the PC, the user can specify a default device by
-          # setting an environment variable. For example, to use device #1.
-          #
-          #     set PM_RECOMMENDED_INPUT_DEVICE=1
-          #
-          # The user should first determine the available device ID by using
-          # the supplied application "testin" or "testout".
-          #
-          # In general, the registry is a better place for this kind of info,
-          # and with USB devices that can come and go, using integers is not
-          # very reliable for device identification. Under Windows, if
-          # PM_RECOMMENDED_OUTPUT_DEVICE (or PM_RECOMMENDED_INPUT_DEVICE) is
-          # *NOT* found in the environment, then the default device is obtained
-          # by looking for a string in the registry under:
-          #     HKEY_LOCAL_MACHINE/SOFTWARE/PortMidi/Recommended_Input_Device
-          # and HKEY_LOCAL_MACHINE/SOFTWARE/PortMidi/Recommended_Output_Device
-          # for a string. The number of the first device with a substring that
-          # matches the string exactly is returned. For example, if the string
-          # in the registry is "USB", and device 1 is named
-          # "In USB MidiSport 1x1", then that will be the default
-          # input because it contains the string "USB".
-          #
-          # In addition to the name, get_device_info() returns "interf", which
-          # is the interface name. (The "interface" is the underlying software
-          #     system or API used by PortMidi to access devices. Examples are
-          #     MMSystem, DirectX (not implemented), ALSA, OSS (not implemented), etc.)
-          #     At present, the only Win32 interface is "MMSystem", the only Linux
-          #     interface is "ALSA", and the only Max OS X interface is "CoreMIDI".
-          # To specify both the interface and the device name in the registry,
-          # separate the two with a comma and a space, e.g.:
-          #     MMSystem, In USB MidiSport 1x1
-          # In this case, the string before the comma must be a substring of
-          # the "interf" string, and the string after the space must be a
-          # substring of the "name" name string in order to match the device.
-          #
-          # Note: in the current release, the default is simply the first device
-          #     (the input or output device with the lowest PmDeviceID).
-
         midin_id = pygame.midi.get_default_input_id()
         # if there is a not None return make sure it is an int.
         self.assertIsInstance(midin_id, int)
@@ -379,53 +280,6 @@ class MidiModuleTest(unittest.TestCase):
 
     def test_get_default_output_id(self):
 
-        # __doc__ (as of 2009-05-19) for pygame.midi.get_default_output_device_id:
-
-          # get the device number of the default output device.
-          # pygame.midi.get_default_output_device_id(): return default_id
-          #
-          #
-          # Return the default device ID or -1 if there are no devices.
-          # The result can be passed to the Input()/Ouput() class.
-          #
-          # On the PC, the user can specify a default device by
-          # setting an environment variable. For example, to use device #1.
-          #
-          #     set PM_RECOMMENDED_OUTPUT_DEVICE=1
-          #
-          # The user should first determine the available device ID by using
-          # the supplied application "testin" or "testout".
-          #
-          # In general, the registry is a better place for this kind of info,
-          # and with USB devices that can come and go, using integers is not
-          # very reliable for device identification. Under Windows, if
-          # PM_RECOMMENDED_OUTPUT_DEVICE (or PM_RECOMMENDED_INPUT_DEVICE) is
-          # *NOT* found in the environment, then the default device is obtained
-          # by looking for a string in the registry under:
-          #     HKEY_LOCAL_MACHINE/SOFTWARE/PortMidi/Recommended_Input_Device
-          # and HKEY_LOCAL_MACHINE/SOFTWARE/PortMidi/Recommended_Output_Device
-          # for a string. The number of the first device with a substring that
-          # matches the string exactly is returned. For example, if the string
-          # in the registry is "USB", and device 1 is named
-          # "In USB MidiSport 1x1", then that will be the default
-          # input because it contains the string "USB".
-          #
-          # In addition to the name, get_device_info() returns "interf", which
-          # is the interface name. (The "interface" is the underlying software
-          #     system or API used by PortMidi to access devices. Examples are
-          #     MMSystem, DirectX (not implemented), ALSA, OSS (not implemented), etc.)
-          #     At present, the only Win32 interface is "MMSystem", the only Linux
-          #     interface is "ALSA", and the only Max OS X interface is "CoreMIDI".
-          # To specify both the interface and the device name in the registry,
-          # separate the two with a comma and a space, e.g.:
-          #     MMSystem, In USB MidiSport 1x1
-          # In this case, the string before the comma must be a substring of
-          # the "interf" string, and the string after the space must be a
-          # substring of the "name" name string in order to match the device.
-          #
-          # Note: in the current release, the default is simply the first device
-          #     (the input or output device with the lowest PmDeviceID).
-
         c = pygame.midi.get_default_output_id()
         self.assertIsInstance(c, int)
         self.assertTrue(c >= -1)
@@ -433,15 +287,6 @@ class MidiModuleTest(unittest.TestCase):
         self.assertRaises(RuntimeError, pygame.midi.get_default_output_id)
 
     def test_get_device_info(self):
-
-        # __doc__ (as of 2009-05-19) for pygame.midi.get_device_info:
-
-          # returns (interf, name, input, output, opened)
-          # pygame.midi.get_device_info(an_id): return (interf, name, input,
-          # output, opened)
-          #
-          #
-          # If the id is out of range, the function returns None.
 
         an_id = pygame.midi.get_default_output_id()
         if an_id != -1:
@@ -467,14 +312,6 @@ class MidiModuleTest(unittest.TestCase):
 
     def test_init(self):
 
-        # __doc__ (as of 2009-05-19) for pygame.midi.init:
-
-          # initialize the midi module
-          # pygame.midi.init(): return None
-          #
-          # Call the initialisation function before using the midi module.
-          #
-          # It is safe to call this more than once.
         pygame.midi.quit()
         self.assertRaises(RuntimeError, pygame.midi.get_count)
         # initialising many times should be fine.
@@ -484,13 +321,6 @@ class MidiModuleTest(unittest.TestCase):
         pygame.midi.init()
 
     def test_midis2events(self):
-
-        # __doc__ (as of 2009-05-19) for pygame.midi.midis2events:
-
-          # converts midi events to pygame events
-          # pygame.midi.midis2events(midis, device_id): return [Event, ...]
-          #
-          # Takes a sequence of midi events and returns list of pygame events.
 
         midi_data = ([[0xc0, 0, 1, 2], 20000],
                      [[0x90, 60, 100, 'blablabla'], 20000]
@@ -511,17 +341,6 @@ class MidiModuleTest(unittest.TestCase):
 
     def test_quit(self):
 
-        # __doc__ (as of 2009-05-19) for pygame.midi.quit:
-
-          # uninitialize the midi module
-          # pygame.midi.quit(): return None
-          #
-          #
-          # Called automatically atexit if you don't call it.
-          #
-          # It is safe to call this function more than once.
-
-
          # It is safe to call this more than once.
         pygame.midi.quit()
         pygame.midi.init()
@@ -532,11 +351,6 @@ class MidiModuleTest(unittest.TestCase):
         pygame.midi.quit()
 
     def test_time(self):
-
-        # __doc__ (as of 2009-05-19) for pygame.midi.time:
-
-          # returns the current time in ms of the PortMidi timer
-          # pygame.midi.time(): return time
 
         mtime = pygame.midi.time()
         self.assertIsInstance(mtime, int)
