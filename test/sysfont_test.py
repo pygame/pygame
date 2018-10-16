18ebf0e492df3471f18a785386eb202639b28a1e
René Dudfield <renesd@gmail.com>
2018-10-16 22:38:42 +0200

    Adding a minimal test for sysfont on mac.

--- test/sysfont_test.py
+++ test/sysfont_test.py
@@ -1,44 +1,22 @@
 import unittest
+import platform
 
 class SysfontModuleTest(unittest.TestCase):
     def todo_test_create_aliases(self):
-
-        # __doc__ (as of 2008-08-02) for pygame.sysfont.create_aliases:
-
-          #
-
         self.fail()
 
     def todo_test_initsysfonts(self):
-
-        # __doc__ (as of 2008-08-02) for pygame.sysfont.initsysfonts:
-
-          #
-
         self.fail()
 
-    def todo_test_initsysfonts_darwin(self):
-
-        # __doc__ (as of 2008-08-02) for pygame.sysfont.initsysfonts_darwin:
-
-          #
-
-        self.fail()
+    @unittest.skipIf('Darwin' not in platform.platform(), 'Not mac we skip.')
+    def test_initsysfonts_darwin(self):
+        import pygame.sysfont
+        self.assertTrue(len(pygame.sysfont.get_fonts()) > 10)
 
     def todo_test_initsysfonts_unix(self):
-
-        # __doc__ (as of 2008-08-02) for pygame.sysfont.initsysfonts_unix:
-
-          #
-
         self.fail()
 
     def todo_test_initsysfonts_win32(self):
-
-        # __doc__ (as of 2008-08-02) for pygame.sysfont.initsysfonts_win32:
-
-          #
-
         self.fail()
 
 ################################################################################
