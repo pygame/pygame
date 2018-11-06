13684a5b2ed1c28fb510728c43b24e12bea2add2
David Lönnhager <dv.lnh.d@gmail.com>
2018-11-06 23:13:03 +0100

    Renderer and Texture

--- src_py/_sdl2.py
+++ src_py/_sdl2.py
@@ -4,7 +4,8 @@ temporary bindings for the SDL2 renderer
 
 import ctypes
 import ctypes.util
-from ctypes import c_char_p, c_char, c_void_p, c_int, c_uint, c_uint32, POINTER
+from ctypes import c_char_p, c_char, c_void_p, c_int, c_uint, c_uint32, c_uint8, POINTER, Structure, c_size_t
+from ctypes import py_object, cast, POINTER, byref, c_byte
 
 
 SDL2_dll = ctypes.CDLL(ctypes.util.find_library('SDL2'))
@@ -23,11 +24,16 @@ SDL_DestroyWindow.restype = None
 
 # RENDERER
 
+SDL_RENDERER_SOFTWARE = 0x00000001
+SDL_RENDERER_ACCELERATED = 0x00000002
+SDL_RENDERER_PRESENTVSYNC = 0x00000004
+SDL_RENDERER_TARGETTEXTURE = 0x00000008
+
 SDL_CreateRenderer = SDL2_dll.SDL_CreateRenderer
 SDL_CreateRenderer.argtypes = (c_void_p, c_int, c_uint32)
 SDL_CreateRenderer.restype = c_void_p
 SDL_DestroyRenderer = SDL2_dll.SDL_DestroyRenderer
-SDL_DestroyRenderer.argtypes = c_void_p, 
+SDL_DestroyRenderer.argtypes = c_void_p,
 SDL_DestroyRenderer.restype = None
 
 SDL_RenderCopy = SDL2_dll.SDL_RenderCopy
@@ -42,11 +48,23 @@ SDL_RenderPresent = SDL2_dll.SDL_RenderPresent
 SDL_RenderPresent.argtypes = c_void_p,
 SDL_RenderPresent.restype = None
 
+SDL_SetRenderDrawColor = SDL2_dll.SDL_SetRenderDrawColor
+SDL_SetRenderDrawColor.argtypes = (c_void_p, c_uint8, c_uint8, c_uint8, c_uint8)
+SDL_SetRenderDrawColor.restype = c_int
+
+SDL_GetRenderDrawColor = SDL2_dll.SDL_GetRenderDrawColor
+SDL_GetRenderDrawColor.argtypes = (c_void_p, POINTER(c_uint8), POINTER(c_uint8), POINTER(c_uint8), POINTER(c_uint8))
+SDL_GetRenderDrawColor.restype = c_int
+
 # TEXTURE
 
 SDL_CreateTexture = SDL2_dll.SDL_CreateTexture
 SDL_CreateTexture.argtypes = (c_void_p, c_uint32, c_int, c_int, c_int)
 SDL_CreateTexture.restype = c_void_p
+
+SDL_CreateTextureFromSurface = SDL2_dll.SDL_CreateTextureFromSurface
+SDL_CreateTextureFromSurface.argtypes = (c_void_p, c_void_p)
+SDL_CreateTextureFromSurface.restype = c_void_p
 
 SDL_DestroyTexture = SDL2_dll.SDL_DestroyTexture
 SDL_DestroyTexture.argtypes = c_void_p,
@@ -55,3 +73,19 @@ SDL_DestroyTexture.restype = None
 SDL_UpdateTexture = SDL2_dll.SDL_UpdateTexture
 SDL_UpdateTexture.argtypes = (c_void_p, c_void_p, c_void_p, c_int)
 SDL_UpdateTexture.restype = c_int
+
+# SURFACE
+
+class _pgSurfaceObject(Structure):
+    _fields_ = [
+        ('HEAD', c_byte * object.__basicsize__),
+        ('surf', c_void_p), # SDL_Surface *
+        #owner
+        #subsurface
+        #weakreflist
+        #locklist
+        #dependency
+    ]
+
+def get_surface_ptr(surface):
+    return _pgSurfaceObject.from_address(id(surface)).surf
---
+++ src_py/render.py
@@ -0,0 +1,39 @@
+from ._sdl2 import *
+from pygame import Surface
+
+
+class Renderer:
+    SOFTWARE = SDL_RENDERER_SOFTWARE
+    ACCELERATED = SDL_RENDERER_ACCELERATED
+    PRESENT_VSYNC = SDL_RENDERER_PRESENTVSYNC
+    TARGET_TEXTURE = SDL_RENDERER_TARGETTEXTURE
+
+    def __init__(self, window, index=-1, flags=ACCELERATED):
+        self._renderer = SDL_CreateRenderer(window._win, index, flags)
+        self._draw_color = (255, 255, 255, 255)
+
+    def __del__(self):
+        SDL_DestroyRenderer(self._renderer)
+
+    @property
+    def draw_color(self):
+        return self._draw_color
+
+    @draw_color.setter
+    def draw_color(self, new_value):
+        self._draw_color = new_value[:]
+        SDL_SetRenderDrawColor(self._renderer, *new_value)
+
+    def clear(self):
+        SDL_RenderClear(self._renderer)
+
+    def present(self):
+        SDL_RenderPresent(self._renderer)
+
+class Texture:
+    def __init__(self, renderer, surface):
+        self.renderer = renderer
+        self._tex = SDL_CreateTextureFromSurface(renderer._renderer, get_surface_ptr(surface))
+
+    def __del__(self):
+        SDL_DestroyTexture(self._tex)
