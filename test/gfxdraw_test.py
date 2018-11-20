4157ab46f633f04da7498e221081bab1aba80442
David Lönnhager <dv.lnh.d@gmail.com>
2018-11-20 14:20:45 +0100

    gfxdraw_test: fix test_aapolygon

--- test/gfxdraw_test.py
+++ test/gfxdraw_test.py
@@ -1,5 +1,5 @@
 # Two unit tests fail! Disable to allow automated builds to continue.
-__tags__ = ('ignore', 'subprocess_ignore')
+#__tags__ = ('ignore', 'subprocess_ignore')
 
 import unittest
 import pygame
@@ -572,12 +572,7 @@ class GfxdrawDefaultTest( unittest.TestCase ):
         fg = self.foreground_color
         bg = self.background_color
         points = [(10, 80), (10, 15), (92, 25), (92, 80)]
-        fg_test_points = (points +
-                          [(points[0][0], points[0][1] - 1),
-                           (points[0][0] + 1, points[0][1]),
-                           (points[3][0] - 1, points[3][1]),
-                           (points[3][0], points[3][1] - 1),
-                           (points[2][0], points[2][1] + 1)])
+        fg_test_points = points
         bg_test_points = [(points[0][0] - 1, points[0][1]),
                           (points[0][0], points[0][1] + 1),
                           (points[0][0] - 1, points[0][1] + 1),
@@ -596,7 +591,9 @@ class GfxdrawDefaultTest( unittest.TestCase ):
             bg_adjusted = surf.unmap_rgb(surf.map_rgb(bg))
             pygame.gfxdraw.aapolygon(surf, points, fg)
             for posn in fg_test_points:
-                self.check_not_at(surf, posn, bg_adjusted)
+                self.check_at(surf, posn, fg_adjusted)
+            for posn in bg_test_points:
+                self.check_not_at(surf, posn, fg_adjusted)
             for posn in bg_test_points:
                 self.check_at(surf, posn, bg_adjusted)
 
