81babcd34365e8d13f4bf141a8265743490ba711
e1000 <eea@terbium>
2018-11-29 21:11:53 +0100

    fix test_runner again : 'u' for unexpected success

--- test/test_utils/test_runner.py
+++ test/test_utils/test_runner.py
@@ -106,7 +106,7 @@ return (first 10 and last 10 lines):
 
 RAN_TESTS_DIV = (70 * "-") + "\nRan"
 
-DOTS = re.compile("^([FE.sx]*)$", re.MULTILINE)
+DOTS = re.compile("^([FE.sux]*)$", re.MULTILINE)
 
 def combine_results(all_results, t):
     """
