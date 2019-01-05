8d94ac5d721cdc381aa2ff58eb5f4d969b691b3d
David Lönnhager <dv.lnh.d@gmail.com>
2019-01-05 23:54:27 +0100

    Fix mask example float error.

--- examples/mask.py
+++ examples/mask.py
@@ -19,7 +19,7 @@ def maskFromSurface(surface, threshold = 127):
     if key:
         for y in range(surface.get_height()):
             for x in range(surface.get_width()):
-                if surface.get_at((x+0.1,y+0.1)) != key:
+                if surface.get_at((x,y)) != key:
                     mask.set_at((x,y),1)
     else:
         for y in range(surface.get_height()):
