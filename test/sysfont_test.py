53dd883b18e600cf1369f56efd4c4dd1ff59af2f
arnthor89 <arnthorj2@gmail.com>
2019-02-18 12:52:10 +0100

    #13 [fix] Add more tests for windows

--- test/sysfont_test.py
+++ test/sysfont_test.py
@@ -21,10 +21,15 @@ class SysfontModuleTest(unittest.TestCase):
     def todo_test_initsysfonts_unix(self):
         self.fail()
 
+    def test_get_fonts(self):
+        import pygame.sysfont
+        self.assertTrue(len(pygame.sysfont.get_fonts()) > 1)
+
     @unittest.skipIf('Windows' not in platform.system(), 'Not win we skip.')
     def test_initsysfonts_win32(self):
         import pygame.sysfont
-        self.assertTrue(len(pygame.sysfont.get_fonts()) > 1)
+        fonts = pygame.sysfont.initsysfonts_win32()
+        self.assertTrue(fonts)
 
 ################################################################################
 
