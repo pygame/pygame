f2c2d1e0994ca8558fa16f6a8e9118cb2c588a55
René Dudfield <renesd@gmail.com>
2019-03-24 12:05:17 +0100

    rpmforge didn't download, commented out. Seems to work anyway.

--- buildconfig/manylinux-build/docker_base/libpng/build-png.sh
+++ buildconfig/manylinux-build/docker_base/libpng/build-png.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# set -e -x
+set -e -x
 
-# cd $(dirname `readlink -f "$0"`)
+cd $(dirname `readlink -f "$0"`)
 
 PNG=libpng-1.6.36
 
 curl -sL http://download.sourceforge.net/libpng/${PNG}.tar.gz > ${PNG}.tar.gz
-# sha512sum -c png.sha512
+sha512sum -c png.sha512
 
-# tar xzf ${PNG}.tar.gz
-# cd $PNG
-# ./configure
-# make
-# make install
+tar xzf ${PNG}.tar.gz
+cd $PNG
+./configure
+make
+make install
