3f61e2897d16d094d7917f35f0eadd6480f63208
Thomas Kluyver <thomas@kluyver.me.uk>
2019-04-12 14:22:46 +0100

    Really build portmidi without Java interface

--- buildconfig/manylinux-build/docker_base/portmidi/build-portmidi.sh
+++ buildconfig/manylinux-build/docker_base/portmidi/build-portmidi.sh
@@ -9,19 +9,6 @@ curl -sL http://downloads.sourceforge.net/project/portmedia/portmidi/217/${SRC_Z
 sha512sum -c portmidi.sha512
 unzip $SRC_ZIP
 
-if [ "$(uname -i)" = "x86_64" ]; then
-    export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk.x86_64
-    JRE_LIB_DIR=amd64
-else
-    export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk
-    JRE_LIB_DIR=i386
-fi
-ls ${JAVA_HOME}
-ls ${JAVA_HOME}/jre
-ls ${JAVA_HOME}/jre/lib
-ls ${JAVA_HOME}/jre/lib/$JRE_LIB_DIR
-ls ${JAVA_HOME}/jre/lib/$JRE_LIB_DIR/server
-
 cd portmidi/
 patch -p1 < ../no-java.patch
 #cmake -DJAVA_JVM_LIBRARY=${JAVA_HOME}/jre/lib/${JRE_LIB_DIR}/server/libjvm.so .
