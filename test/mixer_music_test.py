c0b10a8c6721b52976d687029d8da36764fa3b49
David Lönnhager <dv.lnh.d@gmail.com>
2019-05-02 16:31:46 +0200

    Test mixer.music.unload()

--- test/mixer_music_test.py
+++ test/mixer_music_test.py
@@ -77,6 +77,20 @@ class MixerMusicModuleTest(unittest.TestCase):
         finally:
             os.remove(temp_file)
 
+    def test_unload(self):
+        import shutil
+        import tempfile
+        ep = unicode_(example_path('data'))
+        org_file = os.path.join(ep, u'house_lo.wav')
+        tmpfd, tmppath = tempfile.mkstemp('.wav')
+        os.close(tmpfd)
+        shutil.copy(org_file, tmppath)
+        try:
+            pygame.mixer.music.load(tmppath)
+            pygame.mixer.music.unload()
+        finally:
+            os.remove(tmppath)
+
     def todo_test_queue(self):
 
         # __doc__ (as of 2008-08-02) for pygame.mixer_music.queue:
