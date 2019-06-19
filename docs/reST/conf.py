a957284835a803758e4bba147ac9631d60e7bcf1
René Dudfield <renesd@gmail.com>
2019-06-19 18:21:23 +0200

    2.0.0.dev2

--- docs/reST/conf.py
+++ docs/reST/conf.py
@@ -48,9 +48,9 @@ copyright = u'2011-2019, pygame developers'
 # built documents.
 #
 # The short X.Y version.
-version = '2.0.0.dev1'
+version = '2.0.0.dev2'
 # The full version, including alpha/beta/rc tags.
-release = '2.0.0.dev1'
+release = '2.0.0.dev2'
 
 # Format strings for the version directives
 versionadded_format = 'New in pygame %s'
