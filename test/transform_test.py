d77d1813ff012bac8fff6f1d6387fe223f0233b3
Robert Pfeiffer <bob@blubberquark.de>
2019-08-09 18:33:30 +0200

    make scale2xraw a special case of scale

--- test/transform_test.py
+++ test/transform_test.py
@@ -918,13 +918,14 @@ class TransformModuleTest( unittest.TestCase ):
         s.fill((0,0,0))
         pygame.draw.circle(s, (255,0 , 0), (w//2, h//2), (w//3))
 
-        s2 = pygame.transform.scale2xraw(s)
-        s3 = pygame.transform.scale(s, (w*2, h*2))
+        s2 = pygame.transform.scale(s, (w*2, h*2))
+        s2_2 = pygame.transform.scale(s2, (w*4, h*4))
+        s4 = pygame.transform.scale(s, (w*4, h*4))
         
-        self.assertEqual(s2.get_rect().size, (64, 64))
+        self.assertEqual(s2_2.get_rect().size, (128, 128))
 
-        for pt in test_utils.rect_area_pts(s2.get_rect()):
-            self.assertEqual(s2.get_at(pt), s3.get_at(pt))
+        for pt in test_utils.rect_area_pts(s2_2.get_rect()):
+            self.assertEqual(s2_2.get_at(pt), s4.get_at(pt))
 
     def test_get_smoothscale_backend(self):
         filter_type = pygame.transform.get_smoothscale_backend()
