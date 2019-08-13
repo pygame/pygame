731f32b9ad4e7321253086e48f12509984f9d1aa
Robert Pfeiffer <bob@blubberquark.de>
2019-08-13 16:32:45 +0200

    coding style

--- src_c/scrap_sdl2.c
+++ src_c/scrap_sdl2.c
@@ -1,30 +1,29 @@
 #include "SDL.h"
 #include "SDL_clipboard.h"
 
-char* plaintext_type= "text/plain;charset=utf-8";
+char *plaintext_type = "text/plain;charset=utf-8";
 
 int
-pygame_scrap_contains(char *type){
-    return (strcmp(type, plaintext_type) == 0)
-        && SDL_HasClipboardText();
+pygame_scrap_contains(char *type)
+{
+    return (strcmp(type, plaintext_type) == 0) && SDL_HasClipboardText();
 }
 
-char*
-pygame_scrap_get(char *type, unsigned long *count){
+char *
+pygame_scrap_get(char *type, unsigned long *count)
+{
     char *retval = NULL;
     char *clipboard = NULL;
-
 
     if (!pygame_scrap_initialized()) {
         PyErr_SetString(pgExc_SDLError, "scrap system not initialized.");
         return NULL;
     }
 
-    if  (strcmp(type, plaintext_type) == 0){
-        printf("type OK\n");
+    if (strcmp(type, plaintext_type) == 0) {
         clipboard = SDL_GetClipboardText();
-        if (clipboard != NULL){
-            *count=strlen(clipboard);
+        if (clipboard != NULL) {
+            *count = strlen(clipboard);
             retval = strdup(clipboard);
             SDL_free(clipboard);
             return retval;
@@ -33,9 +32,10 @@ pygame_scrap_get(char *type, unsigned long *count){
     return NULL;
 }
 
-char**
-pygame_scrap_get_types(void){
-    char** types;
+char **
+pygame_scrap_get_types(void)
+{
+    char **types;
     types = malloc(sizeof(char *) * 2);
     if (!types)
         return NULL;
@@ -47,24 +47,27 @@ pygame_scrap_get_types(void){
 }
 
 int
-pygame_scrap_init(void){
+pygame_scrap_init(void)
+{
     SDL_Init(SDL_INIT_VIDEO);
     _scrapinitialized = 1;
     return _scrapinitialized;
 }
 
 int
-pygame_scrap_lost(void){
+pygame_scrap_lost(void)
+{
     return 1;
 }
 
 int
-pygame_scrap_put(char *type, int srclen, char *src){
+pygame_scrap_put(char *type, int srclen, char *src)
+{
     if (!pygame_scrap_initialized()) {
         PyErr_SetString(pgExc_SDLError, "scrap system not initialized.");
         return 0;
     }
-    if (strcmp(type, plaintext_type) == 0){
+    if (strcmp(type, plaintext_type) == 0) {
         if (SDL_SetClipboardText(src) == 0) {
             return 1;
         }
