b484fd094c395c7961c4194eb15f418645ee13e8
thiagojobson <thiagojobson@gmail.com>
2019-10-05 17:56:15 -0300

    Updated docstring

--- src_py/sprite.py
+++ src_py/sprite.py
@@ -168,7 +168,7 @@ class Sprite(object):
     def update(self, *args, **kwargs):
         """method to control sprite behavior
 
-        Sprite.update(*args):
+        Sprite.update(*args, **kwargs):
 
         The default implementation of this method does nothing; it's just a
         convenient "hook" that you can override. This method is called by
@@ -453,7 +453,7 @@ class AbstractGroup(object):
     def update(self, *args, **kwargs):
         """call the update method of every member sprite
 
-        Group.update(*args): return None
+        Group.update(*args, **kwargs): return None
 
         Calls the update method of every member sprite. All arguments that
         were passed to this method are passed to the Sprite update function.
