a873b946ca41b9d659145184b18772a0ec9fdaf4
René Dudfield <renesd@gmail.com>
2019-11-04 12:47:01 +0100

    OpenGL screen size from set_mode as requested.

--- test/display_test.py
+++ test/display_test.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import os
+
 import pygame, pygame.transform
 from pygame.compat import unicode_
 
@@ -401,6 +403,20 @@ class DisplayModuleTest(unittest.TestCase):
         #
 
         self.fail()
+
+
+@unittest.skipIf(
+    os.environ.get("SDL_VIDEODRIVER") == "dummy",
+    'OpenGL requires a non-"dummy" SDL_VIDEODRIVER',
+)
+class DisplayOpenGLTest(unittest.TestCase):
+    def test_screen_size_opengl(self):
+        """ returns a surface with the same size requested.
+        |tags:display,slow,opengl|
+        """
+        pygame.display.init()
+        screen = pygame.display.set_mode((640, 480), pygame.OPENGL)
+        self.assertEqual((640, 480), screen.get_size())
 
 
 if __name__ == "__main__":
