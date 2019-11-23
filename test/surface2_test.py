abc2a61aeeab89ac89cddc736bf78248263c6878
René Dudfield <renesd@gmail.com>
2019-11-23 17:06:48 +0100

    Findings at the top of the test.

--- test/surface2_test.py
+++ test/surface2_test.py
@@ -1,6 +1,25 @@
 import unittest
 import pygame
 
+
+"""
+SRC: (255, 255, 255, 100)
+DST: (  0,   0,   0,   0)
+SDL1:  -> (255, 255, 255, 100)
+SDL2:  -> ( 99,  99,  99,  99)
+
+SRC: (255, 255, 255, 100)
+DST: (  0,   0,   0, 255)
+SDL1:  -> (100, 100, 100, 255)
+SDL2: -> ( 99,  99,  99, 253)
+
+SRC: (255, 255, 255, 100)
+DST: (  0,   0,   0,  10)
+SDL1:  -> (100, 100, 100, 107)
+SDL2: -> ( 99,  99,  99, 105)
+
+"""
+
 class BlitIssueTest(unittest.TestCase):
 
     # @unittest.skip("causes failures in other tests if run, so skip")
@@ -11,11 +30,18 @@ class BlitIssueTest(unittest.TestCase):
         surf1.fill((255, 255, 255, 100))
 
         surf2 = pygame.Surface((1, 1), pygame.SRCALPHA, 32)
+        surf2.fill((0, 0, 0, 0))
         self.assertEqual(surf2.get_at((0, 0)), (0, 0, 0, 0))
         surf2.blit(surf1, (0, 0))
 
         self.assertEqual(surf1.get_at((0, 0)), (255, 255, 255, 100))
         self.assertEqual(surf2.get_at((0, 0)), (255, 255, 255, 100))
+        # SDL2 result:                         ( 99,  99,  99,  99)
+
+        # (255, 255, 255, 100)   (0, 0, 0, 0)
+        # SDL1:  -> (255, 255, 255, 100)
+        # SDL2:  -> ( 99,  99,  99,  99)
+
 
 
     def test_src_alpha_issue_1289_2(self):
@@ -33,7 +59,11 @@ class BlitIssueTest(unittest.TestCase):
 
         self.assertEqual(surf1.get_at((0, 0)), (255, 255, 255, 100))
         self.assertEqual(surf2.get_at((0, 0)), (100, 100, 100, 255))
+        # SDL2 result:                         ( 99,  99,  99, 253)
 
+        # (255, 255, 255, 100) (0, 0, 0, 255)
+        # SDL1:  -> (100, 100, 100, 255)
+        # SDL2: -> ( 99,  99,  99, 253)
 
 
     def test_src_alpha_issue_1289_3(self):
@@ -51,8 +81,11 @@ class BlitIssueTest(unittest.TestCase):
 
         self.assertEqual(surf1.get_at((0, 0)), (255, 255, 255, 100))
         self.assertEqual(surf2.get_at((0, 0)), (100, 100, 100, 107))
+        # SDL2 result                          ( 99,  99,  99, 105)
 
-
+        # (255, 255, 255, 100) (0, 0, 0, 10)
+        # SDL1:  -> (100, 100, 100, 107)
+        # SDL2: -> ( 99,  99,  99, 105)
 
 
 if __name__ == "__main__":
