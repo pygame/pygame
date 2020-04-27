d8e8ea8d3de948698ea74b7ae3d651b87796301e
Robert Pfeiffer <bob@blubberquark.de>
2020-04-27 20:03:42 +0200

    tests

--- test/surface_test.py
+++ test/surface_test.py
@@ -212,6 +212,49 @@ class SurfaceTypeTest(AssertRaisesRegexMixin, unittest.TestCase):
         for pt in test_utils.rect_outer_bounds(fill_rect):
             self.assertNotEqual(s1.get_at(pt), color)
 
+    def test_fill_rle(self):
+        """Ensure an RLE sub-surface works independently of its parent."""
+        color = (250, 25, 25, 255)
+        color2 = (200, 200, 250, 255)
+        sub_rect = pygame.Rect(16, 16, 16, 16)
+        s0 = pygame.Surface((32, 32), 24)
+        s1 = pygame.Surface((32, 32), 24)
+        s1.set_colorkey((255,0,255), pygame.RLEACCEL)
+        s0.blit(s1, (0,0))
+        s1.fill(color)
+        self.assertTrue(s1.get_flags()&pygame.RLEACCEL)
+
+    def test_subsurface_rle(self):
+        """Ensure an RLE sub-surface works independently of its parent."""
+        color = (250, 25, 25, 255)
+        color2 = (200, 200, 250, 255)
+        sub_rect = pygame.Rect(16, 16, 16, 16)
+        s0 = pygame.Surface((32, 32), 24)
+        s1 = pygame.Surface((32, 32), 24)
+        s1.set_colorkey((255,0,255), pygame.RLEACCEL)
+        s1.fill(color)
+        s2 = s1.subsurface(sub_rect)
+        s2.fill(color2)
+        s0.blit(s1, (0,0))
+        self.assertTrue(s1.get_flags()&pygame.RLEACCEL)
+        self.assertTrue(not s2.get_flags()&pygame.RLEACCEL)
+
+    @unittest.expectedFailure
+    def test_subsurface_rle2(self):
+        color = (250, 25, 25, 255)
+        color2 = (200, 200, 250, 255)
+        sub_rect = pygame.Rect(16, 16, 16, 16)
+
+        s0 = pygame.Surface((32, 32), 24)
+        s1 = pygame.Surface((32, 32), 24)
+        s1.set_colorkey((255,0,255), pygame.RLEACCEL)
+        s1.fill(color)
+        s2 = s1.subsurface(sub_rect)
+        s2.fill(color2)
+        s0.blit(s2, (0,0))
+        self.assertTrue(s1.get_flags()&pygame.RLEACCEL)
+        self.assertTrue(not s2.get_flags()&pygame.RLEACCEL)
+
     def test_fill_negative_coordinates(self):
 
         # negative coordinates should be clipped by fill, and not draw outside the surface.
