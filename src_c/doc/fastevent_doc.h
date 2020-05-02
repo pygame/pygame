2dc3c18155262adb3a6025ebcbd208dfb6c51dee
MyreMylar <dan@myrespace.com>
2020-05-02 07:34:00 +0100

    Attempt to upgrade the existing documentation for the fastevent module to the current system (so it works).

---
+++ src_c/doc/fastevent_doc.h
@@ -0,0 +1,47 @@
+/* Auto generated file: with makeref.py .  Docs go in docs/reST/ref/ . */
+#define DOC_PYGAMEFASTEVENT "pygame module for interacting with events and queues"
+#define DOC_PYGAMEFASTEVENTINIT "init() -> None\ninitialize pygame.fastevent"
+#define DOC_PYGAMEFASTEVENTGETINIT "get_init() -> bool\nreturns True if the fastevent module is currently initialized"
+#define DOC_PYGAMEFASTEVENTPUMP "pump() -> None\ninternally process pygame event handlers"
+#define DOC_PYGAMEFASTEVENTWAIT "wait() -> Event\nwait for an event"
+#define DOC_PYGAMEFASTEVENTPOLL "poll() -> Event\nget an available event"
+#define DOC_PYGAMEFASTEVENTGET "get() -> list of Events\nget all events from the queue"
+#define DOC_PYGAMEFASTEVENTPOST "post(Event) -> None\nplace an event on the queue"
+
+
+/* Docs in a comment... slightly easier to read. */
+
+/*
+
+pygame.fastevent
+pygame module for interacting with events and queues
+
+pygame.fastevent.init
+ init() -> None
+initialize pygame.fastevent
+
+pygame.fastevent.get_init
+ get_init() -> bool
+returns True if the fastevent module is currently initialized
+
+pygame.fastevent.pump
+ pump() -> None
+internally process pygame event handlers
+
+pygame.fastevent.wait
+ wait() -> Event
+wait for an event
+
+pygame.fastevent.poll
+ poll() -> Event
+get an available event
+
+pygame.fastevent.get
+ get() -> list of Events
+get all events from the queue
+
+pygame.fastevent.post
+ post(Event) -> None
+place an event on the queue
+
+*/
