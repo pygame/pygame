5f3c3a5eb7bd4b2e2c48fda4f064e45787a5c8af
René Dudfield <renesd@gmail.com>
2020-05-05 20:19:22 +0200

    2.0.0.dev9

--- docs/reST/conf.py
+++ docs/reST/conf.py
@@ -48,9 +48,9 @@ copyright = u'2011-2019, pygame developers'
 # built documents.
 #
 # The short X.Y version.
-version = '2.0.0.dev8'
+version = '2.0.0.dev9'
 # The full version, including alpha/beta/rc tags.
-release = '2.0.0.dev8'
+release = '2.0.0.dev9'
 
 # Format strings for the version directives
 versionadded_format = 'New in pygame %s'
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ EXTRAS = {}
 
 METADATA = {
     "name":             "pygame",
-    "version":          "2.0.0.dev8",
+    "version":          "2.0.0.dev9",
     "license":          "LGPL",
     "url":              "https://www.pygame.org",
     "author":           "A community project.",
