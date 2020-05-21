fcbf121e5e45d39a1e2f33deab19f73e64be60b5
MyreMylar <dan@myrespace.com>
2020-05-21 18:50:27 +0100

    fix to make image.c compile on sdl1

--- src_c/image.c
+++ src_c/image.c
@@ -30,7 +30,7 @@
 
 #include "doc/image_doc.h"
 
-#if __SSE4_2__ || PG_COMPILE_SSE4_2
+#if (__SSE4_2__ || PG_COMPILE_SSE4_2) && (SDL_VERSION_ATLEAST(2, 0, 0))
 #include <emmintrin.h>
 /* SSSE 3 */
 #include <tmmintrin.h>
@@ -320,7 +320,7 @@ image_get_extended(PyObject *self, PyObject *arg)
     return PyInt_FromLong(GETSTATE(self)->is_extended);
 }
 
-#if __SSE4_2__ || PG_COMPILE_SSE4_2
+#if (__SSE4_2__ || PG_COMPILE_SSE4_2) && (SDL_VERSION_ATLEAST(2, 0, 0))
 #define SSE42_ALIGN_NEEDED 16
 #define SSE42_ALIGN __attribute__((aligned(SSE42_ALIGN_NEEDED)))
 
@@ -510,7 +510,7 @@ tostring_surf_32bpp(SDL_Surface *surf, int flipped,
     Uint32 Bloss = surf->format->Bloss;
     Uint32 Aloss = surf->format->Aloss;
 
-#if __SSE4_2__ || PG_COMPILE_SSE4_2
+#if (__SSE4_2__ || PG_COMPILE_SSE4_2) && (SDL_VERSION_ATLEAST(2, 0, 0))
     if (/* SDL uses Uint32, SSE uses int for building vectors.
          * Related, we assume that Uint32 is packed so 4 of
          * them perfectly matches an __m128i.
