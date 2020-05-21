c979c522ce5357b2dba4ac5a165d7858567adb64
MyreMylar <dan@myrespace.com>
2020-05-21 15:26:15 +0100

    update tests to use same data file as example code

--- test/overlay_test.py
+++ test/overlay_test.py
@@ -1,5 +1,6 @@
 import unittest
 import pygame
+from pygame.compat import xrange_
 
 SDL2 = pygame.get_sdl_version()[0] >= 2
 
@@ -8,41 +9,50 @@ class OverlayTypeTest(unittest.TestCase):
     @unittest.skipIf(SDL2, "Overlay not ported to SDL2")
     def test_display(self):
         """Test we can create an overlay of all the different types"""
+
+        f = open('../examples/data/yuv_1.pgm', "rb")
+        fmt = f.readline().strip().decode()
+        res = f.readline().strip().decode()
+        unused_col = f.readline().strip()
+        if fmt != "P5":
+            print(
+                "Unknown format: %s ( len %d ). Exiting..." % (fmt, len(fmt)))
+            return
+
+        w, h = [int(x) for x in res.split(" ")]
+        h = int((h * 2) / 3)
+        # Read into strings
+        y = f.read(w * h)
+        u = b''
+        v = b''
+        for _ in xrange_(0, int(h / 2)):
+            u += (f.read(int(w / 2)))
+            v += (f.read(int(w / 2)))
+
+        f.close()
+
         pygame.display.init()
 
-        display_surface = pygame.display.set_mode((320, 240))
+        display_surface = pygame.display.set_mode((w, h))
         display_surface_rect = display_surface.get_rect()
         display_center = display_surface_rect.center
 
-        supported_formats = (pygame.YV12_OVERLAY,
-                             pygame.IYUV_OVERLAY,
-                             pygame.YUY2_OVERLAY,
-                             pygame.UYVY_OVERLAY,
-                             pygame.YVYU_OVERLAY)
+        # data from pgm file works for these two formats
+        formats_24_bit = (pygame.YV12_OVERLAY,
+                          pygame.IYUV_OVERLAY)
 
-        sizes = ((4, 4),
-                 (2, 2),
-                 (2, 2),
-                 (2, 2),
-                 (2, 2))
+        # still need good data for these three formats
+        formats_xx_bit = (pygame.YUY2_OVERLAY,
+                          pygame.UYVY_OVERLAY,
+                          pygame.YVYU_OVERLAY)
 
-        data = ((b'0xFF0xFF0xFF0xFF0xFF0xFF0xFF0xFF'
-                 b'0xFF0xFF0xFF0xFF0xFF0xFF0xFF0xFF',
-                 b'0xFF0xFF0xFF0xFF0xFF0xFF0xFF0xFF'
-                 b'0xFF0xFF0xFF0xFF0xFF0xFF0xFF0xFF',
-                 b'0xFF0xFF0xFF0xFF0xFF0xFF0xFF0xFF'
-                 b'0xFF0xFF0xFF0xFF0xFF0xFF0xFF0xFF'),
-                (b'0xFF0xFF0xFF0xFF', b'0xFF0xFF0xFF0xFF',
-                 b'0xFF0xFF0xFF0xFF'),
-                (b'0xFF0xFF0xFF0xFF', b'0xFF0xFF0xFF0xFF',
-                 b'0xFF0xFF0xFF0xFF'),
-                (b'0xFF0xFF0xFF0xFF', b'0xFF0xFF0xFF0xFF',
-                 b'0xFF0xFF0xFF0xFF'),
-                (b'0xFF0xFF0xFF0xFF', b'0xFF0xFF0xFF0xFF',
-                 b'0xFF0xFF0xFF0xFF'))
+        sizes = ((w, h),
+                 (w, h))
+        raw_data = ((y, u, v),
+                    (y, u, v))
 
         created_overlays = 0
-        for overlay_format, size, raw in zip(supported_formats, sizes, data):
+        for overlay_format, size, raw in zip(formats_24_bit, sizes, raw_data):
             overlay = pygame.Overlay(overlay_format, size)
             overlay_location = pygame.Rect((0, 0), size)
             overlay_location.center = display_center
@@ -50,7 +60,7 @@ class OverlayTypeTest(unittest.TestCase):
             overlay.display(raw)
             created_overlays += 1
 
-        self.assertEqual(created_overlays, 5)
+        self.assertEqual(created_overlays, 2)
 
     @unittest.skipIf(SDL2, "Overlay not ported to SDL2")
     def test_get_hardware(self):
@@ -64,23 +74,40 @@ class OverlayTypeTest(unittest.TestCase):
     @unittest.skipIf(SDL2, "Overlay not ported to SDL2")
     def test_set_location(self):
         """Test overlay set location"""
+
+        f = open('../examples/data/yuv_1.pgm', "rb")
+        fmt = f.readline().strip().decode()
+        res = f.readline().strip().decode()
+        unused_col = f.readline().strip()
+        if fmt != "P5":
+            print(
+                "Unknown format: %s ( len %d ). Exiting..." % (fmt, len(fmt)))
+            return
+
+        w, h = [int(x) for x in res.split(" ")]
+        h = int((h * 2) / 3)
+        # Read into strings
+        y = f.read(w * h)
+        u = b''
+        v = b''
+        for _ in xrange_(0, int(h / 2)):
+            u += (f.read(int(w / 2)))
+            v += (f.read(int(w / 2)))
+
+        f.close()
+
         pygame.display.init()
 
-        display_surface = pygame.display.set_mode((320, 240))
+        display_surface = pygame.display.set_mode((w, h))
         display_surface.fill((0, 0, 0))
         pygame.display.update()
 
-        overlay = pygame.Overlay(pygame.YV12_OVERLAY, (4, 4))
+        overlay = pygame.Overlay(pygame.YV12_OVERLAY, (w, h))
 
-        raw_data = (b'0xFF0xFF0xFF0xFF0xFF0xFF0xFF0xFF'
-                    b'0xFF0xFF0xFF0xFF0xFF0xFF0xFF0xFF',
-                    b'0xFF0xFF0xFF0xFF0xFF0xFF0xFF0xFF'
-                    b'0xFF0xFF0xFF0xFF0xFF0xFF0xFF0xFF',
-                    b'0xFF0xFF0xFF0xFF0xFF0xFF0xFF0xFF'
-                    b'0xFF0xFF0xFF0xFF0xFF0xFF0xFF0xFF')
+        raw_data = (y, u, v)
         overlay.display(raw_data)
         pygame.display.update()
-        overlay_location = pygame.Rect((0, 0), (4, 4))
+        overlay_location = pygame.Rect((0, 0), (w, h))
 
         positions = ((1, 1), (1, 190), (270, 1), (270, 190))
         for position in positions:
@@ -89,7 +116,7 @@ class OverlayTypeTest(unittest.TestCase):
             overlay.display()
             pygame.display.update()
             self.assertEqual(display_surface.get_at(position),
-                             pygame.Color(0, 171, 0))
+                             pygame.Color(0, 151, 0))
 
 
 ################################################################################
