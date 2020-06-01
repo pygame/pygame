e83b8035164117c02b0338328ee8a5da01d3aa46
Kevin Huang <huangk11@uci.edu>
2020-06-01 16:09:47 -0700

    added two more unittests to account for errors that occur when method is called after pygame.display.quit()

--- test/surface_test.py
+++ test/surface_test.py
@@ -1014,16 +1014,29 @@ class SurfaceTypeTest(AssertRaisesRegexMixin, unittest.TestCase):
         colorkey = pygame.Color(r, g, b)
         s.set_colorkey(colorkey)
         
+        #test for ideal case
         self.assertEqual(s.get_colorkey(), (r, g, b, 255))
         
+        #test for if the color_key is set using pygame.RLEACCEL
         s.set_colorkey(colorkey, pygame.RLEACCEL)
         self.assertEqual(s.get_colorkey(), (r, g, b, 255))
         
+        #test for if the color key is not what's expected
         s.set_colorkey(pygame.Color(r + 1, g + 1, b + 1))
         self.assertNotEqual(s.get_colorkey(), (r, g, b, 255))
 
         s.set_colorkey(pygame.Color(r, g, b, a)) # regardless of whether alpha is not 255, colorkey returned from surface is always 255
         self.assertEqual(s.get_colorkey(), (r, g, b, 255))
+        
+        # test for using method after display.quit() is called...
+        pygame.display.set_mode()
+        pygame.display.quit()
+        self.assertRaises(pygame.error)
+
+        # test for using method when display is created with OpenGL and the SDL version is 1
+        if pygame.get_sdl_version()[0] > 1: # this function returns a tuple
+            pygame.display.set_mode(flags=pygame.OpenGL)
+            self.assertRaises(pygame.error)
 
     def test_get_height(self):
         sizes = ((1, 1), (119, 10), (10, 119), (1, 1000), (1000, 1), (1000, 1000))
