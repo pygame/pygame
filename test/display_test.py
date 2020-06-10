dc26bd89188eec056a4b550fc9cc04d2d65f3037
Grigoris Tsopouridis <43859151+gtsopus@users.noreply.github.com>
2020-06-10 23:18:57 +0300

    Add display.set_gamma test
    Adds a test function for display.set_gamma ,Issue#1750

--- test/display_test.py
+++ test/display_test.py
@@ -314,23 +314,15 @@ class DisplayModuleTest(unittest.TestCase):
 
         self.assertFalse(display.get_init())
 
-    def todo_test_set_gamma(self):
+    def test_set_gamma(self):
+        pygame.display.set_mode((1, 1))
+        gammas = [0.0,0.25,0.5,0.88,1.0]
+        for gamma in gammas:
+            self.assertEqual(pygame.display.set_gamma(gamma),True)
+        gammas = [(0.5,0.5,0.5),(1.0,1.0,1.0),(0.22,0.33,0.44),(0.0,0.0,0.0)]
+        for gammaTuple in gammas:
+            self.assertEqual(pygame.display.set_gamma(gammaTuple[0],gammaTuple[1],gammaTuple[2]),True)
 
-        # __doc__ (as of 2008-08-02) for pygame.display.set_gamma:
-
-        # pygame.display.set_gamma(red, green=None, blue=None): return bool
-        # change the hardware gamma ramps
-        #
-        # Set the red, green, and blue gamma values on the display hardware.
-        # If the green and blue arguments are not passed, they will both be
-        # the same as red. Not all systems and hardware support gamma ramps,
-        # if the function succeeds it will return True.
-        #
-        # A gamma value of 1.0 creates a linear color table. Lower values will
-        # darken the display and higher values will brighten.
-        #
-
-        self.fail()
 
     def todo_test_set_gamma_ramp(self):
 
