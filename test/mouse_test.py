fbb698125c886f4fdfcf04d92da84191cea43087
Reminisque <travisc2728@gmail.com>
2020-06-15 19:47:17 -0700

    added unit test for get_cursor() #1762

--- test/mouse_test.py
+++ test/mouse_test.py
@@ -57,9 +57,43 @@ class MouseModuleInteractiveTest(MouseTests):
 
 class MouseModuleTest(MouseTests):
 
-    def todo_test_get_cursor(self):
+    def test_get_cursor(self):
         """Ensures get_cursor works correctly."""
-        self.fail()
+        #error should be raised when the display is unintialized
+        with self.assertRaisesRegex(pygame.error, "video system not initialized"):
+            pygame.display.quit()
+            pygame.mouse.get_cursor()
+        
+        pygame.display.init()
+
+        if not SDL1:
+            with self.assertRaises(TypeError):
+                pygame.mouse.get_cursor()
+        else:
+            size = (8, 8)
+            hotspot = (0, 0)
+            xormask = (0, 96, 120, 126, 112, 96, 0, 0)
+            andmask = (224, 240, 254, 255, 254, 240, 96, 0)
+
+            expected_length = 4
+            expected_cursor = (size, hotspot, xormask, andmask)
+
+            try:
+                cursor = pygame.mouse.get_cursor()
+                
+                self.assertIsInstance(cursor, tuple)
+                self.assertEqual(len(cursor), expected_length)
+
+                for info in cursor:
+                    self.assertIsInstance(info, tuple)
+                
+                pygame.mouse.set_cursor(size, hotspot, xormask, andmask)
+                self.assertEqual(pygame.mouse.get_cursor(), expected_cursor)
+
+            #SDLError should be raised when the mouse cursor is NULL
+            except pygame.error:
+                with self.assertRaises(pygame.error):
+                    pygame.mouse.get_cursor()         
 
     def todo_test_set_cursor(self):
         """Ensures set_cursor works correctly."""
