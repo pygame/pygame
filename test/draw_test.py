38b13645d40d57c1cb0c41e94f4eae0218f92e8f
MightyJosip <josip.komljenovic@fer.hr>
2020-08-03 16:44:42 +0200

    Added test for holes in ellipse

--- test/draw_test.py
+++ test/draw_test.py
@@ -601,6 +601,27 @@ class DrawEllipseMixin(object):
 
             surface.unlock()
 
+    def test_ellipse__no_holes(self):
+        width = 80
+        height = 70
+        surface = pygame.Surface((width + 1, height))
+        rect = pygame.Rect(0, 0, width, height)
+        for thickness in range(1, 37, 5):
+            surface.fill('BLACK')
+            self.draw_ellipse(surface, 'RED', rect, thickness)
+            for y in range(height):
+                number_of_changes = 0
+                drawn_pixel = False
+                for x in range(width + 1):
+                    if not drawn_pixel and surface.get_at((x, y)) == pygame.Color('RED') or \
+                           drawn_pixel and surface.get_at((x, y)) == pygame.Color('BLACK'):
+                        drawn_pixel = not drawn_pixel
+                        number_of_changes += 1
+                if y < thickness or y > height - thickness - 1:
+                    self.assertEqual(number_of_changes, 2)
+                else:
+                    self.assertEqual(number_of_changes, 4)
+
     def test_ellipse__max_width(self):
         """Ensures an ellipse with max width (and greater) is drawn correctly."""
         ellipse_color = pygame.Color("yellow")
