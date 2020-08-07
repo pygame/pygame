af2733494dd978eb4092a985c60a52a3519df312
Sigurdur Sveinn Halldorsson <siggisv@gmail.com>
2020-08-07 17:32:13 +0200

    Optimize getting radius of sprite
    Changed the code in pygame.sprite.collide_circle() and
    pygame.sprite.collide_circle_ratio() that gets the radius of the sprites
    being tested for collision.
    
    Checking for collision using those takes now about 12-13% less than
    before when the radius attribute of the sprites has been set. This comes
    with the disadvantage that it now takes about 16-17% more time when the
    radius attribute has not been set.
    
    Considering that it is at most only once per sprite that they don't have
    that attribute set when using those collision checks (unless the user of
    this library has for some reason decided to have his code delete that
    attribute) then the cost of having that first check of each sprite taking
    longer should be overshadowed by the benefit of all the following checks on
    that sprite being quicker.

--- src_py/sprite.py
+++ src_py/sprite.py
@@ -1483,27 +1483,27 @@ def collide_circle(left, right):
     ydistance = left.rect.centery - right.rect.centery
     distancesquared = xdistance ** 2 + ydistance ** 2
 
-    if hasattr(left, 'radius'):
+    try:
         leftradius = left.radius
-    else:
+    except AttributeError:
         leftrect = left.rect
         # approximating the radius of a square by using half of the diagonal,
         # might give false positives (especially if its a long small rect)
         leftradius = (0.5 * ((leftrect.width ** 2 +
                               leftrect.height ** 2) ** 0.5))
         # store the radius on the sprite for next time
-        setattr(left, 'radius', leftradius)
+        left.radius = leftradius
 
-    if hasattr(right, 'radius'):
+    try:
         rightradius = right.radius
-    else:
+    except AttributeError:
         rightrect = right.rect
         # approximating the radius of a square by using half of the diagonal
         # might give false positives (especially if its a long small rect)
         rightradius = (0.5 * ((rightrect.width ** 2 +
                                rightrect.height ** 2) ** 0.5))
         # store the radius on the sprite for next time
-        setattr(right, 'radius', rightradius)
+        right.radius = rightradius
     return distancesquared <= (leftradius + rightradius) ** 2
 
 
@@ -1562,24 +1562,24 @@ class collide_circle_ratio(object):  # noqa pylint: disable=invalid-name; this i
         ydistance = left.rect.centery - right.rect.centery
         distancesquared = xdistance ** 2 + ydistance ** 2
 
-        if hasattr(left, "radius"):
+        try:
             leftradius = left.radius
-        else:
+        except AttributeError:
             leftrect = left.rect
             leftradius = (0.5 * ((leftrect.width ** 2 +
                                   leftrect.height ** 2) ** 0.5))
             # store the radius on the sprite for next time
-            setattr(left, 'radius', leftradius)
+            left.radius = leftradius
         leftradius *= ratio
 
-        if hasattr(right, "radius"):
+        try:
             rightradius = right.radius
-        else:
+        except AttributeError:
             rightrect = right.rect
             rightradius = (0.5 * ((rightrect.width ** 2 +
                                    rightrect.height ** 2) ** 0.5))
             # store the radius on the sprite for next time
-            setattr(right, 'radius', rightradius)
+            right.radius = rightradius
         rightradius *= ratio
 
         return distancesquared <= (leftradius + rightradius) ** 2
