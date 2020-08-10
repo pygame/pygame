eef8c4e78b1254d5bae1695d8e737a7f6a3c24e3
Andy Nguyen <anguye13@binghamton.edu>
2020-08-10 09:05:51 -0400

    Added test for subsurfaces

--- test/surface_test.py
+++ test/surface_test.py
@@ -1391,11 +1391,26 @@ class SurfaceTypeTest(AssertRaisesRegexMixin, unittest.TestCase):
         # Test get_pitch() on several surfaces of varying size
         sizes = ((1, 1), (7, 33), (33, 7), (1, 734), (734, 1), (734, 734))
         for width, height in sizes:
+            # Test get_pitch() on parent surface
             surf = pygame.Surface((width, height))
             buff = surf.get_buffer()
             pitch = buff.length / surf.get_height()
             test_pitch = surf.get_pitch()
             self.assertEqual(pitch, test_pitch)
+            # Test get_pitch() on subsurface with same rect as parent
+            rect1 = surf.get_rect()
+            subsurf1 = surf.subsurface(rect1)
+            sub_buff1 = subsurf1.get_buffer()
+            sub_pitch1 = sub_buff1.length / subsurf1.get_height()
+            test_sub_pitch1 = subsurf1.get_pitch()
+            self.assertEqual(sub_pitch1, test_sub_pitch1)
+            # Test get_pitch on subsurface with modified rect
+            rect2 = rect1.inflate(-width/2, -height/2)
+            subsurf2 = surf.subsurface(rect2)
+            sub_buff2 = subsurf2.get_buffer()
+            sub_pitch2 = sub_buff2.length / subsurf2.get_height()
+            test_sub_pitch2 = subsurf2.get_pitch()
+            self.assertEqual(sub_pitch2, test_sub_pitch2)
 
     def todo_test_get_shifts(self):
 
