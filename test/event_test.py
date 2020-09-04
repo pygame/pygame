1dc693efb51e906636fdf9e7f91513c856730933
Sigurdur Sveinn Halldorsson <siggisv@gmail.com>
2020-09-04 23:14:03 +0200

    Add unittest to catch off-by-one bug in event.c
    The custom user event types returned by pygame.event.custom_type()
    should be up to pygame.NUMEVENTS, but not include it.

--- test/event_test.py
+++ test/event_test.py
@@ -599,6 +599,18 @@ class EventModuleTest(unittest.TestCase):
         self.assertEqual(len(queue), 1)
         self.assertEqual(queue[0].type, atype)
 
+    def test_custom_type__end_boundary(self):
+        """Ensure custom_type() raises error when no more custom types.
+
+        The last allowed custom type number should be (pygame.NUMEVENTS - 1).
+        """
+        start = pygame.event.custom_type() + 1
+        for i in range(start, pygame.NUMEVENTS):
+            last = pygame.event.custom_type()
+        self.assertEqual(last, pygame.NUMEVENTS - 1)
+        with self.assertRaises(pygame.error):
+            pygame.event.custom_type()
+
     def test_get_blocked(self):
         """Ensure an event's blocked state can be retrieved."""
         # Test each event is not blocked.
