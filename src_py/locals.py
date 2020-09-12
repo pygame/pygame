ad593b53667e93d751926631ba305130a63a09e2
René Dudfield <renesd@gmail.com>
2020-09-12 15:21:46 +0200

    supress import * lint warnings

--- src_py/locals.py
+++ src_py/locals.py
@@ -22,7 +22,7 @@
 """Set of functions from PyGame that are handy to have in
 the local namespace for your module"""
 
-from pygame.constants import *  # pylint: disable=wildcard-import
+from pygame.constants import *  # pylint: disable=wildcard-import; lgtm[py/polluting-import]
 from pygame.rect import Rect
 import pygame.color as color
 Color = color.Color
