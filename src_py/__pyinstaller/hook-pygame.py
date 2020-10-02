46ea857f5c18207d7b7d891304b0f052405ecb63
Ankith <46915066+ankith26@users.noreply.github.com>
2020-10-02 12:17:15 +0530

    Update Pyinstaller hooks

--- src_py/__pyinstaller/hook-pygame.py
+++ src_py/__pyinstaller/hook-pygame.py
@@ -3,7 +3,25 @@ binaries hook for pygame seems to be required for pygame 2.0 Windows.
 Otherwise some essential DLLs will not be transfered to the exe.
 """
 
+import os
 import platform
+
+from pygame import __file__ as pygame_main_file
+
+# Any resources that pygame depends upon must be put into this list,
+# so that pyinstaller includes it while running
+pygame_res = ["freesansbold.ttf", "pygame.ico", "pygame_icon.bmp",
+              "pygame_icon.icns", "pygame_icon.svg", "pygame_icon.tiff"]
+
+# Get pygame's folder
+pygame_folder = os.path.dirname(os.path.abspath(pygame_main_file))
+
+# datas is the variable that pyinstaller looks for while processing hooks
+datas = []
+for res in pygame_res:
+    res_path = os.path.join(pygame_folder, res)
+    if os.path.exists(res_path):
+        datas.append((res_path, "pygame"))
 
 if platform.system() == "Windows":
     
