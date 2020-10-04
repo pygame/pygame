dc13e71ca2f0097d8349e7ddb931e49f2c1005ac
Ankith <46915066+ankith26@users.noreply.github.com>
2020-10-04 14:05:37 +0530

    Update hooks

--- src_py/__pyinstaller/hook-pygame.py
+++ src_py/__pyinstaller/hook-pygame.py
@@ -1,6 +1,9 @@
 """
 binaries hook for pygame seems to be required for pygame 2.0 Windows.
 Otherwise some essential DLLs will not be transfered to the exe.
+
+And also put hooks for datas, resources that pygame uses, to work 
+correctly with pyinstaller
 """
 
 import os
@@ -8,23 +11,27 @@ import platform
 
 from pygame import __file__ as pygame_main_file
 
-# Any resources that pygame depends upon must be put into this list,
-# so that pyinstaller includes it while running
-pygame_res = ["freesansbold.ttf", "pygame.ico", "pygame_icon.bmp",
-              "pygame_icon.icns", "pygame_icon.svg", "pygame_icon.tiff"]
-
 # Get pygame's folder
 pygame_folder = os.path.dirname(os.path.abspath(pygame_main_file))
 
 # datas is the variable that pyinstaller looks for while processing hooks
 datas = []
-for res in pygame_res:
-    res_path = os.path.join(pygame_folder, res)
+
+# A helper to append the relative path of a resource to hook variable - datas
+def _append_to_datas(file_path):
+    global datas
+    res_path = os.path.join(pygame_folder, file_path)
     if os.path.exists(res_path):
         datas.append((res_path, "pygame"))
 
-if platform.system() == "Windows":
-    
+# First append the font file, then based on the OS, append pygame icon file
+_append_to_datas("freesansbold.ttf")
+if platform.system() == "Darwin":
+    _append_to_datas("pygame_icon.tiff")
+else:
+    _append_to_datas("pygame_icon.bmp")
+
+if platform.system() == "Windows": 
     from PyInstaller.utils.hooks import collect_dynamic_libs
 
     pre_binaries = collect_dynamic_libs('pygame')
