05d0de98b037eb70290680a7c2bb3db3cb3e8a2c
MyreMylar <dan@myrespace.com>
2020-10-15 15:25:55 +0100

    Another RLE test that only works on SDL 1

--- test/surface_test.py
+++ test/surface_test.py
@@ -408,6 +408,40 @@ class SurfaceTypeTest(AssertRaisesRegexMixin, unittest.TestCase):
         finally:
             pygame.display.quit()
 
+    @unittest.skipIf(pygame.get_sdl_version()[0] == 2, "only works in SDL 1")
+    def test_set_alpha__set_colorkey_rle(self):
+        pygame.display.init()
+        try:
+            pygame.display.set_mode((640, 480))
+            blit_to_surf = pygame.Surface((80, 71))
+            blit_to_surf.fill((255, 255, 255))
+
+            image = pygame.image.load(example_path(os.path.join("data",
+                                                                "alien1.png")))
+            image = image.convert()
+            orig_colorkey = image.get_colorkey()
+
+            # Add the RLE flag while setting alpha for the whole surface
+            image.set_alpha(90, RLEACCEL)
+            blit_to_surf.blit(image, (0, 0))
+            sample_pixel_rle = blit_to_surf.get_at((50, 50))
+
+            # Now reset the colorkey to the original value with RLE
+            image.set_colorkey(orig_colorkey, RLEACCEL)
+            blit_to_surf.fill((255, 255, 255))
+            blit_to_surf.blit(image, (0, 0))
+            sample_pixel_no_rle = blit_to_surf.get_at((50, 50))
+
+            self.assertAlmostEqual(sample_pixel_rle.r,
+                                   sample_pixel_no_rle.r, delta=2)
+            self.assertAlmostEqual(sample_pixel_rle.g,
+                                   sample_pixel_no_rle.g, delta=2)
+            self.assertAlmostEqual(sample_pixel_rle.b,
+                                   sample_pixel_no_rle.b, delta=2)
+
+        finally:
+            pygame.display.quit()
+
     def test_fill_negative_coordinates(self):
 
         # negative coordinates should be clipped by fill, and not draw outside the surface.
