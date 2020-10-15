894537c44b3e4ae404541cc1d018a36a09e57090
MyreMylar <dan@myrespace.com>
2020-10-15 14:30:52 +0100

    Added another solarwolf sourced RLE test and improved existing one with more asserts

--- test/surface_test.py
+++ test/surface_test.py
@@ -320,16 +320,55 @@ class SurfaceTypeTest(AssertRaisesRegexMixin, unittest.TestCase):
         def optimize(img):
             clear = img.get_colorkey()
             img.set_colorkey(clear, RLEACCEL)
+            self.assertEqual(img.get_colorkey(), clear)
             return img.convert()
 
         pygame.display.init()
         try:
             pygame.display.set_mode((640, 480))
+
             image = pygame.image.load(example_path(os.path.join("data",
                                                                 "alien1.png")))
+            orig_colorkey = image.get_colorkey()
+
             image = optimize(image)
             image = optimize(image)
+            self.assertTrue(image.get_flags() & pygame.RLEACCELOK)
+            self.assertTrue(not image.get_flags() & pygame.RLEACCEL)
+            self.assertEqual(image.get_colorkey(), orig_colorkey)
             self.assertTrue(isinstance(image, pygame.Surface))
+        finally:
+            pygame.display.quit()
+
+    def test_solarwolf_rle_usage_2(self):
+        """ Test for RLE status after setting alpha """
+
+        pygame.display.init()
+        try:
+            pygame.display.set_mode((640, 480))
+            blit_to_surf = pygame.Surface((100, 100))
+
+            image = pygame.image.load(example_path(os.path.join("data",
+                                                                "alien1.png")))
+            image = image.convert()
+            orig_colorkey = image.get_colorkey()
+
+            # set the colorkey with RLEACCEL, should add the RLEACCELOK flag
+            image.set_colorkey(orig_colorkey, RLEACCEL)
+            self.assertTrue(image.get_flags() & pygame.RLEACCELOK)
+            self.assertTrue(not image.get_flags() & pygame.RLEACCEL)
+
+            # now blit the surface - should add the RLEACCEL flag
+            blit_to_surf.blit(image, (0, 0))
+            self.assertTrue(image.get_flags() & pygame.RLEACCELOK)
+            self.assertTrue(image.get_flags() & pygame.RLEACCEL)
+
+            # Now set the alpha, without RLE acceleration - should strip all
+            # RLE flags
+            image.set_alpha(90)
+            self.assertTrue(not image.get_flags() & pygame.RLEACCELOK)
+            self.assertTrue(not image.get_flags() & pygame.RLEACCEL)
+
         finally:
             pygame.display.quit()
 
