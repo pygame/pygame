0b625f51d9e921cf745e83822e72927634867f67
Sigurdur Sveinn Halldorsson <siggisv@gmail.com>
2020-10-24 21:02:01 +0200

    Add test_lines__color_with_thickness()
    Only two more todos left in draw_test.py

--- test/draw_test.py
+++ test/draw_test.py
@@ -2235,9 +2235,32 @@ class LinesMixin(BaseLineMixin):
                 for pos, color in border_pos_and_color(surface):
                     self.assertEqual(color, expected_color, "pos={}".format(pos))
 
-    def todo_test_lines__color_with_thickness(self):
+    def test_lines__color_with_thickness(self):
         """Ensures thick lines are drawn using the correct color."""
-        self.fail()
+        x_left = y_top = 5
+        for surface in self._create_surfaces():
+            x_right = surface.get_width() - 5
+            y_bottom = surface.get_height() - 5
+            endpoints = ((x_left, y_top), (x_right, y_top),
+                         (x_right, y_bottom), (x_left, y_bottom))
+            for expected_color in self.COLORS:
+                self.draw_lines(surface, expected_color, True, endpoints, 3)
+
+                for t in (-1, 0, 1):
+                    for x in range(x_left, x_right+1):
+                        for y in (y_top, y_bottom):
+                            pos = (x, y+t)
+                            self.assertEqual(
+                                    surface.get_at(pos), expected_color,
+                                    "pos={}".format(pos)
+                            )
+                    for y in range(y_top, y_bottom+1):
+                        for x in (x_left, x_right):
+                            pos = (x+t, y)
+                            self.assertEqual(
+                                    surface.get_at(pos), expected_color,
+                                    "pos={}".format(pos)
+                            )
 
     def test_lines__gaps(self):
         """Tests if the lines drawn contain any gaps.
