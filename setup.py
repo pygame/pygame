d0396e4fb5889f3189c3503de5d2c5fe4764233c
Ankith <46915066+ankith26@users.noreply.github.com>
2020-10-31 20:01:13 +0530

    Update setup code to load threads module

--- setup.py
+++ setup.py
@@ -761,7 +761,6 @@ date_files = [(path, files) for path, files in data_files if files]
 PACKAGEDATA = {
        "cmdclass":    cmdclass,
        "packages":    ['pygame',
-                       'pygame.threads',
                        'pygame._sdl2',
                        'pygame.tests',
                        'pygame.tests.test_utils',
@@ -781,7 +780,6 @@ PACKAGEDATA = {
                        'pygame.__pyinstaller'],
        "package_dir": {'pygame': 'src_py',
                        'pygame._sdl2': 'src_py/_sdl2',
-                       'pygame.threads': 'src_py/threads',
                        'pygame.tests': 'test',
                        'pygame.docs': 'docs',
                        'pygame.examples': 'examples',
@@ -801,11 +799,9 @@ if STRIPPED:
     PACKAGEDATA = {
     "cmdclass":    cmdclass,
     "packages":    ['pygame',
-                    'pygame.threads',
                     'pygame._sdl2'],
     "package_dir": {'pygame': 'src_py',
-                    'pygame._sdl2': 'src_py/_sdl2',
-                    'pygame.threads': 'src_py/threads'},
+                    'pygame._sdl2': 'src_py/_sdl2'},
     "ext_modules": extensions,
     "zip_safe":  False,
     "data_files": data_files
