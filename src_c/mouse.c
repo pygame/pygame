d8c5c24319af0c97acc7e65e9fb666d92109e170
Starbuck5 <charlie.c.hayden@gmail.com>
2020-12-15 17:07:21 -0800

    Add deprecation warning for mouse.set_system_cursor()

--- src_c/mouse.c
+++ src_c/mouse.c
@@ -392,6 +392,10 @@ static PyObject *
 mouse_set_system_cursor(PyObject *self, PyObject *args)
 {
     int constant;
+
+    if (PyErr_WarnEx(PyExc_DeprecationWarning, "set_cursor() now has all the functionality of set_system_cursor(), so set_system_cursor() will be deprecated in pygame 2.1", 1) == -1) {
+        return NULL;
+    }
     
     VIDEO_INIT_CHECK();
 
