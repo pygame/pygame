09b63f017d2d3af02f8ff7e2b50e7f1e920c6bce
zoldalma <szgabe870@gmail.com>
2020-12-21 22:36:54 +0100

    [skip ci] Revert ordereddicts; update sprite group docs

--- src_py/sprite.py
+++ src_py/sprite.py
@@ -93,14 +93,6 @@ from pygame.rect import Rect
 from pygame.time import get_ticks
 from pygame.mask import from_surface
 
-#check if we need to use orderedDict (see #1951)
-import platform
-import sys
-if sys.version_info < (3, 7, 0) and platform.python_implementation() != 'PyPy':
-    from collections import OrderedDict
-    USE_ORDEREDDICT = True
-else:
-    USE_ORDEREDDICT = False
 
 class Sprite(object):
     """simple base class for visible game objects
@@ -362,10 +354,7 @@ class AbstractGroup(object):
     _spritegroup = True
 
     def __init__(self):
-        if USE_ORDEREDDICT:
-            self.spritedict = OrderedDict()
-        else:
-            self.spritedict = {}
+        self.spritedict = {}
         self.lostsprites = []
 
     def sprites(self):
