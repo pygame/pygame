22988c74fb740fb24ec6e13393be9d51226ea73d
René Dudfield <renesd@gmail.com>
2020-12-22 19:07:10 +0100

    manylinux fixes for SDL2-2.0.14 release.

--- buildconfig/manylinux-build/docker_base/pulseaudio/build-pulseaudio.sh
+++ buildconfig/manylinux-build/docker_base/pulseaudio/build-pulseaudio.sh
@@ -5,7 +5,7 @@ cd /pulseaudio_build/
 PULSEFILE="pulseaudio-14.0"
 
 curl -sL https://www.freedesktop.org/software/pulseaudio/releases/${PULSEFILE}.tar.xz > ${PULSEFILE}.tar.xz
-# sha512sum -c pulseaudio.sha512
+sha512sum -c pulseaudio.sha512
 unxz ${PULSEFILE}.tar.xz
 tar xf ${PULSEFILE}.tar
 
--- buildconfig/manylinux-build/docker_base/sndfile/build-sndfile.sh
+++ buildconfig/manylinux-build/docker_base/sndfile/build-sndfile.sh
@@ -7,7 +7,7 @@ SNDFILE="libsndfile-$SNDFILEVER.tar.bz2"
 curl -sL https://github.com/libsndfile/libsndfile/releases/download/v${SNDFILEVER}/${SNDFILE} > ${SNDFILE}
 # https://github.com/libsndfile/libsndfile/releases/download/v1.0.30/libsndfile-1.0.30.tar.bz2
 
-# sha512sum -c sndfile.sha512
+sha512sum -c sndfile.sha512
 tar xf ${SNDFILE}
 cd libsndfile-${SNDFILEVER}
 ./configure
