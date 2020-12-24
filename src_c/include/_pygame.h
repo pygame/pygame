5c9b69fc83862feb300bc0abc15131b16284cb2d
René Dudfield <renesd@gmail.com>
2020-12-24 14:16:23 +0100

    2.0.2.dev1

--- src_c/include/_pygame.h
+++ src_c/include/_pygame.h
@@ -58,7 +58,7 @@
 /* version macros (defined since version 1.9.5) */
 #define PG_MAJOR_VERSION 2
 #define PG_MINOR_VERSION 0
-#define PG_PATCH_VERSION 1
+#define PG_PATCH_VERSION 2
 #define PG_VERSIONNUM(MAJOR, MINOR, PATCH) (1000*(MAJOR) + 100*(MINOR) + (PATCH))
 #define PG_VERSION_ATLEAST(MAJOR, MINOR, PATCH)                             \
     (PG_VERSIONNUM(PG_MAJOR_VERSION, PG_MINOR_VERSION, PG_PATCH_VERSION) >= \
