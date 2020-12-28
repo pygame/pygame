afbd99b8a823eab112b515fef0611d9247c007b3
MyreMylar <dan@myrespace.com>
2020-12-28 09:04:51 +0000

    Comment out broken pragma section
    This section of defines and pragmas is new to this version and
    breaks on my raspberry pi. It doesn't seem to do anything useful
    for us so I removed it.

--- src_c/include/sse2neon.h
+++ src_c/include/sse2neon.h
@@ -75,26 +75,28 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-/* Architecture-specific build options */
-/* FIXME: #pragma GCC push_options is only available on GCC */
-#if defined(__GNUC__)
-#if defined(__arm__) && __ARM_ARCH == 7
-/* According to ARM C Language Extensions Architecture specification,
- * __ARM_NEON is defined to a value indicating the Advanced SIMD (NEON)
- * architecture supported.
- */
-#if !defined(__ARM_NEON) || !defined(__ARM_NEON__)
-#error "You must enable NEON instructions (e.g. -mfpu=neon) to use SSE2NEON."
-#endif
-#pragma GCC push_options
-#pragma GCC target("fpu=neon")
-#elif defined(__aarch64__)
-#pragma GCC push_options
-#pragma GCC target("+simd")
-#else
-#error "Unsupported target. Must be either ARMv7-A+NEON or ARMv8-A."
-#endif
-#endif
+// These cause the build to fail on raspberry pi with 'unsupported target'
+// and don't seem to do anything particularly useful
+///* Architecture-specific build options */
+///* FIXME: #pragma GCC push_options is only available on GCC */
+//#if defined(__GNUC__)
+//#if defined(__arm__) && __ARM_ARCH == 7
+///* According to ARM C Language Extensions Architecture specification,
+// * __ARM_NEON is defined to a value indicating the Advanced SIMD (NEON)
+// * architecture supported.
+// */
+//#if !defined(__ARM_NEON) || !defined(__ARM_NEON__)
+//#error "You must enable NEON instructions (e.g. -mfpu=neon) to use SSE2NEON."
+//#endif
+//#pragma GCC push_options
+//#pragma GCC target("fpu=neon")
+//#elif defined(__aarch64__)
+//#pragma GCC push_options
+//#pragma GCC target("+simd")
+//#else
+//#error "Unsupported target. Must be either ARMv7-A+NEON or ARMv8-A."
+//#endif
+//#endif
 
 #include <arm_neon.h>
 
