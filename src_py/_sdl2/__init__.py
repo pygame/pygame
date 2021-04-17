9ec04e52d8caddfbbe1ded34239a5842a4dd6912
Starbuck5 <charlie.c.hayden@gmail.com>
2021-04-16 17:01:30 -0700

    Make namespace as similar as possible

--- src_py/_sdl2/__init__.py
+++ src_py/_sdl2/__init__.py
@@ -7,4 +7,6 @@ from .cyvideo import * # pylint: disable=wildcard-import; lgtm[py/polluting-impo
 for attribute in dir(cyvideo):
     if not attribute.startswith("_"):
         setattr(video, attribute, getattr(cyvideo, attribute))
+
+del cyvideo #remove from namespace
     
