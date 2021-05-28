ca3f46fe16a05b4732865bbd1c4e10419f8db6ed
odidev <odidev@puresoftware.com>
2021-05-28 14:52:09 +0900

    Add aarch64 wheel build support
    Signed-off-by: odidev <odidev@puresoftware.com>

--- buildconfig/manylinux-build/build-wheels.sh
+++ buildconfig/manylinux-build/build-wheels.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e -x
 
-export SUPPORTED_PYTHONS="cp27-cp27mu cp35-cp35m cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39"
+if [ `uname -m` == "aarch64" ]; then
+   export SUPPORTED_PYTHONS="cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39" 
+else
+   export SUPPORTED_PYTHONS="cp27-cp27mu cp35-cp35m cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39" 
+fi
 
 if [[ "$1" == "buildpypy" ]]; then
 	export SUPPORTED_PYTHONS="pp27-pypy_73 pp36-pypy36_pp73 pp37-pypy37_pp73"
