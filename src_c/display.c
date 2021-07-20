d7bd5b141def5e42401ef2c5480d1bf7ae85b1db
Starbuck5 <charlie.c.hayden@gmail.com>
2021-07-20 00:27:43 -0700

    Remove unneeded SDLv2 check
    SDL1 pygame doesn't go into pg_flip_internal, it's all handled in pg_flip. And it was in a huge 1000+ line SDL2 block.

--- src_c/display.c
+++ src_c/display.c
@@ -1685,7 +1685,6 @@ pg_flip_internal(_DisplayState *state)
         SDL_GL_SwapWindow(win);
     }
     else {
-#if IS_SDLv2
         if (pg_renderer != NULL) {
             SDL_Surface *screen =
                 pgSurface_AsSurface(pg_GetDefaultWindowSurface());
@@ -1707,9 +1706,6 @@ pg_flip_internal(_DisplayState *state)
             }
             status = SDL_UpdateWindowSurface(win);
         }
-#else
-        status = SDL_UpdateWindowSurface(win);
-#endif
     }
     Py_END_ALLOW_THREADS;
 
