9b958c339ab7abe9860f634ff51dac84a8476a4a
René Dudfield <renesd@gmail.com>
2021-08-22 13:01:24 +0200

    2.0.2.dev3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ EXTRAS = {}
 
 METADATA = {
     "name":             "pygame",
-    "version":          "2.0.2.dev2",
+    "version":          "2.0.2.dev3",
     "license":          "LGPL",
     "url":              "https://www.pygame.org",
     "author":           "A community project.",
