b00c1f4239bc203991121d89d3febedf5d237954
rethanon <54457966+rethanon@users.noreply.github.com>
2021-08-29 22:52:16 +0100

    Update to docs for mixer and surface
    Minor updates to docs for mixer and surface.

--- buildconfig/pygame-stubs/mixer.pyi
+++ buildconfig/pygame-stubs/mixer.pyi
@@ -19,7 +19,7 @@ def init(
     buffer: int = 512,
     devicename: Optional[str] = None,
     allowedchanges: int = 5,
-) -> True: ...
+) -> bool: ...
 def pre_init(
     frequency: int = 44100,
     size: int = -16,
