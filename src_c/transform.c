5f787bad638e8b30b0c11681f562a2b78c0f2301
Josip Komljenović <josip.komljenovic@fer.hr>
2021-09-12 10:54:46 +0200

    Fix rotate
    2nd try

--- src_c/transform.c
+++ src_c/transform.c
@@ -662,7 +662,8 @@ surf_rotate(PyObject *self, PyObject *arg)
         return NULL;
     surf = pgSurface_AsSurface(surfobj);
     if (surf->w < 1 || surf->h < 1) {
-        return (PyObject *)pgSurface_New(surf);
+        Py_INCREF(surfobj);
+        return surfobj;
     }
 
     if (surf->format->BytesPerPixel == 0 || surf->format->BytesPerPixel > 4)
