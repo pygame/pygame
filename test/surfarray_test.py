3ab21c17ff88b9ee12bee17ab7b8add48c54d7be
bydariogamer <bydariogamer@inventati.org>
2021-09-21 21:44:50 +0200

    add tests for `surfarray`'s new `array_` functions

--- test/surfarray_test.py
+++ test/surfarray_test.py
@@ -19,8 +19,6 @@ from pygame.locals import *
 
 import pygame.surfarray
 
-arraytype = "numpy"
-
 
 IS_PYPY = "PyPy" == platform.python_implementation()
 
@@ -65,9 +63,6 @@ class SurfarrayModuleTest(unittest.TestCase):
         # case a test calls pygame.quit()).
         if not pygame.get_init():
             pygame.init()
-
-        # Makes sure the same array package is used each time.
-        pygame.surfarray.use_arraytype(arraytype)
 
     def _make_surface(self, bitsize, srcalpha=False, palette=None):
         if palette is None:
@@ -317,20 +312,50 @@ class SurfarrayModuleTest(unittest.TestCase):
                         ),
                     )
 
+    def test_array_red(self):
+        self._test_array_rgb("red", 0)
+
+    def test_array_green(self):
+        self._test_array_rgb("green", 1)
+
+    def test_array_blue(self):
+        self._test_array_rgb("blue", 2)
+
+    def _test_array_rgb(self, operation, mask_posn):
+        method_name = "array_" + operation
+
+        array_rgb = getattr(pygame.surfarray, method_name)
+        palette = [
+            (0, 0, 0, 255),
+            (5, 13, 23, 255),
+            (29, 31, 37, 255),
+            (131, 157, 167, 255),
+            (179, 191, 251, 255),
+        ]
+        plane = [c[mask_posn] for c in palette]
+
+        targets = [
+            self._make_src_surface(24, palette=palette),
+            self._make_src_surface(32, palette=palette),
+            self._make_src_surface(32, palette=palette, srcalpha=True),
+        ]
+
+        for surf in targets:
+            self.assertFalse(surf.get_locked())
+            for (x, y), i in self.test_points:
+                surf.fill(palette[i])
+                arr = array_rgb(surf)
+                self.assertEqual(arr[x, y], plane[i])
+                surf.fill((100, 100, 100, 250))
+                self.assertEqual(arr[x, y], plane[i])
+                self.assertFalse(surf.get_locked())
+                del arr
+
     def test_blit_array(self):
 
-        # bug 24 at http://pygame.motherhamster.org/bugzilla/
-        if "numpy" in pygame.surfarray.get_arraytypes():
-            prev = pygame.surfarray.get_arraytype()
-            # This would raise exception:
-            #  File "[...]\pygame\_numpysurfarray.py", line 381, in blit_array
-            #    (array[:,:,1::3] >> losses[1] << shifts[1]) | \
-            # TypeError: unsupported operand type(s) for >>: 'float' and 'int'
-            pygame.surfarray.use_arraytype("numpy")
-            s = pygame.Surface((10, 10), 0, 24)
-            a = pygame.surfarray.array3d(s)
-            pygame.surfarray.blit_array(s, a)
-            prev = pygame.surfarray.use_arraytype(prev)
+        s = pygame.Surface((10, 10), 0, 24)
+        a = pygame.surfarray.array3d(s)
+        pygame.surfarray.blit_array(s, a)
 
         # target surfaces
         targets = [
@@ -477,11 +502,13 @@ class SurfarrayModuleTest(unittest.TestCase):
                             surf.get_at_mapped((x, y)), int(rint(farr[x, y]))
                         )
 
+    # this test should be removed soon, when the function is deleted
     def test_get_arraytype(self):
         array_type = pygame.surfarray.get_arraytype()
 
         self.assertEqual(array_type, "numpy", "unknown array type %s" % array_type)
 
+    # this test should be removed soon, when the function is deleted
     def test_get_arraytypes(self):
 
         arraytypes = pygame.surfarray.get_arraytypes()
