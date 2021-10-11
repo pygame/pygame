b020f4fc2560dbd2f5f4a6dfce164268ae3f261f
Starbuck5 <46412508+Starbuck5@users.noreply.github.com>
2021-10-11 00:26:13 -0700

    2.0.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ EXTRAS = {}
 
 METADATA = {
     "name":             "pygame",
-    "version":          "2.0.2",
+    "version":          "2.0.3.dev1",
     "license":          "LGPL",
     "url":              "https://www.pygame.org",
     "author":           "A community project.",
