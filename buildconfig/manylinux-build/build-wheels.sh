0298d26d82a8cca09920fffbfeb1b7ccc6696a64
René Dudfield <renesd@gmail.com>
2021-10-23 13:22:07 +0200

    Add python 3.10 building in manylinux.

--- buildconfig/manylinux-build/build-wheels.sh
+++ buildconfig/manylinux-build/build-wheels.sh
@@ -2,13 +2,13 @@
 set -e -x
 
 if [ `uname -m` == "aarch64" ]; then
-   export SUPPORTED_PYTHONS="cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39" 
+   export SUPPORTED_PYTHONS="cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310"
 else
-   export SUPPORTED_PYTHONS="cp27-cp27mu cp35-cp35m cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39" 
+   export SUPPORTED_PYTHONS="cp27-cp27mu cp35-cp35m cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39"
 fi
 
 if [[ "$1" == "buildpypy" ]]; then
-	export SUPPORTED_PYTHONS="pp27-pypy_75 pp37-pypy37_pp75"
+	export SUPPORTED_PYTHONS="pp27-pypy_75 pp37-pypy37_pp75 cp310-cp310"
 fi
 
 
