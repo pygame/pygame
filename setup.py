02967fc3c80b80f548937dae537511666db9c142
Robert Pfeiffer <pfeiffer.robert@gmail.com>
2021-11-02 09:28:31 +0100

    Remove references to SDL1 from setup/buildconfig
    let's see if this still runs...

--- setup.py
+++ setup.py
@@ -122,14 +122,11 @@ def compilation_help():
 
 
 
-if not hasattr(sys, 'version_info') or sys.version_info < (2,7):
-    compilation_help()
-    raise SystemExit("Pygame requires Python version 2.7 or above.")
-if sys.version_info >= (3, 0) and sys.version_info < (3, 4):
+if not hasattr(sys, 'version_info') or sys.version_info < (3, 5):
     compilation_help()
     raise SystemExit("Pygame requires Python3 version 3.5 or above.")
 if IS_PYPY and sys.pypy_version_info < (7,):
-    raise SystemExit("Pygame requires PyPy version 7.0.0 above, compatible with CPython 2.7 or CPython 3.5+")
+    raise SystemExit("Pygame requires PyPy version 7.0.0 above, compatible with CPython 3.5+")
 
 def consume_arg(name):
     if name in sys.argv:
@@ -167,12 +164,6 @@ if consume_arg('-pygame-ci'):
               '-Werror=cast-align -Werror=int-conversion ' + \
               '-Werror=incompatible-pointer-types'
     os.environ['CFLAGS'] = cflags
-
-# For python 2 we remove the -j options.
-if sys.version_info[0] < 3:
-    # Used for parallel builds with setuptools. Not supported by py2.
-    [consume_arg('-j%s' % x) for x in range(32)]
-
 
 STRIPPED=False
 
@@ -406,9 +397,9 @@ if AUTO_CONFIG or not os.path.isfile('Setup'):
 
 try:
     s_mtime = os.stat("Setup")[stat.ST_MTIME]
-    sin_mtime = os.stat(os.path.join('buildconfig', 'Setup.SDL1.in'))[stat.ST_MTIME]
+    sin_mtime = os.stat(os.path.join('buildconfig', 'Setup.SDL2.in'))[stat.ST_MTIME]
     if sin_mtime > s_mtime:
-        print ('\n\nWARNING, "buildconfig/Setup.SDL1.in" newer than "Setup",'
+        print ('\n\nWARNING, "buildconfig/Setup.SDL2.in" newer than "Setup",'
                'you might need to modify "Setup".')
 except OSError:
     pass
