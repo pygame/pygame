8fed549341ce45fa19115901e756623d69b1766a
bydariogamer <bydariogamer@inventati.org>
2021-11-12 09:40:51 +0100

    skip some display tests until issues are solved

--- test/display_test.py
+++ test/display_test.py
@@ -171,22 +171,21 @@ class DisplayModuleTest(unittest.TestCase):
         self.assertTrue(display.get_init())
 
     # This test can be uncommented when issues #991 and #993 are resolved.
-    #
-    # @unittest.skipIf(SDL2, "SDL2 issues")
-    # def test_get_surface(self):
-    #     """Ensures get_surface gets the current display surface."""
-    #     lengths = (1, 5, 100)
-    #
-    #     for expected_size in ((w, h) for w in lengths for h in lengths):
-    #         for expected_depth in (8, 16, 24, 32):
-    #             expected_surface = display.set_mode(expected_size, 0, expected_depth)
-    #
-    #             surface = pygame.display.get_surface()
-    #
-    #             self.assertEqual(surface, expected_surface)
-    #             self.assertIsInstance(surface, pygame.Surface)
-    #             self.assertEqual(surface.get_size(), expected_size)
-    #             self.assertEqual(surface.get_bitsize(), expected_depth)
+    @unittest.skipIf(True, "SDL2 issues")
+    def test_get_surface(self):
+        """Ensures get_surface gets the current display surface."""
+        lengths = (1, 5, 100)
+
+        for expected_size in ((w, h) for w in lengths for h in lengths):
+            for expected_depth in (8, 16, 24, 32):
+                expected_surface = display.set_mode(expected_size, 0, expected_depth)
+
+                surface = pygame.display.get_surface()
+
+                self.assertEqual(surface, expected_surface)
+                self.assertIsInstance(surface, pygame.Surface)
+                self.assertEqual(surface.get_size(), expected_size)
+                self.assertEqual(surface.get_bitsize(), expected_depth)
 
     def test_get_surface__mode_not_set(self):
         """Ensures get_surface handles the display mode not being set."""
@@ -569,34 +568,34 @@ class DisplayModuleTest(unittest.TestCase):
         self.assertTrue(pygame.display.get_allow_screensaver())
 
     # the following test fails always with SDL2
-    # @unittest.skipIf(SDL2, "set_palette() not supported in SDL2")
-    # def test_set_palette(self):
-    #     with self.assertRaises(pygame.error):
-    #         palette = [1, 2, 3]
-    #         pygame.display.set_palette(palette)
-    #     pygame.display.set_mode((1024, 768), 0, 8)
-    #     palette = []
-    #     self.assertIsNone(pygame.display.set_palette(palette))
-    #
-    #     with self.assertRaises(ValueError):
-    #         palette = 12
-    #         pygame.display.set_palette(palette)
-    #     with self.assertRaises(TypeError):
-    #         palette = [[1, 2], [1, 2]]
-    #         pygame.display.set_palette(palette)
-    #     with self.assertRaises(TypeError):
-    #         palette = [[0, 0, 0, 0, 0]] + [[x, x, x, x, x]
-    #                                        for x in range(1, 255)]
-    #         pygame.display.set_palette(palette)
-    #     with self.assertRaises(TypeError):
-    #         palette = "qwerty"
-    #         pygame.display.set_palette(palette)
-    #     with self.assertRaises(TypeError):
-    #         palette = [[123, 123, 123]*10000]
-    #         pygame.display.set_palette(palette)
-    #     with self.assertRaises(TypeError):
-    #         palette = [1, 2, 3]
-    #         pygame.display.set_palette(palette)
+    @unittest.skipIf(True, "set_palette() not supported in SDL2")
+    def test_set_palette(self):
+        with self.assertRaises(pygame.error):
+            palette = [1, 2, 3]
+            pygame.display.set_palette(palette)
+        pygame.display.set_mode((1024, 768), 0, 8)
+        palette = []
+        self.assertIsNone(pygame.display.set_palette(palette))
+
+        with self.assertRaises(ValueError):
+            palette = 12
+            pygame.display.set_palette(palette)
+        with self.assertRaises(TypeError):
+            palette = [[1, 2], [1, 2]]
+            pygame.display.set_palette(palette)
+        with self.assertRaises(TypeError):
+            palette = [[0, 0, 0, 0, 0]] + [[x, x, x, x, x]
+                                           for x in range(1, 255)]
+            pygame.display.set_palette(palette)
+        with self.assertRaises(TypeError):
+            palette = "qwerty"
+            pygame.display.set_palette(palette)
+        with self.assertRaises(TypeError):
+            palette = [[123, 123, 123]*10000]
+            pygame.display.set_palette(palette)
+        with self.assertRaises(TypeError):
+            palette = [1, 2, 3]
+            pygame.display.set_palette(palette)
 
     skip_list = ["dummy", "android"]
     @unittest.skipIf(
