7d4b84abd79a1504cb985f62911860c9eb31a8e2
JoshuaShequin <36478500+JoshuaShequin@users.noreply.github.com>
2021-12-11 16:12:22 -0800

    Added Simple test from MyreMylar for unpause PR
    Added Simple test from MyreMylar for the unpause PR 2899. The test makes sure that the get_pos() function returns the same time before and immediately after and unpause.

--- test/mixer_music_test.py
+++ test/mixer_music_test.py
@@ -4,6 +4,7 @@ import os
 import sys
 import platform
 import unittest
+import time
 
 from pygame.tests.test_utils import example_path
 import pygame
@@ -177,6 +178,26 @@ class MixerMusicModuleTest(unittest.TestCase):
         """Ensures queue() correctly handles invalid filenames."""
         with self.assertRaises(pygame.error):
             pygame.mixer.music.queue("")
+
+    def test_music_pause__unpause(self):
+        """Ensure music has the correct position immediately after unpausing
+
+        |tags:music|
+        """
+        filename = example_path(os.path.join("data", "house_lo.mp3"))
+        pygame.mixer.music.load(filename)
+        pygame.mixer.music.play()
+
+        # Wait 0.5s, then pause
+        time.sleep(0.5)
+        pygame.mixer.music.pause()
+        # Wait 0.5s, get position, unpause, then get position again
+        time.sleep(0.5)
+        before_unpause = pygame.mixer.music.get_pos()
+        pygame.mixer.music.unpause()
+        after_unpause = pygame.mixer.music.get_pos()
+
+        self.assertEqual(before_unpause, after_unpause)
 
     def todo_test_stop(self):
 
