9d53d5a0f9a028accc9341c3955423842385435d
René Dudfield <renesd@gmail.com>
2021-12-24 02:21:46 +0100

    Some formatting issues crept in

--- test/display_test.py
+++ test/display_test.py
@@ -487,15 +487,12 @@ class DisplayModuleTest(unittest.TestCase):
                 self.assertEqual(pygame.display.set_gamma(gamma), True)
 
     @unittest.skipIf(
-        os.environ.get("SDL_VIDEODRIVER") == "dummy",
-        "Needs a not dummy videodriver"
+        os.environ.get("SDL_VIDEODRIVER") == "dummy", "Needs a not dummy videodriver"
     )
     def test_set_gamma__tuple(self):
         pygame.display.set_mode((1, 1))
 
-        gammas = [(0.5, 0.5, 0.5),
-                  (1.0, 1.0, 1.0),
-                  (0.25, 0.33, 0.44)]
+        gammas = [(0.5, 0.5, 0.5), (1.0, 1.0, 1.0), (0.25, 0.33, 0.44)]
         for r, g, b in gammas:
             with self.subTest(r=r, g=g, b=b):
                 self.assertEqual(pygame.display.set_gamma(r, g, b), True)
