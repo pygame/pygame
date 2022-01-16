56da8dea19df14c862a9fe2847c744d8f162d3e5
dr0id <dr0id@bluewin.ch>
2022-01-16 21:11:07 +0100

    rect.collidelistall: add more unittest for different ways to define a rect in the list (tuple, list, double tuple, rect attribute, callable rect attibute).

--- test/rect_test.py
+++ test/rect_test.py
@@ -2005,6 +2005,145 @@ class RectTypeTest(unittest.TestCase):
         f = [Rect(50, 50, 1, 1), Rect(20, 20, 5, 5)]
         self.assertFalse(r.collidelistall(f))
 
+    def test_collidelistall_list_of_tuples(self):
+
+        # __doc__ (as of 2008-08-02) for pygame.rect.Rect.collidelistall:
+
+        # Rect.collidelistall(list): return indices
+        # test if all rectangles in a list intersect
+        #
+        # Returns a list of all the indices that contain rectangles that
+        # collide with the Rect. If no intersecting rectangles are found, an
+        # empty list is returned.
+
+        r = Rect(1, 1, 10, 10)
+
+        l = [
+            (1, 1, 10, 10),
+            (5, 5, 10, 10),
+            (15, 15, 1, 1),
+            (2, 2, 1, 1),
+        ]
+        self.assertEqual(r.collidelistall(l), [0, 1, 3])
+
+        f = [(50, 50, 1, 1), (20, 20, 5, 5)]
+        self.assertFalse(r.collidelistall(f))
+
+    def test_collidelistall_list_of_two_tuples(self):
+
+        # __doc__ (as of 2008-08-02) for pygame.rect.Rect.collidelistall:
+
+        # Rect.collidelistall(list): return indices
+        # test if all rectangles in a list intersect
+        #
+        # Returns a list of all the indices that contain rectangles that
+        # collide with the Rect. If no intersecting rectangles are found, an
+        # empty list is returned.
+
+        r = Rect(1, 1, 10, 10)
+
+        l = [
+            ((1, 1), (10, 10)),
+            ((5, 5), (10, 10)),
+            ((15, 15), (1, 1)),
+            ((2, 2), (1, 1)),
+        ]
+        self.assertEqual(r.collidelistall(l), [0, 1, 3])
+
+        f = [((50, 50), (1, 1)), ((20, 20), (5, 5))]
+        self.assertFalse(r.collidelistall(f))
+
+    def test_collidelistall_list_of_lists(self):
+
+        # __doc__ (as of 2008-08-02) for pygame.rect.Rect.collidelistall:
+
+        # Rect.collidelistall(list): return indices
+        # test if all rectangles in a list intersect
+        #
+        # Returns a list of all the indices that contain rectangles that
+        # collide with the Rect. If no intersecting rectangles are found, an
+        # empty list is returned.
+
+        r = Rect(1, 1, 10, 10)
+
+        l = [
+            [1, 1, 10, 10],
+            [5, 5, 10, 10],
+            [15, 15, 1, 1],
+            [2, 2, 1, 1],
+        ]
+        self.assertEqual(r.collidelistall(l), [0, 1, 3])
+
+        f = [[50, 50, 1, 1], [20, 20, 5, 5]]
+        self.assertFalse(r.collidelistall(f))
+
+    class _ObjectWithRectAttribute(object):
+
+        def __init__(self, r):
+            self.rect = r
+
+    def test_collidelistall_list_of_object_with_rect_attribute(self):
+
+        # __doc__ (as of 2008-08-02) for pygame.rect.Rect.collidelistall:
+
+        # Rect.collidelistall(list): return indices
+        # test if all rectangles in a list intersect
+        #
+        # Returns a list of all the indices that contain rectangles that
+        # collide with the Rect. If no intersecting rectangles are found, an
+        # empty list is returned.
+
+        r = Rect(1, 1, 10, 10)
+
+        l = [
+            self._ObjectWithRectAttribute(Rect(1, 1, 10, 10)),
+            self._ObjectWithRectAttribute(Rect(5, 5, 10, 10)),
+            self._ObjectWithRectAttribute(Rect(15, 15, 1, 1)),
+            self._ObjectWithRectAttribute(Rect(2, 2, 1, 1)),
+        ]
+        self.assertEqual(r.collidelistall(l), [0, 1, 3])
+
+        f = [
+            self._ObjectWithRectAttribute(Rect(50, 50, 1, 1)),
+            self._ObjectWithRectAttribute(Rect(20, 20, 5, 5))
+        ]
+        self.assertFalse(r.collidelistall(f))
+
+    class _ObjectWithCallableRectAttribute(object):
+
+        def __init__(self, r):
+            self._rect = r
+
+        def rect(self):
+            return self._rect
+
+    def test_collidelistall_list_of_object_with_callable_rect_attribute(self):
+
+        # __doc__ (as of 2008-08-02) for pygame.rect.Rect.collidelistall:
+
+        # Rect.collidelistall(list): return indices
+        # test if all rectangles in a list intersect
+        #
+        # Returns a list of all the indices that contain rectangles that
+        # collide with the Rect. If no intersecting rectangles are found, an
+        # empty list is returned.
+
+        r = Rect(1, 1, 10, 10)
+
+        l = [
+            self._ObjectWithCallableRectAttribute(Rect(1, 1, 10, 10)),
+            self._ObjectWithCallableRectAttribute(Rect(5, 5, 10, 10)),
+            self._ObjectWithCallableRectAttribute(Rect(15, 15, 1, 1)),
+            self._ObjectWithCallableRectAttribute(Rect(2, 2, 1, 1)),
+        ]
+        self.assertEqual(r.collidelistall(l), [0, 1, 3])
+
+        f = [
+            self._ObjectWithCallableRectAttribute(Rect(50, 50, 1, 1)),
+            self._ObjectWithCallableRectAttribute(Rect(20, 20, 5, 5))
+        ]
+        self.assertFalse(r.collidelistall(f))
+
     def test_fit(self):
 
         # __doc__ (as of 2008-08-02) for pygame.rect.Rect.fit:
