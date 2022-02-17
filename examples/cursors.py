4108046c1b4fcd7573e66fb2ae62ee093f8aa606
thanice <athamosc@csd.auth.gr>
2022-02-17 19:34:39 +0200

    update cursors example and add new types of cursors

--- examples/cursors.py
+++ examples/cursors.py
@@ -1,104 +1,129 @@
 #!/usr/bin/env python
 """ pygame.examples.cursors
 
-Click a mouse button (if you have one!) and the cursor changes.
+Click a mouse button and the cursor will change.
+
+This example will show you:
+*The different types of cursors that exist
+*How to create a cursor
+*How to set a cursor
 
 """
 import pygame as pg
+import os
 
+#Create bitmap cursor from simple strings
 
-arrow = (
-    "xX                      ",
-    "X.X                     ",
-    "X..X                    ",
-    "X...X                   ",
-    "X....X                  ",
-    "X.....X                 ",
-    "X......X                ",
-    "X.......X               ",
-    "X........X              ",
-    "X.........X             ",
-    "X......XXXXX            ",
-    "X...X..X                ",
-    "X..XX..X                ",
-    "X.X XX..X               ",
-    "XX   X..X               ",
-    "X     X..X              ",
-    "      X..X              ",
-    "       X..X             ",
-    "       X..X             ",
-    "        XX              ",
+# sized 24x24
+thickarrow_strings = (
+    "XX                      ",
+    "XXX                     ",
+    "XXXX                    ",
+    "XX.XX                   ",
+    "XX..XX                  ",
+    "XX...XX                 ",
+    "XX....XX                ",
+    "XX.....XX               ",
+    "XX......XX              ",
+    "XX.......XX             ",
+    "XX........XX            ",
+    "XX........XXX           ",
+    "XX......XXXXX           ",
+    "XX.XXX..XX              ",
+    "XXXX XX..XX             ",
+    "XX   XX..XX             ",
+    "     XX..XX             ",
+    "      XX..XX            ",
+    "      XX..XX            ",
+    "       XXXX             ",
+    "       XX               ",
+    "                        ",
+    "                        ",
+    "                        ",
+)
+
+bitmapCursor1 = pg.cursors.Cursor(
+    (24, 24), (0, 0), *pg.cursors.compile(thickarrow_strings, black='X', white='.', xor='o') 
+)
+
+# sized 24x16
+sizer_x_strings = (
+    "     X      X           ",
+    "    XX      XX          ",
+    "   X.X      X.X         ",
+    "  X..X      X..X        ",
+    " X...XXXXXXXX...X       ",
+    "X................X      ",
+    " X...XXXXXXXX...X       ",
+    "  X..X      X..X        ",
+    "   X.X      X.X         ",
+    "    XX      XX          ",
+    "     X      X           ",
+    "                        ",
     "                        ",
     "                        ",
     "                        ",
     "                        ",
 )
 
-
-no = (
-    "                        ",
-    "                        ",
-    "         XXXXXX         ",
-    "       XX......XX       ",
-    "      X..........X      ",
-    "     X....XXXX....X     ",
-    "    X...XX    XX...X    ",
-    "   X.....X      X...X   ",
-    "   X..X...X      X..X   ",
-    "  X...XX...X     X...X  ",
-    "  X..X  X...X     X..X  ",
-    "  X..X   X...X    X..X  ",
-    "  X..X    X.,.X   X..X  ",
-    "  X..X     X...X  X..X  ",
-    "  X...X     X...XX...X  ",
-    "   X..X      X...X..X   ",
-    "   X...X      X.....X   ",
-    "    X...XX     X...X    ",
-    "     X....XXXXX...X     ",
-    "      X..........X      ",
-    "       XX......XX       ",
-    "         XXXXXX         ",
-    "                        ",
-    "                        ",
+bitmapCursor2 = pg.cursors.Cursor(
+    (24, 16), (0, 0), *pg.cursors.compile(sizer_x_strings, black='X', white='.', xor='o') 
 )
 
 
-def TestCursor(arrow):
-    hotspot = None
-    for y, line in enumerate(arrow):
-        for x, char in enumerate(line):
-            if char in ["x", ",", "O"]:
-                hotspot = x, y
-                break
-        if hotspot is not None:
-            break
-    if hotspot is None:
-        raise Exception("No hotspot specified for cursor '%s'!" % arrow)
-    s2 = []
-    for line in arrow:
-        s2.append(line.replace("x", "X").replace(",", ".").replace("O", "o"))
-    cursor, mask = pg.cursors.compile(s2, "X", ".", "o")
-    size = len(arrow[0]), len(arrow)
-    pg.mouse.set_cursor(size, hotspot, cursor, mask)
+#Create bitmap cursor from premade simple strings
 
+bitmapCursor3 = pg.cursors.diamond
+
+
+#Create a system cursor
+
+systemCursor = pg.SYSTEM_CURSOR_CROSSHAIR
+
+
+#Create color cursor
+
+surf = pg.Surface((40, 40)) 
+surf.fill((120, 50, 50))     
+colorCursor = pg.cursors.Cursor((20, 20), surf)
+
+
+#Load an image and use it as cursor surface
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+imagename = os.path.join(main_dir, "data", "cursor.png")
+image = pg.image.load(imagename)
+
+imageCursor = pg.cursors.Cursor((0, 0), image)
 
 def main():
     pg.init()
     pg.font.init()
-    font = pg.font.Font(None, 24)
-    bg = pg.display.set_mode((800, 600), 0, 24)
-    bg.fill((255, 255, 255))
-    bg.blit(font.render("Click to advance", 1, (0, 0, 0)), (0, 0))
+    font = pg.font.Font(None, 30)
+    bg = pg.display.set_mode((400, 300))
+    bg.fill((220,220,220))
+    text = font.render("Click to change cursor", True, (0, 0, 0))
+    text_rect = text.get_rect(center=(200, 130))
+    bg.blit(text, text_rect)
+    pg.display.set_caption("Cursors Example")
     pg.display.update()
-    for cursor in [no, arrow]:
-        TestCursor(cursor)
-        going = True
-        while going:
-            pg.event.pump()
-            for e in pg.event.get():
-                if e.type == pg.MOUSEBUTTONDOWN:
-                    going = False
-    pg.quit()
+
+    cursors = [bitmapCursor1, bitmapCursor2, bitmapCursor3, systemCursor, colorCursor, imageCursor]
+    index = 0
+    
+    pg.mouse.set_cursor(cursors[index])
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.MOUSEBUTTONDOWN:
+                index += 1
+                index %= len(cursors)
+                pg.mouse.set_cursor(cursors[index])
+
+            if event.type == pg.QUIT:
+                pg.quit()
+                raise SystemExit
+
 
 
 if __name__ == "__main__":
