e1523a95d6157ab833f21b572be92bd013ccc965
Ankith <46915066+ankith26@users.noreply.github.com>
2022-04-15 13:39:13 +0530

    make _Locale private in the typestubs

--- buildconfig/stubs/pygame/context.pyi
+++ buildconfig/stubs/pygame/context.pyi
@@ -3,9 +3,9 @@ from typing import List, Optional
 from typing_extensions import TypedDict
 
 # dict at runtime, TypedDict exists solely for the typechecking benefits
-class Locale(TypedDict):
+class _Locale(TypedDict):
     language: str
     country: Optional[str]
 
 def get_pref_path(org: str, app: str) -> str: ...
-def get_pref_locales() -> List[Locale]: ...
+def get_pref_locales() -> List[_Locale]: ...
