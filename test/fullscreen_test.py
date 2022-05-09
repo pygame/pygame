2a8cc6cfd82de94518ca894def54e610ad1c6d5c
Robert O'Shea <robertoshea2k11@gmail.com>
2022-05-09 14:20:50 +0100

    Added visual test for `toggle_fullscreen`

---
+++ test/fullscreen_test.py
@@ -0,0 +1,68 @@
+import unittest
+
+import pygame
+from pygame.tests import test_utils
+
+class VisualTests(unittest.TestCase):
+    __tags__ = [ "interactive" ]
+
+    screen = None
+    font = None
+    isfullscreen = False
+    aborted = False
+
+    WIDTH = 800
+    HEIGHT = 600
+
+    def setUp(self):
+        if self.screen is None:
+            pygame.init()
+            self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+            pygame.display.set_caption("Fullscreen Tests")
+            self.screen.fill((255, 255, 255))
+            pygame.display.flip()
+            self.font = pygame.font.Font(None, 32)
+
+    def quit(self):
+        if self.screen is not None:
+            if self.isfullscreen:
+                pygame.display.toggle_fullscreen()
+            pygame.quit()
+        self.aborted = True
+    
+    def visual_test(self, fullscreen=False):
+        if self.aborted:
+            return False
+        text = ""
+        if fullscreen:
+            text = "Is this in fullscreen? [y/n]"
+            self.isfullscreen = True
+            pygame.display.toggle_fullscreen()
+        else:
+            text = "Is this not in fullscreen [y/n]"
+        s = self.font.render(text, False, (0, 0, 0))
+        self.screen.blit(s, (self.WIDTH / 2 - self.font.size(text)[0] / 2, 100))
+        pygame.display.flip()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.quit()
+                        return False
+                    if event.key == pygame.K_y:
+                        return True
+                    if event.key == pygame.K_n:
+                        return False
+                if event.type == pygame.QUIT:
+                    self.quit()
+                    return False
+
+    def test_fullscreen_true(self):
+        self.assertTrue(self.visual_test(fullscreen=True))
+
+    def test_fullscreen_false(self):
+        self.assertTrue(self.visual_test(fullscreen=False))
+
+if __name__ == "__main__":
+    unittest.main()
