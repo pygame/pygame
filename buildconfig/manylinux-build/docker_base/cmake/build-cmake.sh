f9e431ab817b807bdb44a16af03a1782f4f74e8e
Ankith <46915066+ankith26@users.noreply.github.com>
2022-05-10 13:27:53 +0530

    Update cmake (use pip install)

--- buildconfig/manylinux-build/docker_base/cmake/build-cmake.sh
+++ buildconfig/manylinux-build/docker_base/cmake/build-cmake.sh
@@ -3,25 +3,21 @@ set -e -x
 
 cd $(dirname `readlink -f "$0"`)
 
-# The latest cmake doesn't compile on centos 5.
-# This version does, and works with latest fluidsynth(2.2.3).
-CMAKE=cmake-3.12.4
+# The latest cmake doesn't easily compile from source on centos 5
+# So cmake is installed with pip
+# This way we save compile time, reduce maintenance+scripting efforts and also 
+# get the right binaries of the latest cmake version that will work on centos 5
+# and above (the pip cmake package provides manylinux1 i686/x86_64 and 
+# manylinux2014 i686/x86_64/aarch64 wheels)
 
-if [ ! -d $CMAKE ]; then
+# this file is only intended to work in our manylinux build system and not on
+# MacOS, which has a modern enough cmake already
 
-	curl -sL --retry 10 https://cmake.org/files/v3.12/${CMAKE}.tar.gz > ${CMAKE}.tar.gz
-	sha512sum -c cmake.sha512
+# any cpython version can be used here 
+# (this must be updated when we drop 3.10 support after a few years)
+PYTHON_VER=cp310-cp310
+PYTHON_BIN=/opt/python/${PYTHON_VER}/bin
 
-	tar xzf ${CMAKE}.tar.gz
-fi
-cd $CMAKE
-
-sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake &&
-
-./bootstrap --prefix=/usr        \
-            --mandir=/share/man  \
-            --no-system-jsoncpp  \
-            --no-system-librhash \
-            --docdir=/share/doc/cmake-3.12.4 &&
-make
-make install
+# this installs cmake in python bin dir, copy it to /usr/bin once installed
+${PYTHON_BIN}/pip install cmake
+cp ${PYTHON_BIN}/cmake /usr/bin
