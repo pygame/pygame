0047998961968f4d2bc83fea8ff72d9bce7a11c7
itzpr3d4t0r <103119829+itzpr3d4t0r@users.noreply.github.com>
2022-06-26 14:30:15 +0200

    Added test_ublits test to blit_test.py

--- test/blit_test.py
+++ test/blit_test.py
@@ -130,6 +130,54 @@ class BlitTest(unittest.TestCase):
         if PRINT_TIMING:
             print(f"Surface.blits generator: {t1 - t0}")
 
+    def test_ublits(self):
+
+        NUM_SURFS = 255
+        PRINT_TIMING = 0
+        dst = pygame.Surface((NUM_SURFS * 10, 10), SRCALPHA, 32)
+        dst.fill((230, 230, 230))
+        blit_list = self.make_blit_list(NUM_SURFS)
+
+        def blits(blit_list):
+            for surface, dest in blit_list:
+                dst.blit(surface, dest)
+
+        from time import time
+
+        t0 = time()
+        results = blits(blit_list)
+        t1 = time()
+        if PRINT_TIMING:
+            print(f"python blits: {t1 - t0}")
+
+        dst.fill((230, 230, 230))
+        t0 = time()
+        results = dst.ublits(blit_list)
+        t1 = time()
+        if PRINT_TIMING:
+            print(f"Surface.ublits :{t1 - t0}")
+
+        # check if we blit all the different colors in the correct spots.
+        for i in range(NUM_SURFS):
+            color = (i * 1, i * 1, i * 1)
+            self.assertEqual(dst.get_at((i * 10, 0)), color)
+            self.assertEqual(dst.get_at(((i * 10) + 5, 5)), color)
+
+        self.assertEqual(len(results), NUM_SURFS)
+
+        t0 = time()
+        results = dst.ublits(blit_list, doreturn=0)
+        t1 = time()
+        if PRINT_TIMING:
+            print(f"Surface.ublits doreturn=0: {t1 - t0}")
+        self.assertEqual(results, None)
+
+        t0 = time()
+        results = dst.ublits(((surf, dest) for surf, dest in blit_list))
+        t1 = time()
+        if PRINT_TIMING:
+            print(f"Surface.ublits generator: {t1 - t0}")
+
     def test_blits_not_sequence(self):
         dst = pygame.Surface((100, 10), SRCALPHA, 32)
         self.assertRaises(ValueError, dst.blits, None)
