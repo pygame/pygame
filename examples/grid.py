f1e75eb13b69eff8ba09c648cc9678a5cb4c5f16
Masonstiller <67441648+Masonstiller@users.noreply.github.com>
2022-07-14 10:47:55 -0700

    updated syntax and combined functions
    fixed import statement, combined event and draw functions, reduced game and window size to better fit screen

--- examples/grid.py
+++ examples/grid.py
@@ -1,60 +1,57 @@
-import pygame
+import pygame as pg
 
 TITLE = "Grid"
 TILES_HORIZONTAL = 10
 TILES_VERTICAL = 10
-TILE_SIZE = 128
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 1280
+TILE_SIZE = 100
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 1000
 
 
 class Player:
     def __init__(self, surface):
         self.surface = surface
-        self.pos = (64, 64)
+        self.pos = (50, 50)
 
     def draw(self):
-        pygame.draw.circle(self.surface, (255, 255, 255), self.pos, 64)
+        pg.draw.circle(self.surface, (255, 255, 255), self.pos, 50)
 
     def move(self, target):
-        x = (128 * (target[0] // 128)) + 64
-        y = (128 *(target[1] // 128)) + 64
+        x = (100 * (target[0] // 100)) + 50
+        y = (100 * (target[1] // 100)) + 50
 
         self.pos = (x, y)
 
 
 class Game:
     def __init__(self):
-        pygame.init()
-        self.clock = pygame.time.Clock()
-        pygame.display.set_caption(TITLE)
-        self.surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pg.init()
+        self.clock = pg.time.Clock()
+        pg.display.set_caption(TITLE)
+        self.surface = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.loop = True
         self.player = Player(self.surface)
 
     def main(self):
         while self.loop:
-            self.events()
-            self.draw()
+            self.grid_loop()
 
-    def events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.loop = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    self.loop = False
-            elif event.type == pygame.MOUSEBUTTONUP:
-                pos = pygame.mouse.get_pos()
-                self.player.move(pos)
-
-    def draw(self):
+    def grid_loop(self):
         self.surface.fill((0, 0, 0))
         for row in range(TILES_HORIZONTAL):
             for col in range(row % 2, TILES_HORIZONTAL, 2):
-                pygame.draw.rect(self.surface, (40, 40, 40), (row * TILE_SIZE, col * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+                pg.draw.rect(self.surface, (40, 40, 40), (row * TILE_SIZE, col * TILE_SIZE, TILE_SIZE, TILE_SIZE))
         self.player.draw()
-        pygame.display.update()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.loop = False
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.loop = False
+            elif event.type == pg.MOUSEBUTTONUP:
+                pos = pg.mouse.get_pos()
+                self.player.move(pos)
+        pg.display.update()
 
 
 if __name__ == "__main__":
