ae7ed7ebbb3dc068b1819c08ee7d46c14cdc517b
Starbuck5 <46412508+Starbuck5@users.noreply.github.com>
2022-07-23 01:11:53 -0700

    fix everything

--- test/font_test.py
+++ test/font_test.py
@@ -583,19 +583,26 @@ class FontTypeTest(unittest.TestCase):
             font.render("WHERE", True, "black")
 
     def test_font_set_script(self):
-        font = pygame.font.Font(None, 16)
+        if pygame_font.__name__ == "pygame.ftfont":
+            return  # this ain't a pygame.ftfont thing!
 
-        self.assertRaises(TypeError, pygame.font.Font.set_script)
-        self.assertRaises(TypeError, pygame.font.Font.set_script, font)
-        self.assertRaises(TypeError, pygame.font.Font.set_script, "hey", "Deva")
-        self.assertRaises(TypeError, font.set_script, 1)
-        self.assertRaises(TypeError, font.set_script, ["D", "e", "v", "a"])
+        font = pygame_font.Font(None, 16)
 
-        self.assertRaises(ValueError, font.set_script, "too long by far")
-        self.assertRaises(ValueError, font.set_script, "")
-        self.assertRaises(ValueError, font.set_script, "a")
+        ttf_version = pygame_font.get_sdl_ttf_version()
+        if ttf_version >= (2, 20, 0):
+            self.assertRaises(TypeError, pygame.font.Font.set_script)
+            self.assertRaises(TypeError, pygame.font.Font.set_script, font)
+            self.assertRaises(TypeError, pygame.font.Font.set_script, "hey", "Deva")
+            self.assertRaises(TypeError, font.set_script, 1)
+            self.assertRaises(TypeError, font.set_script, ["D", "e", "v", "a"])
 
-        font.set_script("Deva")
+            self.assertRaises(ValueError, font.set_script, "too long by far")
+            self.assertRaises(ValueError, font.set_script, "")
+            self.assertRaises(ValueError, font.set_script, "a")
+
+            font.set_script("Deva")
+        else:
+            self.assertRaises(pygame.error, font.set_script, "Deva")
 
 
 @unittest.skipIf(IS_PYPY, "pypy skip known failure")  # TODO
