c7f0c7055818d54826c01f415db5667f887d58fc
Rafael Latowicz <latowicz@optusnet.com.au>
2022-07-31 15:30:59 +1000

    change to some variable names in image_test.py, in the BGRA format section

--- test/image_test.py
+++ test/image_test.py
@@ -526,8 +526,8 @@ class ImageModuleTest(unittest.TestCase):
         import itertools
 
         fmts = ("RGBA", "ARGB", "BGRA")
-        fmt_permutations = list(itertools.permutations(fmts, 2))
-        fmt_combinations = list(itertools.combinations(fmts, 2))
+        fmt_permutations = itertools.permutations(fmts, 2)
+        fmt_combinations = itertools.combinations(fmts, 2)
 
         def convert(fmt1, fmt2, str_buf):
             pos_fmt1 = {k: v for v, k in enumerate(fmt1)}
@@ -561,44 +561,44 @@ class ImageModuleTest(unittest.TestCase):
         )
 
         for pair in fmt_combinations:
-            rgba_buf = pygame.image.tostring(test_surface, pair[0])
-            rgba_buf = convert(pair[1], pair[0], convert(pair[0], pair[1], rgba_buf))
-            test_rotate_functions = pygame.image.fromstring(
-                rgba_buf, test_surface.get_size(), pair[0]
+            fmt1_buf = pygame.image.tostring(test_surface, pair[0])
+            fmt1_convert_buf = convert(
+                pair[1], pair[0], convert(pair[0], pair[1], fmt1_buf)
+            )
+            test_convert_two_way = pygame.image.fromstring(
+                fmt1_convert_buf, test_surface.get_size(), pair[0]
             )
 
             self._assertSurfaceEqual(
                 test_surface,
-                test_rotate_functions,
-                "converting {} to {} and back is not symmetric".format(
-                    pair[0], pair[1]
-                ),
+                test_convert_two_way,
+                f"converting {pair[0]} to {pair[1]} and back is not symmetric",
             )
 
         for pair in fmt_permutations:
-            argb_buf = pygame.image.tostring(test_surface, pair[0])
-            rgba_buf = convert(pair[0], pair[1], argb_buf)
-            test_to_argb_string = pygame.image.fromstring(
-                rgba_buf, test_surface.get_size(), pair[1]
+            fmt1_buf = pygame.image.tostring(test_surface, pair[0])
+            fmt2_convert_buf = convert(pair[0], pair[1], fmt1_buf)
+            test_convert_one_way = pygame.image.fromstring(
+                fmt2_convert_buf, test_surface.get_size(), pair[1]
             )
 
             self._assertSurfaceEqual(
                 test_surface,
-                test_to_argb_string,
-                "{} converted to {} failed".format(pair[0], pair[1]),
+                test_convert_one_way,
+                f"converting {pair[0]} to {pair[1]} failed",
             )
 
         for fmt in fmts:
-            fmt_buf = pygame.image.tostring(test_surface, fmt)
+            test_buf = pygame.image.tostring(test_surface, fmt)
             test_to_from_fmt_string = pygame.image.fromstring(
-                fmt_buf, test_surface.get_size(), fmt
+                test_buf, test_surface.get_size(), fmt
             )
 
             self._assertSurfaceEqual(
                 test_surface,
                 test_to_from_fmt_string,
                 "tostring/fromstring functions are not "
-                'symmetric with "{}" format'.format(fmt),
+                f"symmetric with '{fmt}' format",
             )
 
     def test_tostring_depth_24(self):
