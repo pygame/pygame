cacf9fc33235f4a2a5eac3c9b6a6609e821f86b5
Paul m. p. Peny <mail.peny@free.fr>
2022-08-07 18:10:05 +0200

    fix CPython was built with clang instead of gcc
    as clang does not support unused-but-set-variable

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@
 
 import io
 import platform
+import sysconfig
 
 with open('README.rst', encoding='utf-8') as readme:
     LONG_DESCRIPTION = readme.read()
@@ -443,7 +444,8 @@ for e in extensions:
 
     if "freetype" in e.name and sys.platform not in ("darwin", "win32"):
         # TODO: fix freetype issues here
-        e.extra_compile_args.append("-Wno-error=unused-but-set-variable")
+        if sysconfig.get_config_var("MAINCC") != "clang":        
+            e.extra_compile_args.append("-Wno-error=unused-but-set-variable")
 
     if "mask" in e.name and sys.platform == "win32":
         # skip analyze warnings that pop up a lot in mask for now. TODO fix
