b5ef1117feddcca42fe4ce9158e63cd8d1a43dab
DatDerpyDude81 <myanghpms2021@gmail.com>
2022-09-11 08:15:37 -0700

    formatted comments, imported pygame as pg

--- examples/boilerplate.py
+++ examples/boilerplate.py
@@ -1,24 +1,24 @@
-import pygame
+import pygame as pg
 
-pygame.init()
+pg.init()
 # define variables
-screen = pygame.display.set_mode((1280, 720))
+screen = pg.display.set_mode((1280, 720))
 
-clock = pygame.time.Clock()
+clock = pg.time.Clock()
 
 
 # main loop
 while True:
     # fill the screen with the color purple
     screen.fill("purple")
-# get pygame events
-    for event in pygame.event.get():
+    # get pygame events
+    for event in pg.event.get():
         # if pygame is quitted, end the program
-        if event.type == pygame.QUIT:
-            pygame.quit()
+        if event.type == pg.QUIT:
+            pg.quit()
             raise SystemExit
-# update the screen
-    pygame.display.flip()
+    # update the screen
+    pg.display.flip()
 
-# make the game go at 60 fps
+    # make the game go at 60 fps
     clock.tick(60)
