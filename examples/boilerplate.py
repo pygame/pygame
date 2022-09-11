3129b3e18e211e24a4e73600bb8f820d2e2e0698
DatDerpyDude81 <myanghpms2021@gmail.com>
2022-09-11 08:17:45 -0700

    put loop in a function

--- examples/boilerplate.py
+++ examples/boilerplate.py
@@ -7,18 +7,23 @@ screen = pg.display.set_mode((1280, 720))
 clock = pg.time.Clock()
 
 
-# main loop
-while True:
-    # fill the screen with the color purple
-    screen.fill("purple")
-    # get pygame events
-    for event in pg.event.get():
-        # if pygame is quitted, end the program
-        if event.type == pg.QUIT:
-            pg.quit()
-            raise SystemExit
-    # update the screen
-    pg.display.flip()
+def main():
+    # main loop
+    while True:
+        # fill the screen with the color purple
+        screen.fill("purple")
+        # get pygame events
+        for event in pg.event.get():
+            # if pygame is quitted, end the program
+            if event.type == pg.QUIT:
+                pg.quit()
+                raise SystemExit
+        # update the screen
+        pg.display.flip()
 
-    # make the game go at 60 fps
-    clock.tick(60)
+        # make the game go at 60 fps
+        clock.tick(60)
+
+#run the function
+if __name__ == "__main__":
+    main()
