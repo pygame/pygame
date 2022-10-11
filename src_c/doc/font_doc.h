f45e93af8cb45a69208f89c90b40cb623b6feaed
Robert O'Shea <robertoshea2k11@gmail.com>
2022-10-11 18:07:00 +0100

    Generated documentation

--- src_c/doc/font_doc.h
+++ src_c/doc/font_doc.h
@@ -8,7 +8,7 @@
 #define DOC_PYGAMEFONTGETFONTS "get_fonts() -> list of strings\nget all available fonts"
 #define DOC_PYGAMEFONTMATCHFONT "match_font(name, bold=False, italic=False) -> path\nfind a specific font on the system"
 #define DOC_PYGAMEFONTSYSFONT "SysFont(name, size, bold=False, italic=False) -> Font\ncreate a Font object from the system fonts"
-#define DOC_PYGAMEFONTFONT "Font(filename, size) -> Font\nFont(pathlib.Path, size) -> Font\nFont(object, size) -> Font\ncreate a new Font object from a file"
+#define DOC_PYGAMEFONTFONT "Font(file=None, size=32) -> Font\nFont(filename, size) -> Font\nFont(pathlib.Path, size) -> Font\nFont(object, size) -> Font\ncreate a new Font object from a file"
 #define DOC_FONTBOLD "bold -> bool\nGets or sets whether the font should be rendered in (faked) bold."
 #define DOC_FONTITALIC "italic -> bool\nGets or sets whether the font should be rendered in (faked) italics."
 #define DOC_FONTUNDERLINE "underline -> bool\nGets or sets whether the font should be rendered with an underline."
@@ -70,6 +70,7 @@ pygame.font.SysFont
 create a Font object from the system fonts
 
 pygame.font.Font
+ Font(file=None, size=32) -> Font
  Font(filename, size) -> Font
  Font(pathlib.Path, size) -> Font
  Font(object, size) -> Font
