9f7adc744b5ec5e742cba1a291567268cb24a326
Andrew Coffey <andrewryancoffey@hotmail.com>
2022-12-17 14:42:43 -0600

    wrote test for print_debug_info

---
+++ test/debug_test.py
@@ -0,0 +1,19 @@
+import unittest, unittest.mock
+import io
+
+import pygame
+
+
+class DebugTest(unittest.TestCase):
+    @unittest.mock.patch("sys.stdout", new_callable=io.StringIO)
+    def assert_stdout(self, expected_output, mock_stdout):
+        pygame.print_debug_info()
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    def test_print_debug(self):
+        pygame.print_debug_info("temp_file.txt")
+        with open("temp_file.txt", "r") as temp_file:
+            text = temp_file.read()
+
+        self.assertNotEqual(text, "")
+        self.assert_stdout(text + "\n")
