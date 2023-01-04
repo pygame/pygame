76523444e0f656601497d81af60918e23fae8f56
pmp-p <mail.peny@free.fr>
2023-01-04 14:54:15 +0100

    format

--- src_py/_sdl2/__init__.py
+++ src_py/_sdl2/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-if not sys.platform in ("wasi", "emscripten"):
+if sys.platform not in ("wasi", "emscripten"):
     from .sdl2 import *  # pylint: disable=wildcard-import; lgtm[py/polluting-import]
     from .audio import *  # pylint: disable=wildcard-import; lgtm[py/polluting-import]
     from .video import *  # pylint: disable=wildcard-import; lgtm[py/polluting-import]
