a46b9a4f301be4e2bb908cd26425b2e37044f297
René Dudfield <renesd@gmail.com>
2023-02-20 03:03:24 +0100

    2.2.0.dev2

--- docs/reST/conf.py
+++ docs/reST/conf.py
@@ -49,7 +49,7 @@ copyright = '2000-2022, pygame developers'
 # The short X.Y version.
 version = '2.2.0'
 # The full version, including alpha/beta/rc tags.
-release = '2.2.0.dev1'
+release = '2.2.0.dev2'
 
 # Format strings for the version directives
 versionadded_format = 'New in pygame %s'
