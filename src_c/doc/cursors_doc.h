078c8049ea0629c31abe0cc85a4d0c77964fd555
MarcellPerger1 <marcell.perger.1@gmail.com>
2023-03-14 18:51:16 +0000

    Fix docs for `cursors.Cursor.copy()`

--- src_c/doc/cursors_doc.h
+++ src_c/doc/cursors_doc.h
@@ -3,7 +3,7 @@
 #define DOC_PYGAMECURSORSCOMPILE "compile(strings, black='X', white='.', xor='o') -> data, mask\ncreate binary cursor data from simple strings"
 #define DOC_PYGAMECURSORSLOADXBM "load_xbm(cursorfile) -> cursor_args\nload_xbm(cursorfile, maskfile) -> cursor_args\nload cursor data from an XBM file"
 #define DOC_PYGAMECURSORSCURSOR "Cursor(size, hotspot, xormasks, andmasks) -> Cursor\nCursor(hotspot, surface) -> Cursor\nCursor(constant) -> Cursor\nCursor(Cursor) -> Cursor\nCursor() -> Cursor\npygame object representing a cursor"
-#define DOC_CURSORCOPY ""
+#define DOC_CURSORCOPY "copy() -> Cursor\ncopy the current cursor"
 #define DOC_CURSORTYPE "type -> string\nGets the cursor type"
 #define DOC_CURSORDATA "data -> tuple\nGets the cursor data"
 
@@ -33,7 +33,8 @@ pygame.cursors.Cursor
 pygame object representing a cursor
 
 pygame.cursors.Cursor.copy
-
+ copy() -> Cursor
+copy the current cursor
 
 pygame.cursors.Cursor.type
  type -> string
