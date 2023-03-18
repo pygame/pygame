ca73a4d65d6101e4aa9a8daec565bca9f16be6e1
René Dudfield <renesd@gmail.com>
2023-03-18 16:41:56 +0100

    buildconfig: Bump SDL2-2.26.4 for windows

--- buildconfig/download_win_prebuilt.py
+++ buildconfig/download_win_prebuilt.py
@@ -78,8 +78,8 @@ def get_urls(x86=True, x64=True):
     url_sha1 = []
     url_sha1.extend([
         [
-        'https://www.libsdl.org/release/SDL2-devel-2.24.2-VC.zip',
-        '19b1e9f5bcf700a402bb924c0194c96596283c32',
+        'https://www.libsdl.org/release/SDL2-devel-2.26.4-VC.zip',
+        '0dea77fd5ead8c1a2134af3fc256578327e25879',
         ],
         [
         'https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.5-VC.zip',
@@ -234,12 +234,12 @@ def place_downloaded_prebuilts(temp_dir, move_to_dir, x86=True, x64=True):
         copy(
             os.path.join(
                 temp_dir,
-                'SDL2-devel-2.24.2-VC/SDL2-2.24.2'
+                'SDL2-devel-2.26.4-VC/SDL2-2.26.4'
             ),
             os.path.join(
                 move_to_dir,
                 prebuilt_dir,
-                'SDL2-2.24.2'
+                'SDL2-2.26.4'
             )
         )
 
