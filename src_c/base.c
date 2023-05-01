86726f75be042333ad50fa2d774daefb00f7d1cd
René Dudfield <renesd@gmail.com>
2023-05-01 03:49:02 +0200

    base: Add documentation for pg_SetDefaultWindow

--- src_c/base.c
+++ src_c/base.c
@@ -2219,6 +2219,13 @@ pg_GetDefaultWindow(void)
     return pg_default_window;
 }
 
+/**
+ * \brief Set the default SDL window created by a pygame.display.set_mode()
+ * call. The previous window, if any, is destroyed. Argument *win* may be
+ * *NULL*. This function is called by pygame.display.set_mode().
+ *
+ * \param win The new default window. May be NULL.
+ */
 static void
 pg_SetDefaultWindow(SDL_Window *win)
 {
