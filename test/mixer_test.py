27db81c8a02cadb25bd01335009b2740f216cda6
René Dudfield <renesd@gmail.com>
2023-05-06 02:49:00 +0200

    test mixer: Add test for mixer.get_busy

--- test/mixer_test.py
+++ test/mixer_test.py
@@ -1217,6 +1217,22 @@ class TestGetBusy(unittest.TestCase):
         sound1.stop()
         sound2.stop()
 
+    def test_all_sounds_stopped(self):
+        """
+        Test that get_busy returns False when all sounds are stopped.
+        """
+        sound1 = pygame.mixer.Sound(example_path("data/house_lo.wav"))
+        sound2 = pygame.mixer.Sound(example_path("data/house_lo.wav"))
+        sound1.play()
+        sound2.play()
+        time.sleep(0.2)
+        sound1.stop()
+        sound2.stop()
+        time.sleep(0.2)
+        self.assertFalse(pygame.mixer.get_busy())
+
+
+
 ##################################### MAIN #####################################
 
 if __name__ == "__main__":
