9eb73a321697ee3873d38a80a0f822d4bb6b3915
Yannik Sembritzki <yannik@sembritzki.org>
2023-05-10 11:14:56 +0200

    Add support for keyboard grab without mouse grab (expose SDL_GetWindowKeyboardGrab)

--- test/event_test.py
+++ test/event_test.py
@@ -1,6 +1,7 @@
 import collections
 import time
 import unittest
+import os
 
 import pygame
 
@@ -838,6 +839,22 @@ class EventModuleTest(unittest.TestCase):
         for i in range(5):
             pygame.event.set_grab(i % 2)
             self.assertEqual(pygame.event.get_grab(), i % 2)
+
+    @unittest.skipIf(
+        os.environ.get("SDL_VIDEODRIVER") == "dummy",
+        "requires the SDL_VIDEODRIVER to be a non dummy value",
+    )
+    @unittest.skipIf(pygame.get_sdl_version() < (2, 0, 16), "Needs at least SDL 2.0.16")
+    def test_set_keyboard_grab_and_get_keyboard_grab(self):
+        """Ensure set_keyboard_grab() and get_keyboard_grab() work as expected"""
+
+        surf = pygame.display.set_mode((10, 10))
+
+        pygame.event.set_keyboard_grab(True)
+        self.assertTrue(pygame.event.get_keyboard_grab())
+
+        pygame.event.set_keyboard_grab(False)
+        self.assertFalse(pygame.event.get_keyboard_grab())
 
     def test_poll(self):
         """Ensure poll() works as expected"""
