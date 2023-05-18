a61bde05a930c7d308fb7ac5af3659d97ac9223a
René Dudfield <renesd@gmail.com>
2023-05-18 14:53:19 +0200

    base: Add docs for pg_FloatFromObj

--- src_c/include/_pygame.h
+++ src_c/include/_pygame.h
@@ -140,6 +140,15 @@ typedef struct pg_bufferinfo_s {
 #define pg_TwoIntsFromObj \
     (*(int (*)(PyObject *, int *, int *))PYGAMEAPI_GET_SLOT(base, 4))
 
+/**
+ * \brief Convert number like object *obj* to C float and in *val*.
+ *
+ * \param obj The Python object to convert.
+ * \param val A pointer to the C float to store the result.
+ * \returns 1 if the conversion was successful, 0 otherwise.
+ *
+ * \note This function will clear any Python errors.
+ */
 #define pg_FloatFromObj \
     (*(int (*)(PyObject *, float *))PYGAMEAPI_GET_SLOT(base, 5))
 
