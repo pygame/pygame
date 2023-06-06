e5c3edf31f45ded79925933249ef718916eb6046
Marcin Cieślak <saper@saper.info>
2023-06-06 03:47:03 +0200

    rwobject.c: Use lseek64(3) only for GNU libc
    MacOS, FreeBSD and other C libraries like musl
    neither have nor need lseek64(3).

--- src_c/rwobject.c
+++ src_c/rwobject.c
@@ -33,11 +33,10 @@
 
 #if defined(_WIN32)
 #define PG_LSEEK _lseeki64
-#elif defined(__APPLE__)
-/* Mac does not implement lseek64 */
-#define PG_LSEEK lseek
-#else
+#elif defined(__GLIBC__)
 #define PG_LSEEK lseek64
+#else
+#define PG_LSEEK lseek
 #endif
 
 typedef struct {
