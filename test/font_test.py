fa120b73b787f70b944992db24820fc5a2553519
teknosa <burzum@mal.com>
2023-10-08 21:25:31 +0300

    Added a unit test for checking whether SysFont accepts keyword arguments

--- test/font_test.py
+++ test/font_test.py
@@ -69,6 +69,16 @@ class FontModuleTest(unittest.TestCase):
         self.assertTrue(isinstance(o, pygame_font.FontType))
         o = pygame_font.SysFont("thisisnotafont", 20)
         self.assertTrue(isinstance(o, pygame_font.FontType))
+        
+    def test_SysFont(self):
+        #Check if sysfont accepts keyword arguments
+        if "arial" in fonts:
+            font_name = "arial"
+        else:
+            font_name = sorted(fonts)[0]
+        o = pygame_font.SysFont(font_name, 20, foo=0, bar=1)
+        self.assertTrue(isinstance(o, pygame_font.FontType))
+        
 
     def test_get_default_font(self):
         self.assertEqual(pygame_font.get_default_font(), "freesansbold.ttf")
