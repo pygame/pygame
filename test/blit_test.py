7402393cd4b432976623c1d7a5e566e0edaaaad3
Brandon Hofbauer <71418632+brandonhofbauer@users.noreply.github.com>
2023-11-15 16:40:14 -0800

    Update blit_test.py
    Working on SRCALPHA test style rework

--- test/blit_test.py
+++ test/blit_test.py
@@ -5,18 +5,20 @@ from pygame.locals import *
 
 
 class BlitTest(unittest.TestCase):
-    def test_SRCALPHA(self):
-        """SRCALPHA tests."""
+    def test_SRCALPHA_1(self):
+        """SRCALPHA flag requests the pixel format to include a per-pixel
+        alpha."""
         # blend(s, 0, d) = d
         s = pygame.Surface((1, 1), SRCALPHA, 32)
         s.fill((255, 255, 255, 0))
-
         d = pygame.Surface((1, 1), SRCALPHA, 32)
         d.fill((0, 0, 255, 255))
 
         s.blit(d, (0, 0))
+
         self.assertEqual(s.get_at((0, 0)), d.get_at((0, 0)))
 
+    def test_SRCALPHA_2(self):
         # blend(s, 255, d) = s
         s = pygame.Surface((1, 1), SRCALPHA, 32)
         s.fill((123, 0, 0, 255))
@@ -24,13 +26,17 @@ class BlitTest(unittest.TestCase):
         s1.fill((123, 0, 0, 255))
         d = pygame.Surface((1, 1), SRCALPHA, 32)
         d.fill((10, 0, 0, 0))
+
         s.blit(d, (0, 0))
+
         self.assertEqual(s.get_at((0, 0)), s1.get_at((0, 0)))
 
+    def test_SRCALPHA_3(selfs):
         # TODO: these should be true too.
         # blend(0, sA, 0) = 0
         # blend(255, sA, 255) = 255
         # blend(s, sA, d) <= 255
+        pass
 
     def test_BLEND(self):
         """BLEND_ tests."""
