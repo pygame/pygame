144f2d56230fc0100953621552c27683e83abb3d
cdog1526 <mirkwoodlegolas831@gmail.com>
2023-12-05 23:25:31 -0500

    removed unnecessary files

--- src_py/_sdl2/__init__.py
+++
@@ -1,3 +0,0 @@
-from .sdl2 import *  # pylint: disable=wildcard-import; lgtm[py/polluting-import]
-from .audio import *  # pylint: disable=wildcard-import; lgtm[py/polluting-import]
-from .video import *  # pylint: disable=wildcard-import; lgtm[py/polluting-import]
