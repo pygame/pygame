bf5682edd9475b9e65ec2be5605280fe13404ee3
chpauline <88563834+chpauline@users.noreply.github.com>
2023-12-12 14:44:33 -0800

    Add whitespace for readability

--- test/blit_test.py
+++ test/blit_test.py
@@ -70,9 +70,11 @@ class BlitTest(unittest.TestCase):
         d.fill((30, 0, 255, 255))
         s.blit(d, (0, 0), None, BLEND_SUB)
         self.assertEqual(s.get_at((0, 0))[0], 0)
+
     def make_blit_list(self, num_surfs):
         """Generate a list of tuples representing surfaces and destinations
         for blitting"""
+
         blit_list = []
         for i in range(num_surfs):
             dest = (i * 10, 0)
@@ -81,15 +83,18 @@ class BlitTest(unittest.TestCase):
             surf.fill(color)
             blit_list.append((surf, dest))
         return blit_list
+    
     def test_blits(self):
         """Test pygame.Surface.blits by comparing its performance
         and correctness against manual blitting and includes checks
         for correct color placement and timing measurements."""
+
         NUM_SURFS = 255
         PRINT_TIMING = 0
         dst = pygame.Surface((NUM_SURFS * 10, 10), SRCALPHA, 32)
         dst.fill((230, 230, 230))
         blit_list = self.make_blit_list(NUM_SURFS)
+
         def manuel_blits(blit_list):
             """Manually iterates over the blit_list and blits each surface
             onto the destination rectangle."""
@@ -138,25 +143,33 @@ class BlitTest(unittest.TestCase):
             print(f"Surface.blits generator: {t1 - t0}")
 
     def test_blits_not_sequence(self):
-        """Tests that calling blits() with an invalid non-sequence None argument raises a ValueError."""
+        """Tests that calling blits() with an invalid non-sequence None argument 
+        raises a ValueError."""
+
         dst = pygame.Surface((100, 10), SRCALPHA, 32)
         with self.assertRaises(ValueError):
             dst.blits(None)
 
     def test_blits_wrong_length(self):
-        """Tests that calling blits() with an invalid sequence containing a single surface (without a destination) raises a ValueError."""
+        """Tests that calling blits() with an invalid sequence containing a single surface 
+        (without a destination) raises a ValueError."""
+
         dst = pygame.Surface((100, 10), SRCALPHA, 32)
         with self.assertRaises(ValueError):
             dst.blits([pygame.Surface((10, 10), SRCALPHA, 32)])
 
     def test_blits_bad_surf_args(self):
-        """Tests that calling blits() with a sequence containing an invalid tuple of None arguments raises a TypeError."""
+        """Tests that calling blits() with a sequence containing an invalid tuple of 
+        None arguments raises a TypeError."""
+
         dst = pygame.Surface((100, 10), SRCALPHA, 32)
         with self.assertRaises(TypeError):
             dst.blits([(None, None)])
 
     def test_blits_bad_dest(self):
-        """Tests that calling blits() with a sequence containing an invalid tuple with a destination of None raises a TypeError."""
+        """Tests that calling blits() with a sequence containing an invalid tuple with a 
+        destination of None raises a TypeError."""
+        
         dst = pygame.Surface((100, 10), SRCALPHA, 32)
         with self.assertRaises(TypeError):
             dst.blits([(pygame.Surface((10, 10), SRCALPHA, 32), None)])
