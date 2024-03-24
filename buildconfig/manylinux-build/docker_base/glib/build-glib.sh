cd86258edaa9623cd85a243660d366e7b39fc2d1
Christian Clauss <cclauss@me.com>
2024-03-24 01:23:15 +0100

    GLIB=glib-2.80.0

--- buildconfig/manylinux-build/docker_base/glib/build-glib.sh
+++ buildconfig/manylinux-build/docker_base/glib/build-glib.sh
@@ -3,9 +3,9 @@ set -e -x
 
 cd $(dirname `readlink -f "$0"`)
 
-GLIB=glib-2.81.0
+GLIB=glib-2.80.0
 
-curl -sL --retry 10 https://download.gnome.org/sources/glib/2.81/${GLIB}.tar.xz > ${GLIB}.tar.xz
+curl -sL --retry 10 https://download.gnome.org/sources/glib/2.80/${GLIB}.tar.xz > ${GLIB}.tar.xz
 sha512sum -c glib.sha512
 
 unxz ${GLIB}.tar.xz
