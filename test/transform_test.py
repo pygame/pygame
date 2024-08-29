68b2f03c897c7889ea7c4b4555eef4c806f8e605
Alessandro Chitarrini <alessandro.chitarrini51@gmail.com>
2024-08-29 10:51:32 +0200

    Update tests for transform.scale_by rounding fix

--- test/transform_test.py
+++ test/transform_test.py
@@ -136,6 +136,7 @@ class TransformModuleTest(unittest.TestCase):
 
     def test_scale_by(self):
         s = pygame.Surface((32, 32))
+        s.fill((255, 0, 0))
 
         s2 = pygame.transform.scale_by(s, 2)
         self.assertEqual((64, 64), s2.get_size())
@@ -145,6 +146,7 @@ class TransformModuleTest(unittest.TestCase):
 
         dest = pygame.Surface((64, 48))
         pygame.transform.scale_by(s, (2.0, 1.5), dest_surface=dest)
+        self.assertEqual((64, 48), dest.get_size())
 
     def test_smoothscale_by(self):
         s = pygame.Surface((32, 32))
