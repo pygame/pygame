80644be649ff8d27d62c06779f36170ac05b6d4b
René Dudfield <renesd@gmail.com>
2024-09-29 11:21:34 +0200

    manylinux: Remove old pypy 3.7

--- buildconfig/manylinux-build/build-wheels.sh
+++ buildconfig/manylinux-build/build-wheels.sh
@@ -3,7 +3,7 @@ set -e -x
 
 
 if [[ "$1" == "buildpypy" ]]; then
-    export SUPPORTED_PYTHONS="cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310 cp311-cp311 cp312-cp312 pp37-pypy37_pp73 pp39-pypy39_pp73 pp38-pypy38_pp73"
+    export SUPPORTED_PYTHONS="cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310 cp311-cp311 cp312-cp312 pp39-pypy39_pp73 pp38-pypy38_pp73"
 elif [[ "$1" == "buildpypy10" ]]; then
     export SUPPORTED_PYTHONS="cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39 cp310-cp310"
 else
