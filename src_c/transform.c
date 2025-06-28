6459cbedb98e2eb7e435547b25c793fb6bf84828
Zenith Reddy <zenithreddy696@gmail.com>
2025-06-28 13:13:56 +0530

    rounding figure has been set

--- src_c/transform.c
+++ src_c/transform.c
@@ -627,8 +627,9 @@ surf_scale_by(PyObject *self, PyObject *args, PyObject *kwargs)
     surf = pgSurface_AsSurface(surfobj);
     if (!surf)
         return RAISE(pgExc_SDLError, "display Surface quit");
-    newsurf = scale_to(surfobj, surfobj2, (int)(surf->w * scalex),
-                       (int)(surf->h * scaley));
+
+    newsurf = scale_to(surfobj, surfobj2,(int)round(surf->w * scalex),(int)round(surf->h * scaley));
+
     if (!newsurf) {
         return NULL;
     }
